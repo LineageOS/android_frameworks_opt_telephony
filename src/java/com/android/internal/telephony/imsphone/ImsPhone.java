430b05a9ba7b730cea56e79c3c18ab8c282621d6
Cecilia Sörensson <cecilia.sorensson@sonyericsson.com>
2011-01-27 14:27:11 +0100

    Functionality for knowing if USSD is network initiated.
    Add #isNetworkInitiatedUSSD method in MmiCode for others like Telephony
    service to judge if USSD is network-initiated or not.
    
    Test: manual - Checked that return value of #isNetworkInitiatedUSSD.
    Bug: 28948816
    Change-Id: I769b40f214b23c03e1c6c8bbca255548b4d10ec6
    Merged-In: I769b40f214b23c03e1c6c8bbca255548b4d10ec6

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -1470,7 +1470,7 @@ public class ImsPhone extends ImsPhoneBase {
             } else {
                 found.onUssdFinished(ussdMessage, isUssdRequest);
             }
-        } else if (!isUssdError && ussdMessage != null) {
+        } else if (!isUssdError && !TextUtils.isEmpty(ussdMessage)) {
                 // pending USSD not found
                 // The network may initiate its own USSD request
 
