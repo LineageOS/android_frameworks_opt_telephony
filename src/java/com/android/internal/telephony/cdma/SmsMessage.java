9ebea45a36838f0547a9c30f7cd9c60b03aab3b4
Siddartha Pothapragada <spotha@codeaurora.org>
2011-09-19 11:39:18 -0700

    Telephony: Add 'ServiceCategory' to SMS finger print
    In case of certain broadcast messages sent back to back, say if,
    service category 0x0002 Administrative and service category
    0x0003 Maintenance are sent in sequence,
    one after the other, service category 0x0003 fails to be displayed
    on the UI although modem sends it to telephony
    layer. This is because telephony rejects/discards the new
    incoming message - 0x0003 as its finger-print matches exactly
    with the previously sent message 0x0002.
    
    Bug: 7238888
    Change-Id: Id48169924605f423ee97a0866e754462f8c4c30d

--- src/java/com/android/internal/telephony/cdma/SmsMessage.java
+++ src/java/com/android/internal/telephony/cdma/SmsMessage.java
@@ -979,6 +979,7 @@ public class SmsMessage extends SmsMessageBase {
     /* package */ byte[] getIncomingSmsFingerprint() {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
 
+        output.write(mEnvelope.serviceCategory);
         output.write(mEnvelope.teleService);
         output.write(mEnvelope.origAddress.origBytes, 0, mEnvelope.origAddress.origBytes.length);
         output.write(mEnvelope.bearerData, 0, mEnvelope.bearerData.length);
