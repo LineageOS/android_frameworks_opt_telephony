823d29780c3a14388160b04857d8fedb56df57a5
Alan Wang <flwang@codeaurora.org>
2011-11-18 13:45:11 -0800

    Telephony: Delay SIM IO until SIM_READY
    When SIM_REFRESH with SIM_INIT is received we need to
    reread EFs but only if and when app is ready.
    
    Change-Id: Iae4b52839f64c61f962e7d9da1545656dc219266

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -1205,6 +1205,10 @@ public class SIMRecords extends IccRecords {
                 if (DBG) log("handleSimRefresh with SIM_REFRESH_INIT");
                 // need to reload all files (that we care about)
                 onIccRefreshInit();
+                // Reregister for ready notification so that we read files
+                // if app is ready
+                mParentApp.unregisterForReady(this);
+                mParentApp.registerForReady(this, EVENT_APP_READY, null);
                 break;
             case IccRefreshResponse.REFRESH_RESULT_RESET:
                 if (DBG) log("handleSimRefresh with SIM_REFRESH_RESET");
