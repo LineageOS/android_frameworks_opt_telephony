8d23832ebdc754d5b664f29c9d2e22ee9b9c2ac1
Alex Yakavenka <ayakav@codeaurora.org>
2012-06-18 13:59:36 -0700

    Telephony: Add new apn.sim.operator_numeric property
    Use new property to filter apns. We need this new property
    because icc.operator_numeric property is also used in cdma
    and ApnSetting UI might display wrong set of apns if the
    peroperty value used was set by RuimRecords
    CRs-Fixed: 362846
    
    Change-Id: Iba3f5a21797b2a48cbc05cfb0e9b28c60de0c6dd

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -19,6 +19,7 @@ package com.android.internal.telephony.uicc;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ALPHA;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_ISO_COUNTRY;
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC;
+import static com.android.internal.telephony.TelephonyProperties.PROPERTY_APN_SIM_OPERATOR_NUMERIC;
 import android.content.Context;
 import android.os.AsyncResult;
 import android.os.Message;
@@ -1313,6 +1314,7 @@ public class SIMRecords extends IccRecords {
         log("SIMRecords: onAllRecordsLoaded set 'gsm.sim.operator.numeric' to operator='" +
                 operator + "'");
         SystemProperties.set(PROPERTY_ICC_OPERATOR_NUMERIC, operator);
+        SystemProperties.set(PROPERTY_APN_SIM_OPERATOR_NUMERIC, operator);
 
         if (mImsi != null) {
             SystemProperties.set(PROPERTY_ICC_OPERATOR_ISO_COUNTRY,
