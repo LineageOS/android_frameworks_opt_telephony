ade003b80ff6b91d51af2fab4d79db219a72a92b
Shriram Ganesh <sganesh@codeaurora.org>
2012-07-20 12:46:49 +0530

    Telephony: Handle SIM Refresh for MSISDN, CFIS, CFF_CPHS Came from part 2 of original change below: CRs-Fixed: 181461, 278534, 278075, 325699, 440321
    Change-Id: I5f467c1a40979529125ee3ccc11b36ea8caa4e46

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -30,6 +30,7 @@ import android.os.Message;
 import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.telephony.Rlog;
+import android.util.Log;
 
 import com.android.internal.telephony.CommandsInterface;
 import com.android.internal.telephony.MccTable;
@@ -1196,6 +1197,24 @@ public class SIMRecords extends IccRecords {
                 log("[CSP] SIM Refresh for EF_CSP_CPHS");
                 mFh.loadEFTransparent(EF_CSP_CPHS,
                         obtainMessage(EVENT_GET_CSP_CPHS_DONE));
+                break;
+            case EF_MSISDN:
+                mRecordsToLoad++;
+                Log.i(LOG_TAG,"SIM Refresh called for EF_MSISDN");
+                new AdnRecordLoader(mFh).loadFromEF(EF_MSISDN, EF_EXT1, 1,
+                        obtainMessage(EVENT_GET_MSISDN_DONE));
+                break;
+            case EF_CFIS:
+                mRecordsToLoad++;
+                Log.i(LOG_TAG,"SIM Refresh called for EF_CFIS");
+                mFh.loadEFLinearFixed(EF_CFIS,
+                        1, obtainMessage(EVENT_GET_CFIS_DONE));
+                break;
+            case EF_CFF_CPHS:
+                mRecordsToLoad++;
+                Log.i(LOG_TAG,"SIM Refresh called for EF_CFF_CPHS");
+                mFh.loadEFTransparent(EF_CFF_CPHS,
+                obtainMessage(EVENT_GET_CFF_DONE));
                 break;
             default:
                 // For now, fetch all records if this is not a
