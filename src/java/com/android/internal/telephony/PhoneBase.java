4bf323c3a37e910dd6b3eb1ffabe508e0dff5b2a
Venkat Ram Prakash Kotni <vkotni@codeaurora.org>
2012-08-30 17:19:43 +0530

    Telephony: Return 'null' as per TelephonyManager need
    Return 'null' instead of "" (empty string) when Line1AlphaTag,
    IccSerialNumber and Msisdn are unavailable.
    
    Bug: 8449375
    Change-Id: I5d8052922363f4b3b900bcdea490afe22fedce4f

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -702,7 +702,7 @@ public abstract class PhoneBase extends Handler implements Phone {
     @Override
     public String getIccSerialNumber() {
         IccRecords r = mIccRecords.get();
-        return (r != null) ? r.iccid : "";
+        return (r != null) ? r.iccid : null;
     }
 
     @Override
