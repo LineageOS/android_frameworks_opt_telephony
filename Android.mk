79932ec1d6fb628ac9c4e45b9e6253b7e43f3ea8
Ricardo Cerqueira <github@cerqueira.org>
2012-11-22 17:47:31 +0100

    RIL: Make RIL subclassable
    Change-Id: I9144ef279ca9c8cb9e657a28342f0f4cb3e7debd
    
    From: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date: Thu, 22 Nov 2012 19:01:13 +0100
    Subject: [PATCH] telephony: Use reflection to create command interface (RIL class)
    
    Change-Id: Icdc02eaa74b89dbde4ddfb989cc62481d6423312
    
    PhoneFactory: fall back to default RIL if custom RIL Class fails
    
    If the ro.telephony.ril_class property is set to an invalid class it will cause
    Phone to repeatedly FC and make the device unuseable. This allows it to handle the
    exceptions gracefully and fall back on the default RIL.
    
    Change-Id: Ic9c309aff173de5267aa7ef633bfff87243f24fd
    
    PhoneFactory: Block radio init if the device's custom class can't be used
    
    Trap the initialization process in a 10 second error loop. The device
    will be normally usable, and have a dead radio. The radio log buffer
    will have the full stack dump explaining what went wrong during the
    custom class instantiation.
    
    Change-Id: Id47d51737a9fb78e6d43a179ba304b1ba03c4219
    
    From: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date: Thu, 22 Nov 2012 06:02:18 +0000
    Subject: [PATCH] Allow a device to include its own RIL subclass
    
    For cases where a single device needs a subclass, there's no need to
    pollute the common core with it: just stick it in the device's own
    repository, and add something like
    "BOARD_RIL_CLASS := ../../../device/manuf/model/ril/" to the BoardConfig
    
    Change-Id: I9d01ceb6e5587382eb216341e2e373ec0c52848f
    
    From: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date: Sat, 8 Mar 2014 19:35:17 +0700
    Subject: [PATCH] RIL: mark mInstanceId as protected
    
    To use in RIL subclasses
    
    Change-Id: I4ab1ea79079e3b03670b1e985f7a574df1f16658
    
    From: Dan Pasanen <dan.pasanen@gmail.com>
    Date: Mon, 10 Nov 2014 23:50:17 -0600
    Subject: [PATCH] RIL: set responseCellInfoList protected to override in sub classes
    
    Change-Id: Ibbbee0c1a59ab2be6b71fc2f70c6279314b55d6e
    
    From: JustArchi <JustArchi@JustArchi.net>
    Date: Sat, 2 May 2015 06:17:49 +0200
    Subject: [PATCH] RIL: Make some methods protected
    
    This is required for some RIL subclasses, if they want to use them
    
    Change-Id: I59f0148be0f49b4ee2997a669e74a61c2f6a8069
    
    From: Varun Chitre <varun.chitre15@gmail.com>
    Date: Sat, 16 May 2015 02:28:40 -0400
    Subject: [PATCH] RIL : allow RILReceiver to be overridden
    
    Change-Id: If23252901073a1658fa965e14cd7188ce39397f8
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>
    
    Telephony: Allow more RIL methods to be overridden
    
    Change-Id: I7a18e16256da5b2205910d894008044a6b9478ae
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>
    
    Change-Id: Id43b0fb43ad7f19c2e4b2a6af5590f7b3a17761e

--- Android.mk
+++ Android.mk
@@ -26,6 +26,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src/java) \
 
 LOCAL_JAVA_LIBRARIES := voip-common ims-common telephony-ext
 LOCAL_STATIC_JAVA_LIBRARIES := ims-ext-common
+
+ifneq ($(BOARD_RIL_CLASS),)
+LOCAL_SRC_FILES += $(call find-other-java-files,$(BOARD_RIL_CLASS))
+endif
+
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := telephony-common
 
