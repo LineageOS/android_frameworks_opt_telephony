55ee4a8015b86c13d86f047e5dbccffe3493d3e3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-22 06:02:18 +0000

    Allow a device to include its own RIL subclass
    For cases where a single device needs a subclass, there's no need to
    pollute the common core with it: just stick it in the device's own
    repository, and add something like
    "BOARD_RIL_CLASS := ../../../device/manuf/model/ril/" to the BoardConfig
    
    Change-Id: I9d01ceb6e5587382eb216341e2e373ec0c52848f

--- Android.mk
+++ Android.mk
@@ -25,6 +25,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src/java) \
 	$(call all-logtags-files-under, src/java)
 
 LOCAL_JAVA_LIBRARIES := voip-common ims-common
+
+ifneq ($(BOARD_RIL_CLASS),)
+LOCAL_SRC_FILES += $(call find-other-java-files,$(BOARD_RIL_CLASS))
+endif
+
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := telephony-common
 
