2944d2a296d8d056bc5e7931d6d92967388a9044
codeworkx <codeworkx@cyanogenmod.org>
2012-11-22 19:48:05 +0100

    telephony: add SamsungExynos4RIL class
    Forward ported from CM10 including contributions from:
    Andrew Dodd<atd7@cornell.edu>
    Daniel Bateman <jetison.24@gmail.com>
    Daniel Hillenbrand <codeworkx@cyanogenmod.org>
    Ethan Chen <intervigil@gmail.com>
    Lee Chu <lee.lc.chu@gmail.com>
    Mike Kasick <mike@kasick.org>
    
    Change-Id: Ibc33a866a02a88b346ae30aa6f09a398edfc79c1
    
    Conflicts:
    	src/java/com/android/internal/telephony/RIL.java

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -103,6 +103,7 @@ class RILRequest {
     //***** Instance Variables
     int mSerial;
     int mRequest;
+    long creationTime;
     Message mResult;
     Parcel mParcel;
     RILRequest mNext;
@@ -134,6 +135,7 @@ class RILRequest {
 
         rr.mRequest = request;
         rr.mResult = result;
+        rr.creationTime = System.currentTimeMillis();
         rr.mParcel = Parcel.obtain();
 
         if (result != null && result.getTarget() == null) {
