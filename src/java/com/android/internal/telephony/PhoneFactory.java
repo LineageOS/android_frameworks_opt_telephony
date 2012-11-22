249c25805c7c3848f1253f313e1b9fcb7a6c26ae
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-22 19:01:13 +0100

    telephony: Use reflection to create command interface (RIL class)
    Change-Id: Icdc02eaa74b89dbde4ddfb989cc62481d6423312
    
    PhoneFactory: fall back to default RIL if custom RIL Class fails
    
    If the ro.telephony.ril_class property is set to an invalid class it will cause
    Phone to repeatedly FC and make the device unuseable. This allows it to handle the
    exceptions gracefully and fall back on the default RIL.
    
    Change-Id: Ic9c309aff173de5267aa7ef633bfff87243f24fd
    
    PhoneFactory: Block radio init if the device's custom class can't be used
    
    Trap the initialization process in a 10 second error loop. The device
    will be normally usable, and have a dead radio. The radio log buffer
    will have the full stack dump explaining what went wrong during the
    custom class instantiation
    
    Change-Id: Id47d51737a9fb78e6d43a179ba304b1ba03c4219

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -44,6 +44,7 @@ import com.android.internal.telephony.uicc.UiccController;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
 
 /**
  * {@hide}
@@ -135,6 +136,8 @@ public class PhoneFactory {
                 int[] networkModes = new int[numPhones];
                 sProxyPhones = new PhoneProxy[numPhones];
                 sCommandsInterfaces = new RIL[numPhones];
+                String sRILClassname = SystemProperties.get("ro.telephony.ril_class", "RIL").trim();
+                Rlog.i(LOG_TAG, "RILClassname is " + sRILClassname);
 
                 for (int i = 0; i < numPhones; i++) {
                     //reads the system properties and makes commandsinterface
@@ -162,8 +165,20 @@ public class PhoneFactory {
                     }
 
                     Rlog.i(LOG_TAG, "Network Mode set to " + Integer.toString(networkModes[i]));
-                    sCommandsInterfaces[i] = new RIL(context, networkModes[i],
-                            cdmaSubscription, i);
+                    // Use reflection to construct the RIL class (defaults to RIL)
+                    try {
+                        sCommandsInterfaces[i] = instantiateCustomRIL(
+                                                     sRILClassname, context, networkModes[i], cdmaSubscription, i);
+                    } catch (Exception e) {
+                        // 6 different types of exceptions are thrown here that it's
+                        // easier to just catch Exception as our "error handling" is the same.
+                        // Yes, we're blocking the whole thing and making the radio unusable. That's by design.
+                        // The log message should make it clear why the radio is broken
+                        while (true) {
+                            Rlog.e(LOG_TAG, "Unable to construct custom RIL class", e);
+                            try {Thread.sleep(10000);} catch (InterruptedException ie) {}
+                        }
+                    }
                 }
                 Rlog.i(LOG_TAG, "Creating SubscriptionController");
                 SubscriptionController.init(context, sCommandsInterfaces);
@@ -237,6 +252,14 @@ public class PhoneFactory {
                     sPhoneNotifier, phoneId);
             return phone;
         }
+    }
+
+    private static <T> T instantiateCustomRIL(
+                      String sRILClassname, Context context, int networkMode, int cdmaSubscription, Integer instanceId)
+                      throws Exception {
+        Class<?> clazz = Class.forName("com.android.internal.telephony." + sRILClassname);
+        Constructor<?> constructor = clazz.getConstructor(Context.class, int.class, int.class, Integer.class);
+        return (T) clazz.cast(constructor.newInstance(context, networkMode, cdmaSubscription, instanceId));
     }
 
     public static Phone getDefaultPhone() {
