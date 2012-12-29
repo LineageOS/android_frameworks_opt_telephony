8a62547ffa69d77368eb3b231521079a4ca5530f
invisiblek <dan.pasanen@gmail.com>
2012-12-29 09:18:11 -0600

    RIL: Use a prop for a common Samsung bug and clean up some of the mess
    This adds the prop ro.ril.samsung_data_call_magic which will be used to enable
    a fix for Samsung data call bug present in most (if not all) samsung devices.
    
    Also clean up the RILs that had this in them and remove SamsungQualcommD2RIL since
    it's only purpose was to add this.
    
    Change-Id: Ibb3b2e1aa3ba5e14c08f23d1840829cab17a168b

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -235,6 +235,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
     // WAKE_LOCK_TIMEOUT occurs.
     int mRequestMessagesWaiting;
 
+    private boolean mSamsungDataCallMagic = SystemProperties.getBoolean("ro.ril.samsung_data_call_magic", false);
+
     //I'd rather this be LinkedList or something
     ArrayList<RILRequest> mRequestsList = new ArrayList<RILRequest>();
 
@@ -3179,6 +3181,13 @@ public class RIL extends BaseCommands implements CommandsInterface {
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
             dc.isVoicePrivacy = (0 != p.readInt());
+            if ( mSamsungDataCallMagic == true) {
+                //Some Samsung magic data for Videocalls
+                // hack taken from smdk4210ril class
+                voiceSettings = p.readInt();
+                //printing it to cosole for later investigation
+                Log.d(LOG_TAG, "Samsung magic = " + voiceSettings);
+            }
             dc.number = p.readString();
             int np = p.readInt();
             dc.numberPresentation = DriverCall.presentationFromCLIP(np);
