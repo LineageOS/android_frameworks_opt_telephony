d79e9ba251e2e3acb2e679e52514b6f238d50cd0
Wink Saville <wink@google.com>
2013-01-28 15:26:08 -0800

    Delete frameworks/base/voip use voip-common from frameworks/opt/net/voip
    Change-Id: Ieaba759a0f69b45c4b8839cbed1fe757cdf190c5

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src/java) \
 	$(call all-Iaidl-files-under, src/java) \
 	$(call all-logtags-files-under, src/java)
 
+LOCAL_JAVA_LIBRARIES := voip-common
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := telephony-common
 
