97cabb4543823fd989f4aa371fca41b1aeb07278
Dan Pasanen <dan.pasanen@gmail.com>
2013-03-11 13:38:24 -0500

    PhoneProxy: Add a prop to enable always creating of CDMALTEPhone
    Current Motorola OMAP4 devices need this in order to function properly
    Will need to enable telephony.rilV7NeedsCDMALTEPhone prop on these devices
    
    Change-Id: I22ef7688e422986d63a0cced1fb2e872c6e01c80

--- src/java/com/android/internal/telephony/PhoneProxy.java
+++ src/java/com/android/internal/telephony/PhoneProxy.java
@@ -55,6 +55,8 @@ public class PhoneProxy extends Handler implements Phone {
     private boolean mResetModemOnRadioTechnologyChange = false;
 
     private int mRilVersion;
+    private boolean mRilV7NeedsCDMALTEPhone = SystemProperties.getBoolean(
+                    "telephony.rilV7NeedsCDMALTEPhone", false);
 
     private static final int EVENT_VOICE_RADIO_TECH_CHANGED = 1;
     private static final int EVENT_RADIO_ON = 2;
@@ -145,7 +147,8 @@ public class PhoneProxy extends Handler implements Phone {
     private void updatePhoneObject(int newVoiceRadioTech) {
 
         if (mActivePhone != null) {
-            if(mRilVersion == 6 && getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) {
+            if((mRilVersion == 6 && getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE) ||
+                mRilV7NeedsCDMALTEPhone) {
                 /*
                  * On v6 RIL, when LTE_ON_CDMA is TRUE, always create CDMALTEPhone
                  * irrespective of the voice radio tech reported.
