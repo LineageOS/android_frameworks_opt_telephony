04187ec4d9cdef64cc1257815fa9e44e37202a86
Sudheer Reddy Yampalla <syampa@codeaurora.org>
2013-03-22 10:34:07 +0530

    Telephony: Add + for international number for MT SMS
    Add + if international number is not having + at starting.
    
    Change-Id: I0000b0685181f17ba6b5b0eaaba60a77971e7908
    Bug: 10951615

--- src/java/com/android/internal/telephony/cdma/SmsMessage.java
+++ src/java/com/android/internal/telephony/cdma/SmsMessage.java
@@ -710,6 +710,11 @@ public class SmsMessage extends SmsMessageBase {
 
         if (mOriginatingAddress != null) {
             mOriginatingAddress.address = new String(mOriginatingAddress.origBytes);
+            if (mOriginatingAddress.ton == CdmaSmsAddress.TON_INTERNATIONAL_OR_IP) {
+                if (mOriginatingAddress.address.charAt(0) != '+') {
+                    mOriginatingAddress.address = "+" + mOriginatingAddress.address;
+                }
+            }
             if (VDBG) Rlog.v(LOG_TAG, "SMS originating address: "
                     + mOriginatingAddress.address);
         }
