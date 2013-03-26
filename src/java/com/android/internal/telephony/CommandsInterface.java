5bd3f135d8b12c2e983a1bb4e8d3163dbb5dc196
Rakesh Pallerla <rakesh@codeaurora.org>
2013-03-26 16:34:14 +0530

    Add ME Depersonalization support.
    Add support for all ME De-Personalizations as per specs
    3GPP2 C.S0068-0 and 3GPP TS 22.022.
    
    Change-Id: Ieeb196c72eb74311643f6af0bf8958acb9541406
    CRs-fixed: 467276

--- src/java/com/android/internal/telephony/CommandsInterface.java
+++ src/java/com/android/internal/telephony/CommandsInterface.java
@@ -746,7 +746,7 @@ public interface CommandsInterface {
 
     void changeBarringPassword(String facility, String oldPwd, String newPwd, Message result);
 
-    void supplyNetworkDepersonalization(String netpin, Message result);
+    void supplyDepersonalization(String netpin, String type, Message result);
 
     /**
      *  returned message
