8159692460efb1a0788d4756b6953bb7ad5073a3
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-04-01 17:38:02 +0100

    RIL: Add compatibility with qcom's DSDS
    Devices with radio libraries built against a DSDS-enabled qualcomm
    BSP (anything in the last 4 or 5 months, apparently) have a slight
    incompatibility in the RIL protocol: it expects the first 4 bytes
    of the datastream to be the slot's identification (even if the
    device isn't dual-SIM). Absence of those 4 bytes will make the
    connection hang.
    
    Change-Id: I9f5821add02e7e5b5286c3b0818cb4a94edcc13c

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -560,6 +560,20 @@ public class RIL extends BaseCommands implements CommandsInterface {
                 mSocket = s;
                 Rlog.i(RILJ_LOG_TAG, "Connected to '" + rilSocket + "' socket");
 
+                /* Compatibility with qcom's DSDS (Dual SIM) stack */
+                if (needsOldRilFeature("qcomdsds")) {
+                    String str = "SUB1";
+                    byte[] data = str.getBytes();
+                    try {
+                        mSocket.getOutputStream().write(data);
+                        Log.i(LOG_TAG, "Data sent!!");
+                    } catch (IOException ex) {
+                            Log.e(LOG_TAG, "IOException", ex);
+                    } catch (RuntimeException exc) {
+                        Log.e(LOG_TAG, "Uncaught exception ", exc);
+                    }
+                }
+
                 int length = 0;
                 try {
                     InputStream is = mSocket.getInputStream();
