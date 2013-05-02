43875b86e42bbe815b3cd8e11f873c071c51fe6c
Sandeep Kunta <skunta@codeaurora.org>
2013-05-02 15:57:51 +0530

    Stk: Add support for alpha in REFRESH command
    RIL_UNSOL_STK_EVENT_NOTIFY is given by RIL in case there is alpha
    id to be displayed to user in refresh proactive cmd.
    Support is added to display Alpha id to user when this event comes
    due to REFRESH proactive cmd.
    Also if refresh mode is init or reset, idle mode text needs to be
    cleared as per stk 3GPP spec 11.14. This logic is handled in stk
    app to satisfy both the above requiremnts.
    
    CRs-Fixed: 474693
    
    Change-Id: Ic16da08d611753f2ee4a5b288f46b985078ca46f

--- src/java/com/android/internal/telephony/cat/CatService.java
+++ src/java/com/android/internal/telephony/cat/CatService.java
@@ -339,9 +339,8 @@ public class CatService extends Handler implements AppInterface {
                 }
                 break;
             case REFRESH:
-                // ME side only handles refresh commands which meant to remove IDLE
-                // MODE TEXT.
-                cmdParams.mCmdDet.typeOfCommand = CommandType.SET_UP_IDLE_MODE_TEXT.value();
+                //Stk app service displays alpha id to user if it is present, nothing to do here
+                CatLog.d(this, "Pass Refresh to Stk app");
                 break;
             case SET_UP_IDLE_MODE_TEXT:
                 resultCode = cmdParams.mLoadIconFailed ? ResultCode.PRFRMD_ICON_NOT_DISPLAYED
