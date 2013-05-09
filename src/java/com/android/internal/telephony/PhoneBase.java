c9ebcc6034f8517b05e1b7fcbe27dce822a304ab
Abhishek Adappa <aadappa@codeaurora.org>
2013-05-09 13:13:09 -0700

    IccPhoneBookInterfaceManager will decide which Card App to use.
    Phone will not tell IccPhoneBoookIntefaceManager which record
    to use. Phone will only provide the card associated with it.
    
    PhoneBook will iterate through the apps on the card to
    determine if it is a 3G or 2G card and use EF_PBR or
    EF_ADN respectively.
    
    Reset mIs3gCard flag before determining if present card is 3G.
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1
    CRs-Fixed: 478627

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -586,6 +586,10 @@ public abstract class PhoneBase extends Handler implements Phone {
         return mContext;
     }
 
+    // Set the Card into the Phone Book.
+    protected void setCardInPhoneBook() {
+    }
+
     // Will be called when icc changed
     protected abstract void onUpdateIccAvailability();
 
