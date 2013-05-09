b50f7d9025de4d685bec4ecfcf2ed44994b976e5
Abhishek Adappa <aadappa@codeaurora.org>
2013-05-09 13:13:09 -0700

    IccPhoneBookInterfaceManager will decide which Card App to use
    Phone will not tell IccPhoneBoookIntefaceManager which record
    to use. Phone will only provide the card associated with it.
    
    PhoneBook will iterate through the apps on the card to
    determine if it is a 3G or 2G card and use EF_PBR or EF_ADN
    respectively.
    
    Change-Id: Icf5997dd6a6ba5e1de675cf5f4334c78c2c037f1
    CRs-Fixed: 478627

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -114,7 +114,7 @@ public class GSMPhone extends PhoneBase {
     GsmCallTracker mCT;
     protected GsmServiceStateTracker mSST;
     ArrayList <GsmMmiCode> mPendingMMIs = new ArrayList<GsmMmiCode>();
-    SimPhoneBookInterfaceManager mSimPhoneBookIntManager;
+    protected SimPhoneBookInterfaceManager mSimPhoneBookIntManager;
     PhoneSubInfo mSubInfo;
 
     Registrant mPostDialHandler;
@@ -1521,11 +1521,25 @@ public class GSMPhone extends PhoneBase {
         return  mUiccController.getUiccCardApplication(UiccController.APP_FAM_3GPP);
     }
 
+    // Set the Card into the Phone Book.
+    @Override
+    protected void setCardInPhoneBook() {
+        if (mUiccController == null ) {
+            return;
+        }
+
+        mSimPhoneBookIntManager.setIccCard(mUiccController.getUiccCard());
+    }
+
     @Override
     protected void onUpdateIccAvailability() {
         if (mUiccController == null ) {
             return;
         }
+
+        // Get the latest info on the card and
+        // send this to Phone Book
+        setCardInPhoneBook();
 
         UiccCardApplication newUiccApplication = getUiccCardApplication();
 
@@ -1535,7 +1549,6 @@ public class GSMPhone extends PhoneBase {
                 if (LOCAL_DEBUG) log("Removing stale icc objects.");
                 if (mIccRecords.get() != null) {
                     unregisterForSimRecordEvents();
-                    mSimPhoneBookIntManager.updateIccRecords(null);
                 }
                 mIccRecords.set(null);
                 mUiccApplication.set(null);
@@ -1545,7 +1558,6 @@ public class GSMPhone extends PhoneBase {
                 mUiccApplication.set(newUiccApplication);
                 mIccRecords.set(newUiccApplication.getIccRecords());
                 registerForSimRecordEvents();
-                mSimPhoneBookIntManager.updateIccRecords(mIccRecords.get());
             }
         }
     }
