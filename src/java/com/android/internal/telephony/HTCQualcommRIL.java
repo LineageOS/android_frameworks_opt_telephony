1196b35e54e187a453b8bf6ecfbb37e1f98f83e3
Ethan Chen <intervigil@gmail.com>
2013-05-16 16:09:58 -0700

    Revert "libril: HTC8960RIL: add flag to force CDMA+LTE network mode"
    This reverts part of commit: 492c36536d259d55ea947a03ff22ad05ae6f51b2
    
    Change-Id: I8cd4d1261b26e9fbe2aaaf7d423b9606aa243494

--- src/java/com/android/internal/telephony/HTCQualcommRIL.java
+++ src/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -65,9 +65,6 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
         // a new method just for naming sake.
         boolean oldRil = needsOldRilFeature("icccardstatus");
 
-        // force CDMA + LTE network type
-        boolean forceCdmaLteNetworkType = needsOldRilFeature("forceCdmaLteNetworkType");
-
         IccCardStatus cardStatus = new IccCardStatus();
         cardStatus.setCardState(p.readInt());
         cardStatus.setUniversalPinState(p.readInt());
@@ -106,25 +103,7 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
             cardStatus.mApplications[i] = appStatus;
         }
 
-        // pretty hack way to do it. but keeps it out of CM telephony stack
-        if (forceCdmaLteNetworkType)
-            setPreferredNetworkType(8, null);
-
         return cardStatus;
-    }
-
-    @Override
-    public void setPreferredNetworkType(int networkType , Message response) {
-        /**
-          * If not using a USIM, ignore LTE mode and go to 3G
-          */
-        if (!mUSIM && networkType == RILConstants.NETWORK_MODE_LTE_GSM_WCDMA &&
-                 mSetPreferredNetworkType >= RILConstants.NETWORK_MODE_WCDMA_PREF) {
-            networkType = RILConstants.NETWORK_MODE_WCDMA_PREF;
-        }
-        mSetPreferredNetworkType = networkType;
-
-        super.setPreferredNetworkType(networkType, response);
     }
 
     @Override
