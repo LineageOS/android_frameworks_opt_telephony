597a9738b415d842ae6aedbd95ff1720498d3e5b
Shareef Ali <shareefalis@cyanogenmod.org>
2013-05-17 07:46:42 -0400

    SamsungQcomRIL: New class and get rid of old and outdated codes
    Consolidate the RIL and base on the CDMA RIL
    Now inherit class related to the Samsung Qualcomm family
    
    Special thanks to Dan Pasanen for coninuing the patcheset
    while my macbook was in repair.
    
    * Rebase on standard ril
    * samsungDriverCall now handled in the superclass
    
    * Supports:
      * Quad-core Exynos4 with Qualcomm modem and later
      * Snapdragon S3 and later
    
    Change-Id: Ie593e32087a9bfeb5d16802a10c2739cf1061676

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -210,6 +210,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
     static final String LOG_TAG = "RILJ";
     static final boolean RILJ_LOGD = true;
     static final boolean RILJ_LOGV = false; // STOP SHIP if true
+    protected boolean samsungDriverCall = false;
 
     /**
      * Wake lock timeout should be longer than the longest timeout in
@@ -2968,7 +2969,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
      *
      * @param rilVer is the version of the ril or -1 if disconnected.
      */
-    private void notifyRegistrantsRilConnectionChanged(int rilVer) {
+    protected void notifyRegistrantsRilConnectionChanged(int rilVer) {
         mRilVersion = rilVer;
         if (mRilConnectedRegistrants != null) {
             mRilConnectedRegistrants.notifyRegistrants(
@@ -3198,6 +3199,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
             dc.als = p.readInt();
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
+            if(samsungDriverCall)
+                 p.readInt();
             dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
             int np = p.readInt();
