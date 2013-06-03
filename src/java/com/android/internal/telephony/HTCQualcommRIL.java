a8dd7a045649b15d95b3afde47ba5c8ebe28d2de
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-06-03 17:12:00 +0200

    HTCQualcommRIL: clean up unused functions and responses
    Change-Id: I0c381677dbb2a8a76008c8eec6e1057fd22be43c

--- src/java/com/android/internal/telephony/HTCQualcommRIL.java
+++ src/java/com/android/internal/telephony/HTCQualcommRIL.java
@@ -24,7 +24,6 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
-import android.telephony.SignalStrength;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -33,24 +32,7 @@ import com.android.internal.telephony.IccCardStatus;
 
 import java.util.ArrayList;
 
-/**
- * Qualcomm RIL class for basebands that do not send the SIM status
- * piggybacked in RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED. Instead,
- * these radios will send radio state and we have to query for SIM
- * status separately.
- *
- * {@hide}
- */
 public class HTCQualcommRIL extends RIL implements CommandsInterface {
-
-    private static final int RIL_UNSOL_ENTER_LPM = 3023;
-    private static final int RIL_UNSOL_TPMR_ID = 3024;
-    private static final int RIL_UNSOL_CDMA_3G_INDICATOR = 4259;
-    private static final int RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR = 4262;
-    private static final int RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE = 4802;
-    private static final int RIL_UNSOL_RESPONSE_VOICE_RADIO_TECH_CHANGED = 21004;
-    private static final int RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED = 5755;
-    private static final int RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED = 5757;
 
     public HTCQualcommRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
@@ -72,54 +54,6 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
     }
 
     @Override
-    protected Object
-    responseSignalStrength(Parcel p) {
-        /* HTC signal strength format:
-         * 0: GW_SignalStrength
-         * 1: GW_SignalStrength.bitErrorRate
-         * 2: CDMA_SignalStrength.dbm
-         * 3: CDMA_SignalStrength.ecio
-         * 4: EVDO_SignalStrength.dbm
-         * 5: EVDO_SignalStrength.ecio
-         * 6: EVDO_SignalStrength.signalNoiseRatio
-         * 7: ATT_SignalStrength.dbm
-         * 8: ATT_SignalStrength.ecno
-         * 9: LTE_SignalStrength.signalStrength
-         * 10: LTE_SignalStrength.rsrp
-         * 11: LTE_SignalStrength.rsrq
-         * 12: LTE_SignalStrength.rssnr
-         * 13: LTE_SignalStrength.cqi
-         */
-
-        int parcelSize = p.dataSize();
-
-        int gsmSignalStrength = p.readInt();
-        int gsmBitErrorRate = p.readInt();
-        int cdmaDbm = p.readInt();
-        int cdmaEcio = p.readInt();
-        int evdoDbm = p.readInt();
-        int evdoEcio = p.readInt();
-        int evdoSnr = p.readInt();
-        if (parcelSize == 14) {
-            /* Signal strength parcel contains HTC ATT signal strength */
-            p.readInt(); // ATT_SignalStrength.dbm
-            p.readInt(); // ATT_SignalStrength.ecno
-        }
-        int lteSignalStrength = p.readInt();
-        int lteRsrp = p.readInt();
-        int lteRsrq = p.readInt();
-        int lteRssnr = p.readInt();
-        int lteCqi = p.readInt();
-        boolean isGsm = (mPhoneType == RILConstants.GSM_PHONE);
-
-        SignalStrength signalStrength = new SignalStrength(gsmSignalStrength,
-                gsmBitErrorRate, cdmaDbm, cdmaEcio, evdoDbm, evdoEcio, evdoSnr,
-                lteSignalStrength, lteRsrp, lteRsrq, lteRssnr, lteCqi, isGsm);
-
-        return signalStrength;
-    }
-
-    @Override
     protected void
     processUnsolicited (Parcel p) {
         Object ret;
@@ -127,14 +61,6 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
         int response = p.readInt();
 
         switch(response) {
-            case RIL_UNSOL_ENTER_LPM: ret = responseVoid(p); break;
-            case RIL_UNSOL_TPMR_ID: ret = responseVoid(p); break;
-            case RIL_UNSOL_CDMA_3G_INDICATOR:  ret = responseInts(p); break;
-            case RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR:  ret = responseInts(p); break;
-            case RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE:  ret = responseInts(p); break;
-            case RIL_UNSOL_RESPONSE_VOICE_RADIO_TECH_CHANGED: ret = responseVoid(p); break;
-            case RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED: ret = responseVoid(p); break;
-            case RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED: ret = responseVoid(p); break;
             case RIL_UNSOL_RIL_CONNECTED: ret = responseInts(p); break;
 
             default:
@@ -147,21 +73,6 @@ public class HTCQualcommRIL extends RIL implements CommandsInterface {
         }
 
         switch(response) {
-            case RIL_UNSOL_ENTER_LPM:
-            case RIL_UNSOL_TPMR_ID:
-            case RIL_UNSOL_CDMA_3G_INDICATOR:
-            case RIL_UNSOL_CDMA_ENHANCE_ROAMING_INDICATOR:
-            case RIL_UNSOL_RESPONSE_PHONE_MODE_CHANGE:
-            case RIL_UNSOL_RESPONSE_VOICE_RADIO_TECH_CHANGED:
-            case RIL_UNSOL_RESPONSE_IMS_NETWORK_STATE_CHANGED:
-            case RIL_UNSOL_RESPONSE_DATA_NETWORK_STATE_CHANGED:
-                if (RILJ_LOGD) unsljLogRet(response, ret);
-
-                if (mExitEmergencyCallbackModeRegistrants != null) {
-                    mExitEmergencyCallbackModeRegistrants.notifyRegistrants(
-                                        new AsyncResult (null, null, null));
-                }
-                break;
             case RIL_UNSOL_RIL_CONNECTED: {
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
