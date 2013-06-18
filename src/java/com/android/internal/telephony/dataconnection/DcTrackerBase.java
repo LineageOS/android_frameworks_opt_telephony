8dae752c227d634033c78626b2a9a61decef1a5c
Alex Yakavenka <ayakav@codeaurora.org>
2013-06-18 09:49:13 -0700

    Fix cdma data calls based on NV.
    For NV data calls we need to create dummy profiles
    When DcTracker switches from lte to cdma we need to bring
    down existing data call and establish new one (same goes
    for switching cdma->lte).
    
    Bug: 10804691
    
    Conflicts:
    src/java/com/android/internal/telephony/dataconnection/DcTracker.java
    Change-Id: I7ad8d0632854ea9875afb76709c1ba894bf048fd

--- src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -761,7 +761,7 @@ public abstract class DcTrackerBase extends Handler {
     protected abstract void onCleanUpConnection(boolean tearDown, int apnId, String reason);
     protected abstract void onCleanUpAllConnections(String cause);
     public abstract boolean isDataPossible(String apnType);
-    protected abstract void onUpdateIcc();
+    protected abstract boolean onUpdateIcc();
     protected abstract void completeConnection(ApnContext apnContext);
     public abstract void setDataAllowed(boolean enable, Message response);
     public abstract String[] getPcscfAddress(String apnType);
