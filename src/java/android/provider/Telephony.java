2d262ccc50f8d1aa7648c0256664689d7f4bcb9a
Danny Baumann <dannybaumann@web.de>
2013-06-26 17:05:55 +0200

    Store phone blacklist in content provider (1/4)
    Change-Id: I9dafdb3e10bd2a71695c2a02cc483c594d86c543

--- src/java/android/provider/Telephony.java
+++ src/java/android/provider/Telephony.java
@@ -1996,4 +1996,68 @@ public final class Telephony {
                 CMAS_CERTAINTY
         };
     }
+
+    public static final class Blacklist implements BaseColumns {
+        /**
+         * The content:// style URL for this table
+         */
+        public static final Uri CONTENT_URI =
+                Uri.parse("content://blacklist");
+
+        /**
+         * The content:// style URL for filtering this table by number
+         */
+        public static final Uri CONTENT_FILTER_BYNUMBER_URI =
+                Uri.parse("content://blacklist/bynumber");
+
+        /**
+         * The content:// style URL for filtering this table on phone numbers
+         */
+        public static final Uri CONTENT_PHONE_URI =
+                Uri.parse("content://blacklist/phone");
+
+        /**
+         * The content:// style URL for filtering this table on message numbers
+         */
+        public static final Uri CONTENT_MESSAGE_URI =
+                Uri.parse("content://blacklist/message");
+
+
+        /**
+         * Query parameter used to match numbers by regular-expression like
+         * matching. Supported are the '*' and the '.' operators.
+         * <p>
+         * TYPE: boolean
+         */
+        public static final String REGEX_KEY = "regex";
+
+        /**
+         * The default sort order for this table
+         */
+        public static final String DEFAULT_SORT_ORDER = "number ASC";
+
+        /**
+         * The phone number as the user entered it.
+         * <P>Type: TEXT</P>
+         */
+        public static final String NUMBER = "number";
+
+        /**
+         * Whether the number contains a regular expression pattern
+         * <P>Type: BOOLEAN (read only)</P>
+         */
+        public static final String IS_REGEX = "is_regex";
+
+        /**
+         * Blacklisting mode for phone calls
+         * <P>Type: INTEGER (int)</P>
+         */
+        public static final String PHONE_MODE = "phone";
+
+        /**
+         * Blacklisting mode for messages
+         * <P>Type: INTEGER (int)</P>
+         */
+        public static final String MESSAGE_MODE = "message";
+    }
 }
