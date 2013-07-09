35758fb158d8bd025f6623f77bb6fb4be3997a8c
Abhishek Adappa <aadappa@codeaurora.org>
2013-07-08 17:59:23 -0700

    Clean up Icc Refresh handling
    1. When a SIM refresh is received of type INIT or RESET,
    don't immediately start re-fetching the records, because
    the app might not be ready. In this case, we could end up
    broadcasting that all records are loaded pre-maturely.
    
    On a refresh of the above type, the app will move out of the
    ready state and back into the PIN state, if PIN is enabled.
    Start reading the records only after the PIN has been verified
    and the app has moved to the READY state.
    
    2. UiccController will listen for refresh and propagate it to
    the Card and the Apps. The App will set its state to UNKNOWN
    on receiving a refresh.
    UiccController will also request card status on receiving
    refresh to get the latest state.
    
    This avoids having to make assumptions on the ordering of the
    Unsols for Sim Refresh and card state changes. This also
    ensures that after an app state is marked UNKNOWN,
    it will move to READY, if it is truly ready.
    
    3. Moved common implementation for refresh into IccRecords
    for better code reuse.
    
    4. Side effect of #3 is RuimRecords will now reset AdnCache when
    refresh of type INIT is received.
    
    5. Do not send Radio Power Off on SIM_RESET
    
    This change is needed as per GCF test spec
    
    CRs-Fixed: 441924
    CRs-Fixed: 503646
    
    Conflicts:
    src/java/com/android/internal/telephony/uicc/UiccController.java
    Change-Id: I58b1237a49d4f943a699d751a8680c25ce4394ad

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -165,7 +165,6 @@ public class SIMRecords extends IccRecords {
     private static final int EVENT_SET_CPHS_MAILBOX_DONE = 25;
     private static final int EVENT_GET_INFO_CPHS_DONE = 26;
     // private static final int EVENT_SET_MSISDN_DONE = 30; Defined in IccRecords as 30
-    private static final int EVENT_SIM_REFRESH = 31;
     private static final int EVENT_GET_CFIS_DONE = 32;
     private static final int EVENT_GET_CSP_CPHS_DONE = 33;
     private static final int EVENT_GET_GID1_DONE = 34;
@@ -208,8 +207,6 @@ public class SIMRecords extends IccRecords {
         // recordsToLoad is set to 0 because no requests are made yet
         mRecordsToLoad = 0;
 
-        mCi.registerForIccRefresh(this, EVENT_SIM_REFRESH, null);
-
         // Start off by setting empty state
         resetRecords();
         mParentApp.registerForReady(this, EVENT_APP_READY, null);
@@ -221,7 +218,6 @@ public class SIMRecords extends IccRecords {
     public void dispose() {
         if (DBG) log("Disposing SIMRecords this=" + this);
         //Unregister for all events
-        mCi.unregisterForIccRefresh(this);
         mParentApp.unregisterForReady(this);
         mParentApp.unregisterForLocked(this);
         resetRecords();
@@ -1132,14 +1128,6 @@ public class SIMRecords extends IccRecords {
                     ((Message) ar.userObj).sendToTarget();
                 }
                 break;
-            case EVENT_SIM_REFRESH:
-                isRecordLoadResponse = false;
-                ar = (AsyncResult)msg.obj;
-                if (DBG) log("Sim REFRESH with exception: " + ar.exception);
-                if (ar.exception == null) {
-                    handleSimRefresh((IccRefreshResponse)ar.result);
-                }
-                break;
             case EVENT_GET_CFIS_DONE:
                 isRecordLoadResponse = true;
 
@@ -1233,7 +1221,8 @@ public class SIMRecords extends IccRecords {
         }
     }
 
-    private void handleFileUpdate(int efid) {
+    @Override
+    protected void handleFileUpdate(int efid) {
         switch(efid) {
             case EF_MBDN:
                 mRecordsToLoad++;
@@ -1278,49 +1267,6 @@ public class SIMRecords extends IccRecords {
                 // TODO: Handle other cases, instead of fetching all.
                 mAdnCache.reset();
                 fetchSimRecords();
-                break;
-        }
-    }
-
-    private void handleSimRefresh(IccRefreshResponse refreshResponse){
-        if (refreshResponse == null) {
-            if (DBG) log("handleSimRefresh received without input");
-            return;
-        }
-
-        if (refreshResponse.aid != null &&
-                !refreshResponse.aid.equals(mParentApp.getAid())) {
-            // This is for different app. Ignore.
-            return;
-        }
-
-        switch (refreshResponse.refreshResult) {
-            case IccRefreshResponse.REFRESH_RESULT_FILE_UPDATE:
-                if (DBG) log("handleSimRefresh with SIM_FILE_UPDATED");
-                handleFileUpdate(refreshResponse.efId);
-                break;
-            case IccRefreshResponse.REFRESH_RESULT_INIT:
-                if (DBG) log("handleSimRefresh with SIM_REFRESH_INIT");
-                // need to reload all files (that we care about)
-                onIccRefreshInit();
-                break;
-            case IccRefreshResponse.REFRESH_RESULT_RESET:
-                if (DBG) log("handleSimRefresh with SIM_REFRESH_RESET");
-                if (requirePowerOffOnSimRefreshReset()) {
-                    mCi.setRadioPower(false, null);
-                    /* Note: no need to call setRadioPower(true).  Assuming the desired
-                    * radio power state is still ON (as tracked by ServiceStateTracker),
-                    * ServiceStateTracker will call setRadioPower when it receives the
-                    * RADIO_STATE_CHANGED notification for the power off.  And if the
-                    * desired power state has changed in the interim, we don't want to
-                    * override it with an unconditional power on.
-                    */
-                }
-                mAdnCache.reset();
-                break;
-            default:
-                // unknown refresh operation
-                if (DBG) log("handleSimRefresh with unknown operation");
                 break;
         }
     }
