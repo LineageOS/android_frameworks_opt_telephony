b77e69611a6199b2f0c524129eb1b725255e676f
Abhishek Adappa <aadappa@codeaurora.org>
2013-07-17 14:59:13 -0700

    Don't display prompt when card inserted/removed
    UiccCard displays a prompt to reboot the device when a card is
    inserted or removed. Don't display this if the modem supports
    hot swapping.
    
    Change-Id: I43fc95e61382671f61286818304ec58e922e8605
    (cherry picked from commit b056d5fcf4e5181e3d5098ac4000dc6d7e9aabf9)
    (cherry picked from commit a7e8c9a6397b52c8f1b618d734a1762befae4cb7)
    (cherry picked from commit 80a5464ba1af1cb9772cc932a418bf063245a1c4)

--- src/java/com/android/internal/telephony/uicc/UiccCard.java
+++ src/java/com/android/internal/telephony/uicc/UiccCard.java
@@ -228,6 +228,15 @@ public class UiccCard {
     }
 
     private void onIccSwap(boolean isAdded) {
+
+        boolean isHotSwapSupported = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hotswapCapable);
+
+        if(isHotSwapSupported) {
+            //no need to prompt the user to reboot the device
+            return;
+        }
+
         synchronized (mLock) {
             // TODO: Here we assume the device can't handle SIM hot-swap
             //      and has to reboot. We may want to add a property,
