lt03lte-cafaosp/platform-frameworks-opt-telephony

CatService: Alpha ID fix for OPEN_CHANNEL and other p-commands
p-cmds that support the below mentioned scenarios:
- SETUP CALL
- OPEN CHANNEL
- SEND DTMF
- SEND SMS
- SEND SS
- SEND USSD
- LAUNCH BROWSER
- PLAY TONE
- SELECT ITEM

Senarios handled :
- NO ALPHA ID : Introduce a new configurable sys prop
  "persist.atel.noalpha.usrcnf".
   --  Allow display of default UI confirmation if prop is set to true
   --  No UI confirmation to be show if prop is set to false
- NULL ALPHA ID (len = 0 ) :
   -- NO UI confirmation to be displayed

Change-Id: Ie211c9765a21c9c584ce8420b51cfe59476234cc
CRs-Fixed: 344570

src/java/com/android/internal/telephony/cat/CatService.java
@@ -102,7 +102,7 @@ public class CatService extends Handler implements AppInterface {
     private static final int DEV_ID_TERMINAL    = 0x82;
     private static final int DEV_ID_NETWORK     = 0x83;
 
-    static final String STK_DEFAULT = "Defualt Message";
+    static final String STK_DEFAULT = "Default Message";
 
     /* Intentionally private for singleton */
     private CatService(CommandsInterface ci, UiccCardApplication ca, IccRecords ir,
@@ -334,11 +334,24 @@ public class CatService extends Handler implements AppInterface {
             case RECEIVE_DATA:
             case SEND_DATA:
                 BIPClientParams cmd = (BIPClientParams) cmdParams;
-                if (cmd.bHasAlphaId && (cmd.textMsg.text == null)) {
+                /*
+                 * If the text mesg is null, need to send the response
+                 * back to the card in the following scenarios
+                 * - It has alpha ID tag with no Text Msg (or)
+                 * - If alphaUsrCnf is not set. In the above cases
+                 *   there should be no UI indication given to the user.
+                 */
+                boolean alphaUsrCnf = SystemProperties.getBoolean(
+                         "persist.atel.noalpha.usrcnf", false);
+                CatLog.d(this, "alphaUsrCnf: " + alphaUsrCnf + ", bHasAlphaId: " + cmd.bHasAlphaId);
+
+                if (( cmd.textMsg.text == null) && ( cmd.bHasAlphaId || !alphaUsrCnf)) {
                     CatLog.d(this, "cmd " + cmdParams.getCommandType() + " with null alpha id");
                     // If alpha length is zero, we just respond with OK.
                     if (isProactiveCmd) {
                         sendTerminalResponse(cmdParams.cmdDet, ResultCode.OK, false, 0, null);
+                    } else if (cmdParams.getCommandType() == CommandType.OPEN_CHANNEL) {
+                        mCmdIf.handleCallSetupRequestFromSim(true, null);
                     }
                     return;
                 }

LineageOS/android_frameworks_opt_telephony

Conflicts:
	src/java/com/android/internal/telephony/cat/CatService.java

src/java/com/android/internal/telephony/cat/CatService.java
@@ -97,7 +97,7 @@ public class CatService extends Handler implements AppInterface {
     private static final int DEV_ID_KEYPAD      = 0x01;
     private static final int DEV_ID_UICC        = 0x81;
     private static final int DEV_ID_TERMINAL    = 0x82;
-    static final String STK_DEFAULT = "Defualt Message";
+    static final String STK_DEFAULT = "Default Message";
 
     /* Intentionally private for singleton */
     private CatService(CommandsInterface ci, UiccCardApplication ca, IccRecords ir,
@@ -291,11 +291,24 @@ public class CatService extends Handler implements AppInterface {
             case RECEIVE_DATA:
             case SEND_DATA:
                 BIPClientParams cmd = (BIPClientParams) cmdParams;
-                if (cmd.mHasAlphaId && (cmd.mTextMsg.text == null)) {
+                /*
+                 * If the text mesg is null, need to send the response
+                 * back to the card in the following scenarios
+                 * - It has alpha ID tag with no Text Msg (or)
+                 * - If alphaUsrCnf is not set. In the above cases
+                 *   there should be no UI indication given to the user.
+                 */
+                boolean alphaUsrCnf = SystemProperties.getBoolean(
+                         "persist.atel.noalpha.usrcnf", false);
+                CatLog.d(this, "alphaUsrCnf: " + alphaUsrCnf + ", mHasAlphaId: " + cmd.mHasAlphaId);
+
+                if (( cmd.mTextMsg.text == null) && ( cmd.mHasAlphaId || !alphaUsrCnf)) {
                     CatLog.d(this, "cmd " + cmdParams.getCommandType() + " with null alpha id");
                     // If alpha length is zero, we just respond with OK.
                     if (isProactiveCmd) {
                         sendTerminalResponse(cmdParams.mCmdDet, ResultCode.OK, false, 0, null);
+                    } else if (cmdParams.getCommandType() == CommandType.OPEN_CHANNEL) {
+                        mCmdIf.handleCallSetupRequestFromSim(true, null);
                     }
                     return;
                 }