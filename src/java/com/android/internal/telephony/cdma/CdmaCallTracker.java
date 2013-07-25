lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: CDMA - Handle call collision scenarios
There can be 2 types of call collisions
    - Modem/RIL dropping an existing call and replace it with
      another one with the same call id (index)
    - Modem/RIL, after ending the previous call, reports the new
      call with a different call id
Both these above cases have been handled to avert undesirable
behavior at telephony layer.

Change-Id: Ib9fb5e2a46c80783e332c78a034313dadac1e4ce
CRs-Fixed: 453390

src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
@@ -23,6 +23,7 @@ import android.os.Registrant;
 import android.os.RegistrantList;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.ServiceState;
+import android.text.TextUtils;
 import android.util.Log;
 import android.os.SystemProperties;
 
@@ -53,7 +54,7 @@ public final class CdmaCallTracker extends CallTracker {
 
     //***** Constants
 
-    static final int MAX_CONNECTIONS = 1;   // only 1 connection allowed in CDMA
+    static final int MAX_CONNECTIONS = 2;   // only 2 connections allowed in CDMA
     static final int MAX_CONNECTIONS_PER_CALL = 1; // only 1 connection allowed per call
 
     //***** Instance Variables
@@ -525,6 +526,19 @@ public final class CdmaCallTracker extends CallTracker {
             if (DBG_POLL) log("poll: conn[i=" + i + "]=" +
                     conn+", dc=" + dc);
 
+            if (conn != null && dc != null && !TextUtils.isEmpty(conn.address)
+                    && !conn.compareTo(dc)) {
+                // This means we received a different call than we expected in the call list.
+                // Drop the call, and set conn to null, so that the dc can be processed as a new
+                // call by the logic below.
+                // This may happen if for some reason the modem drops the call, and replaces it
+                // with another one, but still using the same index (for instance, if BS drops our
+                // MO and replaces with an MT due to priority rules)
+                log("New call with same index. Dropping old call");
+                droppedDuringPoll.add(conn);
+                conn = null;
+            }
+
             if (conn == null && dc != null) {
                 // Connection appeared in CLCC response that we don't know about
                 if (pendingMO != null && pendingMO.compareTo(dc)) {
@@ -570,26 +584,36 @@ public final class CdmaCallTracker extends CallTracker {
                 }
                 hasNonHangupStateChanged = true;
             } else if (conn != null && dc == null) {
-                // This case means the RIL has no more active call anymore and
-                // we need to clean up the foregroundCall and ringingCall.
-                // Loop through foreground call connections as
-                // it contains the known logical connections.
-                int count = foregroundCall.connections.size();
-                for (int n = 0; n < count; n++) {
-                    if (Phone.DEBUG_PHONE) log("adding fgCall cn " + n + " to droppedDuringPoll");
-                    CdmaConnection cn = (CdmaConnection)foregroundCall.connections.get(n);
-                    droppedDuringPoll.add(cn);
+                if (dcSize != 0)
+                {
+                    // This happens if the call we are looking at (index i)
+                    // got dropped but the call list is not yet empty.
+                    log("conn != null, dc == null. Still have connections in the call list");
+                    droppedDuringPoll.add(conn);
+                } else {
+                    // This case means the RIL has no more active call anymore and
+                    // we need to clean up the foregroundCall and ringingCall.
+                    // Loop through foreground call connections as
+                    // it contains the known logical connections.
+                    int count = foregroundCall.connections.size();
+                    for (int n = 0; n < count; n++) {
+                        if (Phone.DEBUG_PHONE)
+                            log("adding fgCall cn " + n + " to droppedDuringPoll");
+                        CdmaConnection cn = (CdmaConnection) foregroundCall.connections.get(n);
+                        droppedDuringPoll.add(cn);
+                    }
+                    count = ringingCall.connections.size();
+                    // Loop through ringing call connections as
+                    // it may contain the known logical connections.
+                    for (int n = 0; n < count; n++) {
+                        if (Phone.DEBUG_PHONE)
+                            log("adding rgCall cn " + n + " to droppedDuringPoll");
+                        CdmaConnection cn = (CdmaConnection) ringingCall.connections.get(n);
+                        droppedDuringPoll.add(cn);
+                    }
+                    foregroundCall.setGeneric(false);
+                    ringingCall.setGeneric(false);
                 }
-                count = ringingCall.connections.size();
-                // Loop through ringing call connections as
-                // it may contain the known logical connections.
-                for (int n = 0; n < count; n++) {
-                    if (Phone.DEBUG_PHONE) log("adding rgCall cn " + n + " to droppedDuringPoll");
-                    CdmaConnection cn = (CdmaConnection)ringingCall.connections.get(n);
-                    droppedDuringPoll.add(cn);
-                }
-                foregroundCall.setGeneric(false);
-                ringingCall.setGeneric(false);
 
                 // Re-start Ecm timer when the connected emergency call ends
                 if (mIsEcmTimerCanceled) {

LineageOS/android_frameworks_opt_telephony

Change-Id: I65d8d941ccec63be754ca79c36bef34618fb1fb1

src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
@@ -24,6 +24,7 @@ import android.os.RegistrantList;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.ServiceState;
 import android.telephony.Rlog;
+import android.text.TextUtils;
 import android.os.SystemProperties;
 
 import com.android.internal.telephony.CallStateException;
@@ -53,7 +54,7 @@ public final class CdmaCallTracker extends CallTracker {
 
     //***** Constants
 
-    static final int MAX_CONNECTIONS = 1;   // only 1 connection allowed in CDMA
+    static final int MAX_CONNECTIONS = 2;   // only 2 connections allowed in CDMA
     static final int MAX_CONNECTIONS_PER_CALL = 1; // only 1 connection allowed per call
 
     //***** Instance Variables
@@ -515,6 +516,19 @@ public final class CdmaCallTracker extends CallTracker {
             if (DBG_POLL) log("poll: conn[i=" + i + "]=" +
                     conn+", dc=" + dc);
 
+            if (conn != null && dc != null && !TextUtils.isEmpty(conn.mAddress)
+                    && !conn.compareTo(dc)) {
+                // This means we received a different call than we expected in the call list.
+                // Drop the call, and set conn to null, so that the dc can be processed as a new
+                // call by the logic below.
+                // This may happen if for some reason the modem drops the call, and replaces it
+                // with another one, but still using the same index (for instance, if BS drops our
+                // MO and replaces with an MT due to priority rules)
+                log("New call with same index. Dropping old call");
+                mDroppedDuringPoll.add(conn);
+                conn = null;
+            }
+
             if (conn == null && dc != null) {
                 // Connection appeared in CLCC response that we don't know about
                 if (mPendingMO != null && mPendingMO.compareTo(dc)) {
@@ -560,26 +574,36 @@ public final class CdmaCallTracker extends CallTracker {
                 }
                 hasNonHangupStateChanged = true;
             } else if (conn != null && dc == null) {
-                // This case means the RIL has no more active call anymore and
-                // we need to clean up the foregroundCall and ringingCall.
-                // Loop through foreground call connections as
-                // it contains the known logical connections.
-                int count = mForegroundCall.mConnections.size();
-                for (int n = 0; n < count; n++) {
-                    if (Phone.DEBUG_PHONE) log("adding fgCall cn " + n + " to droppedDuringPoll");
-                    CdmaConnection cn = (CdmaConnection)mForegroundCall.mConnections.get(n);
-                    mDroppedDuringPoll.add(cn);
+                if (dcSize != 0)
+                {
+                    // This happens if the call we are looking at (index i)
+                    // got dropped but the call list is not yet empty.
+                    log("conn != null, dc == null. Still have connections in the call list");
+                    mDroppedDuringPoll.add(conn);
+                } else {
+                    // This case means the RIL has no more active call anymore and
+                    // we need to clean up the foregroundCall and ringingCall.
+                    // Loop through foreground call connections as
+                    // it contains the known logical connections.
+                    int count = mForegroundCall.mConnections.size();
+                    for (int n = 0; n < count; n++) {
+                        if (Phone.DEBUG_PHONE)
+                            log("adding fgCall cn " + n + " to droppedDuringPoll");
+                        CdmaConnection cn = (CdmaConnection) mForegroundCall.mConnections.get(n);
+                        mDroppedDuringPoll.add(cn);
+                    }
+                    count = mRingingCall.mConnections.size();
+                    // Loop through ringing call connections as
+                    // it may contain the known logical connections.
+                    for (int n = 0; n < count; n++) {
+                        if (Phone.DEBUG_PHONE)
+                            log("adding rgCall cn " + n + " to droppedDuringPoll");
+                        CdmaConnection cn = (CdmaConnection) mRingingCall.mConnections.get(n);
+                        mDroppedDuringPoll.add(cn);
+                    }
+                    mForegroundCall.setGeneric(false);
+                    mRingingCall.setGeneric(false);
                 }
-                count = mRingingCall.mConnections.size();
-                // Loop through ringing call connections as
-                // it may contain the known logical connections.
-                for (int n = 0; n < count; n++) {
-                    if (Phone.DEBUG_PHONE) log("adding rgCall cn " + n + " to droppedDuringPoll");
-                    CdmaConnection cn = (CdmaConnection)mRingingCall.mConnections.get(n);
-                    mDroppedDuringPoll.add(cn);
-                }
-                mForegroundCall.setGeneric(false);
-                mRingingCall.setGeneric(false);
 
                 // Re-start Ecm timer when the connected emergency call ends
                 if (mIsEcmTimerCanceled) {