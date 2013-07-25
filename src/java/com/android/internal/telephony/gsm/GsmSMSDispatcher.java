lt03lte-cafaosp/platform-frameworks-opt-telephony

Reorganize code to handle class 2 SMS.
If card is having more than one USIM applications then because of
registering mechanism in SIMRecords.java to command interface, command
interface will hold only the last registered application registrant.
So when ever telephony received SMS_ON_SIM it will come to only
last application created not the desired application which is used
by actual phone object.
Move registering mechanism for SMS_ON_SIM to SMSDispatcher
where we can get the proper application.

Change-Id: Ieb6cfad0a2f735563cfce1ea14ead39f8c9458c8
CRs-fixed: 432656

src/java/com/android/internal/telephony/gsm/GsmSMSDispatcher.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * Not a Contribution, Apache license notifications and license are retained
  * for attribution purposes only.
@@ -47,8 +47,10 @@ import com.android.internal.telephony.SmsMessageBase;
 import com.android.internal.telephony.SmsStorageMonitor;
 import com.android.internal.telephony.SmsUsageMonitor;
 import com.android.internal.telephony.TelephonyProperties;
+import com.android.internal.telephony.uicc.IccConstants;
 import com.android.internal.telephony.uicc.IccRecords;
 import com.android.internal.telephony.uicc.IccUtils;
+import com.android.internal.telephony.uicc.SIMRecords;
 import com.android.internal.telephony.uicc.UiccCardApplication;
 import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.uicc.UsimServiceTable;
@@ -83,6 +85,7 @@ public class GsmSMSDispatcher extends SMSDispatcher {
         mImsSMSDispatcher = imsSMSDispatcher;
         mDataDownloadHandler = new UsimDataDownloadHandler(mCm);
         mCm.setOnNewGsmSms(this, EVENT_NEW_SMS, null);
+        mCm.setOnSmsOnSim(this, EVENT_SMS_ON_ICC, null);
         mCm.setOnSmsStatus(this, EVENT_NEW_SMS_STATUS_REPORT, null);
         mCm.setOnNewGsmBroadcastSms(this, EVENT_NEW_BROADCAST_SMS, null);
         mUiccController = UiccController.getInstance();
@@ -93,8 +96,12 @@ public class GsmSMSDispatcher extends SMSDispatcher {
     @Override
     public void dispose() {
         mCm.unSetOnNewGsmSms(this);
+        mCm.unSetOnSmsOnSim(this);
         mCm.unSetOnSmsStatus(this);
         mCm.unSetOnNewGsmBroadcastSms(this);
+        if (mIccRecords.get() != null) {
+            mIccRecords.get().unregisterForNewSms(this);
+        }
     }
 
     @Override
@@ -138,6 +145,12 @@ public class GsmSMSDispatcher extends SMSDispatcher {
 
         case EVENT_ICC_CHANGED:
             onUpdateIccAvailability();
+            break;
+
+        case EVENT_SMS_ON_ICC:
+            if (mIccRecords.get() != null) {
+                ((SIMRecords)(mIccRecords.get())).handleSmsOnIcc((AsyncResult) msg.obj);
+            }
             break;
 
         default:

LineageOS/android_frameworks_opt_telephony

Change-Id: I02616e7a2e0f0250cc27780e53a061ef49f72fd6

src/java/com/android/internal/telephony/gsm/GsmSMSDispatcher.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,8 +44,10 @@ import com.android.internal.telephony.SmsMessageBase;
 import com.android.internal.telephony.SmsStorageMonitor;
 import com.android.internal.telephony.SmsUsageMonitor;
 import com.android.internal.telephony.TelephonyProperties;
+import com.android.internal.telephony.uicc.IccConstants;
 import com.android.internal.telephony.uicc.IccRecords;
 import com.android.internal.telephony.uicc.IccUtils;
+import com.android.internal.telephony.uicc.SIMRecords;
 import com.android.internal.telephony.uicc.UiccCardApplication;
 import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.uicc.UsimServiceTable;
@@ -82,6 +84,7 @@ public final class GsmSMSDispatcher extends SMSDispatcher {
         mCi.setOnNewGsmSms(this, EVENT_NEW_SMS, null);
         mCi.setOnSmsStatus(this, EVENT_NEW_SMS_STATUS_REPORT, null);
         mCi.setOnNewGsmBroadcastSms(this, EVENT_NEW_BROADCAST_SMS, null);
+        mCi.setOnSmsOnSim(this, EVENT_SMS_ON_ICC, null);
         mImsSMSDispatcher = imsSMSDispatcher;
         mUiccController = UiccController.getInstance();
         mUiccController.registerForIccChanged(this, EVENT_ICC_CHANGED, null);
@@ -93,6 +96,10 @@ public final class GsmSMSDispatcher extends SMSDispatcher {
         mCi.unSetOnNewGsmSms(this);
         mCi.unSetOnSmsStatus(this);
         mCi.unSetOnNewGsmBroadcastSms(this);
+        mCi.unSetOnSmsOnSim(this);
+        if (mIccRecords.get() != null) {
+            mIccRecords.get().unregisterForNewSms(this);
+        }
     }
 
     @Override
@@ -136,6 +143,12 @@ public final class GsmSMSDispatcher extends SMSDispatcher {
 
         case EVENT_ICC_CHANGED:
             onUpdateIccAvailability();
+            break;
+
+        case EVENT_SMS_ON_ICC:
+            if (mIccRecords.get() != null) {
+                ((SIMRecords)(mIccRecords.get())).handleSmsOnIcc((AsyncResult) msg.obj);
+            }
             break;
 
         default: