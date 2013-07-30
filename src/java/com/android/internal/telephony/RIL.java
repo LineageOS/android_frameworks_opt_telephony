e9c98ac22bb89d93e37708588ccf473a8b3b8042
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-29 22:15:45 -0500

    SamsungQualcommRIL: rebase Sammyqualcommril to RIL.java 4.3.
    Change-Id: I6a6c8dde58912b4ed814a41644b882e88ece38b1

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3726,7 +3726,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
         }
     }
 
-    private ArrayList<CellInfo> responseCellInfoList(Parcel p) {
+    protected ArrayList<CellInfo> responseCellInfoList(Parcel p) {
         int numberOfInfoRecs;
         ArrayList<CellInfo> response;
 
