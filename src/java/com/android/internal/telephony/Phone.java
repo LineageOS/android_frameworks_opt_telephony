d00975df5654957a2dd1b7be4f959346df9b84fe
Richard LIU <richardl@codeaurora.org>
2013-08-09 17:25:07 +0800

    Add Radio Tech support when manually select PLMN
    Specify radio tech when select PLMN, if RIL reports
    radio tech when search PLMN
    
    Change-Id: I825a0159f7125cce0ea6fc3738a838392fbe8d3a

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -1171,8 +1171,12 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
         nsm.operatorAlphaShort = network.getOperatorAlphaShort();
 
         Message msg = obtainMessage(EVENT_SET_NETWORK_MANUAL_COMPLETE, nsm);
-        mCi.setNetworkSelectionModeManual(network.getOperatorNumeric(), msg);
-
+        if (network.getRadioTech().equals("")) {
+            mCi.setNetworkSelectionModeManual(network.getOperatorNumeric(), msg);
+        } else {
+            mCi.setNetworkSelectionModeManual(network.getOperatorNumeric()
+                    + "+" + network.getRadioTech(), msg);
+        }
         if (persistSelection) {
             updateSavedNetworkOperator(nsm);
         } else {
