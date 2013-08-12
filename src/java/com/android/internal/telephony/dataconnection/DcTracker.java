bb812d2c126ce063fa46bc8b8e77d11a538fb2b1
Peter Pan <peter.pan@sonymobile.com>
2013-08-12 09:40:00 -0400

    Could not enable data connection
    Could not enable the default type APN in below case:
    1. Default apn exists, connect wifi
    2. Delete default apn
    3. Disconnect wifi
    
    enableApnType won't reactivate default apn type when no
    default apn exists. Add default apn criterium to avoid this.
    
    Change-Id: Ic8699669e576e5d2140852f359e34ed6c3275633

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -516,7 +516,8 @@ public class DcTracker extends DcTrackerBase {
     @Override
     public synchronized int enableApnType(String apnType) {
         ApnContext apnContext = mApnContexts.get(apnType);
-        if (apnContext == null || !isApnTypeAvailable(apnType)) {
+        if (apnContext == null || (!TextUtils.equals(apnType, PhoneConstants.APN_TYPE_DEFAULT)
+                && !isApnTypeAvailable(apnType))) {
             if (DBG) log("enableApnType: " + apnType + " is type not available");
             return PhoneConstants.APN_TYPE_NOT_AVAILABLE;
         }
