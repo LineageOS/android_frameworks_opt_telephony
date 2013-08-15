ba19a96830830c4a3d8cfd2435bac407877cdc7f
Shareef Ali <shareefalis@cyanogenmod.org>
2013-08-14 19:03:04 -0500

    Telephony: prevent NPE
    Change-Id: Ia7a08334a901ef67353d5816d0822d0cb2d57daf

--- src/java/com/android/internal/telephony/dataconnection/ApnContext.java
+++ src/java/com/android/internal/telephony/dataconnection/ApnContext.java
@@ -145,6 +145,9 @@ public class ApnContext {
     }
 
     public synchronized ArrayList<ApnSetting> getWaitingApns() {
+        if (mWaitingApns == null) {
+            return new ArrayList<ApnSetting>();
+        }
         return mWaitingApns;
     }
 
