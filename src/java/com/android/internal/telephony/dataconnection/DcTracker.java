63fe4e2b84473bbd61030abd0a4b7c2ba34ce977
Robert Greenwalt <rgreenwalt@google.com>
2013-08-19 15:13:58 -0700

    Fix mvnoMatches function but keep case-insensitive
    Redo previous fix in a way that's still case insensitive.
    
    bug:10171336
    Change-Id: I65e29b72ace2a41ed7a19e3bfb5d23bf3773d41c

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -886,7 +886,9 @@ public final class DcTracker extends DcTrackerBase {
             }
         } else if (mvno_type.equalsIgnoreCase("gid")) {
             String gid1 = r.getGid1();
-            if ((gid1 != null) && gid1.startsWith(mvno_match_data)) {
+            int mvno_match_data_length = mvno_match_data.length();
+            if ((gid1 != null) && (gid1.length() >= mvno_match_data_length) &&
+                    gid1.substring(0, mvno_match_data_length).equalsIgnoreCase(mvno_match_data)) {
                 return true;
             }
         }
