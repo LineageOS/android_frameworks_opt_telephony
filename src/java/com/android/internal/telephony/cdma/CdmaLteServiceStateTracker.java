lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: Fix incorrect status display on UI in airplane mode
UI should display "No Service" instead of "Searching for Service"
in airplane mode.
Changes done to set ERI text to "Searching for Service" only when
radio is on.

CRs-Fixed: 368996
Change-Id: Iee1e6e255090c07b4d931fe7fa4095abe9813df1

src/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -425,7 +425,7 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
         }
 
         if (hasChanged) {
-            if (phone.isEriFileLoaded()) {
+            if ((cm.getRadioState().isOn()) && (phone.isEriFileLoaded())) {
                 String eriText;
                 // Now the CDMAPhone sees the new ServiceState so it can get the
                 // new ERI text

LineageOS/android_frameworks_opt_telephony

src/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -361,20 +361,13 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
         }
 
         if (hasChanged) {
-            if (mPhone.isEriFileLoaded()) {
+            if ((mCi.getRadioState().isOn()) && (mPhone.isEriFileLoaded())) {
                 String eriText;
                 // Now the CDMAPhone sees the new ServiceState so it can get the
                 // new ERI text
                 if (mSS.getVoiceRegState() == ServiceState.STATE_IN_SERVICE ||
                         (mSS.getDataRegState() == ServiceState.STATE_IN_SERVICE)) {
                     eriText = mPhone.getCdmaEriText();
-                } else if (mSS.getVoiceRegState() == ServiceState.STATE_POWER_OFF) {
-                    eriText = (mIccRecords != null) ? mIccRecords.getServiceProviderName() : null;
-                    if (TextUtils.isEmpty(eriText)) {
-                        // Sets operator alpha property by retrieving from
-                        // build-time system property
-                        eriText = SystemProperties.get("ro.cdma.home.operator.alpha");
-                    }
                 } else {
                     // Note that ServiceState.STATE_OUT_OF_SERVICE is valid used
                     // for mRegistrationState 0,2,3 and 4