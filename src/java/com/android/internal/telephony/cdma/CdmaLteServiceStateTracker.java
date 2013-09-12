lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: Fix incorrect status display on UI in airplane mode
UI should display "No Service" instead of "Searching for Service"
in airplane mode.
Changes done to set ERI text to "Searching for Service" only when
radio is on.

CRs-Fixed: 368996
Change-Id: Iee1e6e255090c07b4d931fe7fa4095abe9813df1

src/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -425,7 +425,7 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
         }
 
         if (hasChanged) {
-            if (phone.isEriFileLoaded()) {
+            if ((cm.getRadioState().isOn()) && (phone.isEriFileLoaded())) {
                 String eriText;
                 // Now the CDMAPhone sees the new ServiceState so it can get the
                 // new ERI text

LineageOS/android_frameworks_opt_telephony

Change-Id: Ia60900cb014c4669d05d3cf0a2d2309899440106

src/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -348,7 +348,7 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
         }
 
         if (hasChanged) {
-            if (mPhone.isEriFileLoaded()) {
+            if ((mCi.getRadioState().isOn()) && (mPhone.isEriFileLoaded())) {
                 String eriText;
                 // Now the CDMAPhone sees the new ServiceState so it can get the
                 // new ERI text