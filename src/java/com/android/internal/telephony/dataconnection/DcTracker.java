167cbd4a3dd13ad51a4d4ccc7f202a413ec6ce84
Ross Squires <ross.squires@gmail.com>
2013-09-18 15:36:57 -0400

    APN config now loads for SIM-less CDMA phones
    Conflicts:
    	src/java/com/android/internal/telephony/cdma/CDMAPhone.java
    	src/java/com/android/internal/telephony/dataconnection/DcTracker.java
    
    Change-Id: Iec26d6f3998f33cac0f14f7b18bdd28d35189262

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2227,6 +2227,18 @@ public class DcTracker extends DcTrackerBase {
     private void createAllApnList() {
         mAllDps.clear();
         String operator = getOperatorNumeric();
+
+        mAllApnSettings = new ArrayList<ApnSetting>();
+        IccRecords r = mIccRecords.get();
+        String operator = (r != null) ? r.getOperatorNumeric() : "";
+
+        if (mCdmaSsm.getCdmaSubscriptionSource() ==
+                CdmaSubscriptionSourceManager.SUBSCRIPTION_FROM_NV) {
+            operator = SystemProperties.get(
+                                CDMAPhone.PROPERTY_CDMA_HOME_OPERATOR_NUMERIC,
+                                mPhone.getServiceStateTracker().getOperatorNumeric());
+        }
+
         if (operator != null) {
             String selection = "numeric = '" + operator + "'";
             // query only enabled apn.
@@ -2678,6 +2690,9 @@ public class DcTracker extends DcTrackerBase {
             }
             log("onUpdateIcc: tryRestartDataConnections " + Phone.REASON_NW_TYPE_CHANGED);
             tryRestartDataConnections(Phone.REASON_NW_TYPE_CHANGED);
+        } else if (mCdmaSsm.getCdmaSubscriptionSource() ==
+                CdmaSubscriptionSourceManager.SUBSCRIPTION_FROM_NV) {
+            onRecordsLoaded(Phone.REASON_NV_READY);
         }
     }
 
