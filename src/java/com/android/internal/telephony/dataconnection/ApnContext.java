e807dfc60a4fa89f0e6325d1095d9230a843c1f5
Shareef Ali <shareefalis@cyanogenmod.org>
2013-10-06 03:03:10 -0400

    Telephony: check if the provisioning apn is actually empty before
    checking if they are equal. fixes cases where the stubbed apn
    (empty) equal to the the empty provisioning apn.
    
    Change-Id: I6ca4fd172e2ecc64ca7a2c3fbca5327be61e7b5c

--- src/java/com/android/internal/telephony/dataconnection/ApnContext.java
+++ src/java/com/android/internal/telephony/dataconnection/ApnContext.java
@@ -19,6 +19,7 @@ package com.android.internal.telephony.dataconnection;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.telephony.Rlog;
+import android.text.TextUtils;
 
 import com.android.internal.R;
 import com.android.internal.telephony.DctConstants;
@@ -243,6 +244,9 @@ public class ApnContext {
     public boolean isProvisioningApn() {
         String provisioningApn = mContext.getResources()
                 .getString(R.string.mobile_provisioning_apn);
+        if (TextUtils.isEmpty(provisioningApn)) {
+            return false;
+        }
         if ((mDataProfile != null) && (mDataProfile.apn != null)) {
             return (mDataProfile.apn.equals(provisioningApn));
         } else {
