d62764715ceb7f1dd5d49e4b4667964743546e8b
Shareef Ali <shareefalis@cyanogenmod.org>
2013-10-09 03:36:19 -0400

    Revert "Telephony: DcTracker: Fix CDMA APN Data issues."
    This reverts commit cd2585eb3f4e2db5c8a36382ffa644fa0d1515b4.
    
    Conflicts:
    	src/java/com/android/internal/telephony/dataconnection/DcTracker.java
    
    Change-Id: Ic13235744fca561cff4d49ae5c799420c30b3840

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -753,7 +753,6 @@ public class DcTracker extends DcTrackerBase {
     }
 
     private boolean trySetupData(ApnContext apnContext) {
-        boolean retValue = false;
         if (DBG) {
             log("trySetupData for type:" + apnContext.getDataProfileType() +
                     " due to " + apnContext.getReason() + " apnContext=" + apnContext);
@@ -803,12 +802,8 @@ public class DcTracker extends DcTrackerBase {
                 if (waitingDps.isEmpty()) {
                     notifyNoData(DcFailCause.MISSING_UNKNOWN_APN, apnContext);
                     notifyOffApnsOfAvailability(apnContext.getReason());
-                    retValue = setupData(apnContext);
-                    if(!retValue) {
-                        notifyNoData(DcFailCause.MISSING_UNKNOWN_APN, apnContext);
-                    }
-                    notifyOffApnsOfAvailability(apnContext.getReason());
-                    return retValue;
+                    if (DBG) log("trySetupData: X No APN found retValue=false");
+                    return false;
                 } else {
                     apnContext.setWaitingDataProfiles(waitingDps);
                     if (DBG) {
@@ -822,7 +817,7 @@ public class DcTracker extends DcTrackerBase {
                 log("trySetupData: call setupData, waitingApns : "
                         + apnListToString(apnContext.getWaitingApns()));
             }
-            retValue = setupData(apnContext);
+            boolean retValue = setupData(apnContext);
             notifyOffApnsOfAvailability(apnContext.getReason());
 
             if (DBG) log("trySetupData: X retValue=" + retValue);
@@ -1203,29 +1198,8 @@ public class DcTracker extends DcTrackerBase {
         int profileId = getApnProfileID(apnContext.getDataProfileType());
         apnSetting = apnContext.getNextWaitingApn();
         if (apnSetting == null) {
-            if(PhoneConstants.PHONE_TYPE_CDMA==mPhone.getPhoneType()) {
-                String[] mDunApnTypes = { PhoneConstants.APN_TYPE_DUN };
-                final int mDefaultApnId = DctConstants.APN_DEFAULT_ID;
-                final String[] mDefaultApnTypes = {
-                    PhoneConstants.APN_TYPE_DEFAULT,
-                    PhoneConstants.APN_TYPE_MMS,
-                    PhoneConstants.APN_TYPE_HIPRI };
-                String[] types;
-                int apnId;
-                if (mRequestedApnType.equals(PhoneConstants.APN_TYPE_DUN)) {
-                    types = mDunApnTypes;
-                    apnId = DctConstants.APN_DUN_ID;
-                } else {
-                    types = mDefaultApnTypes;
-                    apnId = mDefaultApnId;
-                }
-                apnSetting = new ApnSetting(apnId, "", "", "", "", "", "", "", "", "",
-                                            "", 0, types, "IP", "IP", true, 0);
-                if (DBG) log("setupData: CDMA detected and apnSetting == null, use stubbed CDMA APN setting= " + apnSetting);
-            } else {
-                if (DBG) log("setupData: return for no apn found!");
-                return false;
-            }
+            if (DBG) log("setupData: return for no apn found!");
+            return false;
         }
 
         dcac = checkForCompatibleConnectedApnContext(apnContext);
