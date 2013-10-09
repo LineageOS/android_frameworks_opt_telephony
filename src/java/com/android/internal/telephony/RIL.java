a38e11089b4c7205717f30a6e19643147ebf6a70
Steve Kondik <shade@chemlab.org>
2013-10-09 00:35:58 -0700

    ril: Add support for opcode translation
     * Various command opcodes have changed in 4.3 due to the addition of
       new commands.  Add support for rewriting the opcodes.
    
    Change-Id: I2a0c7dbcbaf873bd9fa4dc0447fe7ce1a472756a

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -684,6 +684,25 @@ public class RIL extends BaseCommands implements CommandsInterface {
         }
     }
 
+
+    /**
+     * Translate a request/response id coming in from native side.
+     *
+     * @hide
+     */
+    protected int translateIn(int id) {
+        return id;
+    }
+
+    /**
+     * Translate a request/response id before sending to native side.
+     *
+     * @hide
+     */
+    protected int translateOut(int id) {
+        return id;
+
+    }
     //***** CommandsInterface implementation
 
     @Override
@@ -2460,6 +2479,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
             return;
         }
 
+        rr.mRequest = translateOut(rr.mRequest);
+
         msg = mSender.obtainMessage(EVENT_SEND, rr);
 
         acquireWakeLock();
@@ -2470,8 +2491,15 @@ public class RIL extends BaseCommands implements CommandsInterface {
     protected void
     processResponse (Parcel p) {
         int type;
+        int pos = 0;
 
         type = p.readInt();
+        pos = p.dataPosition();
+
+        int req = translateIn(p.readInt());
+        p.setDataPosition(pos);
+        p.writeInt(req);
+        p.setDataPosition(pos);
 
         if (type == RESPONSE_UNSOLICITED) {
             processUnsolicited (p);
