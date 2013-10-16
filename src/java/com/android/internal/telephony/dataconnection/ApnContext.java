2e02c842665e2b231a9e4d7fd867ece725ccab7c
Shareef Ali <shareefalis@cyanogenmod.org>
2013-10-16 01:05:03 -0400

    refix same npe, lost because of the qcril merge
    Change-Id: I5e11f8cdd14c428e32f77e410383ca732e7da19d

--- src/java/com/android/internal/telephony/dataconnection/ApnContext.java
+++ src/java/com/android/internal/telephony/dataconnection/ApnContext.java
@@ -149,6 +149,9 @@ public class ApnContext {
     }
 
     public synchronized ArrayList<DataProfile> getWaitingApns() {
+        if (mWaitingDataProfiles == null) {
+            return new ArrayList<DataProfile>();
+        }
         return mWaitingDataProfiles;
     }
 
