bf9174b5c8d20146063ae03c9fc27ef763adce15
Susheel nyamala <snyamala@codeaurora.org>
2013-10-22 16:16:39 +0530

    Update markings for the OMH changes
    Update markings for the OMH gerrit:478433
    
    Change-Id: Ifa945e4df2fe95e239df1104ffa5590c00ebb295
    (cherry picked from commit 58d161e5b2242fca08be068c45c5e915cae38b18)
    (cherry picked from commit 942d37b7c9f7d4fdcd052535970ba732b77e1aba)
    (cherry picked from commit 7d794f79fa1d87ba7e95d7892981f88f9398a1c9)

--- src/java/com/android/internal/telephony/dataconnection/DcAsyncChannel.java
+++ src/java/com/android/internal/telephony/dataconnection/DcAsyncChannel.java
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (c) 2010-2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
