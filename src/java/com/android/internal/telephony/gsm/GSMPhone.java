gp-b2g/frameworks_base

Telephony:Prevent duplicate auto mode request on EF_CSP read
Prevent duplicate auto network selection mode request
if triggered internally by Android due to
PLMN disabled in EF_CSP SIM record.
Allow mode change request if triggered by user.

Change-Id: Ib3f4d807772d33429f8e1d899a12b2684afc52a2
CRs-Fixed: 301052

telephony/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1295,8 +1295,15 @@ public class GSMPhone extends PhoneBase {
                 break;
 
             case EVENT_SET_NETWORK_AUTOMATIC:
-                ar = (AsyncResult)msg.obj;
-                setNetworkSelectionModeAutomatic((Message)ar.result);
+                // Automatic network selection from EF_CSP SIM record
+                ar = (AsyncResult) msg.obj;
+                if (mSST.ss.getIsManualSelection()) {
+                    setNetworkSelectionModeAutomatic((Message) ar.result);
+                } else {
+                    // prevent duplicate request which will push current PLMN to
+                    // low priority
+                    Log.d(LOG_TAG, "Stop duplicate SET_NETWORK_SELECTION_AUTOMATIC to Ril ");
+                }
                 break;
 
             case EVENT_ICC_RECORD_EVENTS:

LineageOS/android_frameworks_opt_telephony

Change-Id: I9f6828e59abada64fab66d6ff747bf83a3ec4d6c
(cherry picked from commit d13254b8d1b238f5e2f417da8839d5120952898d)
(cherry picked from commit db52d32cc814ee8081de8838bf57936bbcf03a44)
(cherry picked from commit eee13b4a953a8b2587f7181160a6db8ad36ffad4)

src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1367,8 +1367,15 @@ public class GSMPhone extends PhoneBase {
                 break;
 
             case EVENT_SET_NETWORK_AUTOMATIC:
-                ar = (AsyncResult)msg.obj;
-                setNetworkSelectionModeAutomatic((Message)ar.result);
+                // Automatic network selection from EF_CSP SIM record
+                ar = (AsyncResult) msg.obj;
+                if (mSST.mSS.getIsManualSelection()) {
+                    setNetworkSelectionModeAutomatic((Message) ar.result);
+                } else {
+                    // prevent duplicate request which will push current PLMN to
+                    // low priority
+                    Rlog.d(LOG_TAG, "Stop duplicate SET_NETWORK_SELECTION_AUTOMATIC to Ril ");
+                }
                 break;
 
             case EVENT_ICC_RECORD_EVENTS: