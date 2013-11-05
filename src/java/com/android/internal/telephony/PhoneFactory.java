9d286c8311c167783ad9640a08d35f938bc3295a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-05 18:43:43 +0000

    Revert "Initialize RIL with the correct CDMA subscription mode setting"
    This reverts commit 8bf373e05d72cac5b80a6ffcc18e6234430b38cd.
    
    Conflicts:
    	src/java/com/android/internal/telephony/PhoneFactory.java

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -113,11 +113,7 @@ public class PhoneFactory {
                         Settings.Global.PREFERRED_NETWORK_MODE, preferredNetworkMode);
                 Rlog.i(LOG_TAG, "Network Mode set to " + Integer.toString(networkMode));
 
-                // Get cdmaSubscription mode from Settings.Global
-                int cdmaSubscription;
-                cdmaSubscription = Settings.Global.getInt(context.getContentResolver(),
-                                Settings.Global.CDMA_SUBSCRIPTION_MODE,
-                                sPreferredCdmaSubscription);
+                int cdmaSubscription = CdmaSubscriptionSourceManager.getDefault(context);
                 Rlog.i(LOG_TAG, "Cdma Subscription set to " + cdmaSubscription);
 
                 //reads the system properties and makes commandsinterface
