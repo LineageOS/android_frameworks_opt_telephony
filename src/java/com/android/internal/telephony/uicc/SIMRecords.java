ff84dc69d585854fc639cddf3bb92ce2e0f926cd
luxiaol <luxiaol@codeaurora.org>
2013-11-14 16:36:54 +0800

    Telephony: update property in IccCardProxy
    Update property in IccCardProxy, not update them in SIMRecords and RuimRecords.
    
    Multimode card will load multi sim records, the later one will override the
    property set before, so better to set properties based on active app type.
    Or else in some network mode it will get wrong property.
    
    Change-Id: Id4337de67dfcb11c3af29ff95bcf1da8fd48d98e

--- src/java/com/android/internal/telephony/uicc/SIMRecords.java
+++ src/java/com/android/internal/telephony/uicc/SIMRecords.java
@@ -1325,23 +1325,6 @@ public class SIMRecords extends IccRecords {
         // Some fields require more than one SIM record to set
 
         String operator = getOperatorNumeric();
-        if (!TextUtils.isEmpty(operator)) {
-            log("onAllRecordsLoaded set 'gsm.sim.operator.numeric' to operator='" +
-                    operator + "'");
-            setSystemProperty(PROPERTY_ICC_OPERATOR_NUMERIC, operator);
-            setSystemProperty(PROPERTY_APN_SIM_OPERATOR_NUMERIC, operator);
-        } else {
-            log("onAllRecordsLoaded empty 'gsm.sim.operator.numeric' skipping");
-        }
-
-        if (!TextUtils.isEmpty(mImsi)) {
-            log("onAllRecordsLoaded set mcc imsi=" + mImsi);
-            setSystemProperty(PROPERTY_ICC_OPERATOR_ISO_COUNTRY,
-                    MccTable.countryCodeForMcc(Integer.parseInt(mImsi.substring(0,3))));
-        } else {
-            log("onAllRecordsLoaded empty imsi skipping setting mcc");
-        }
-
         setVoiceMailByCountry(operator);
         setSpnFromConfig(operator);
 
