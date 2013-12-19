ad87d6244fe628feee77931258c75585b10527b2
Jon Haus <haus.xda@gmail.com>
2013-12-18 21:32:57 -0500

    PhoneFactory: fall back to default RIL if custom RIL Class fails
    If the ro.telephony.ril_class property is set to an invalid class it will cause
    Phone to repeatedly FC and make the device unuseable. This allows it to handle the
    exceptions gracefully and fall back on the default RIL.
    
    Change-Id: Ic9c309aff173de5267aa7ef633bfff87243f24fd

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -117,19 +117,18 @@ public class PhoneFactory {
                 Rlog.i(LOG_TAG, "Cdma Subscription set to " + cdmaSubscription);
 
                 //reads the system properties and makes commandsinterface
-                String sRILClassname = SystemProperties.get("ro.telephony.ril_class", "RIL");
+                String sRILClassname = SystemProperties.get("ro.telephony.ril_class", "RIL").trim();
                 Rlog.i(LOG_TAG, "RILClassname is " + sRILClassname);
 
                 // Use reflection to construct the RIL class (defaults to RIL)
                 try {
-                    Class<?> classDefinition = Class.forName("com.android.internal.telephony." + sRILClassname);
-                    Constructor<?> constructor = classDefinition.getConstructor(new Class[] {Context.class, int.class, int.class});
-                    sCommandsInterface = (RIL) constructor.newInstance(new Object[] {context, networkMode, cdmaSubscription});
+                    sCommandsInterface = instantiateCustomRIL(
+                                            sRILClassname, context, networkMode, cdmaSubscription);
                 } catch (Exception e) {
                     // 6 different types of exceptions are thrown here that it's
                     // easier to just catch Exception as our "error handling" is the same.
-                    Rlog.i(LOG_TAG, "Unable to construct command interface", e);
-                    throw new RuntimeException(e);
+                    Rlog.e(LOG_TAG, "Unable to construct custom RIL class", e);
+                    sCommandsInterface = new RIL(context, networkMode, cdmaSubscription);
                 }
 
                 // Instantiate UiccController so that all other classes can just call getInstance()
@@ -172,6 +171,14 @@ public class PhoneFactory {
                 sMadeDefaults = true;
             }
         }
+    }
+
+    private static <T> T instantiateCustomRIL(
+                      String sRILClassname, Context context, int networkMode, int cdmaSubscription)
+                      throws Exception {
+        Class<?> clazz = Class.forName("com.android.internal.telephony." + sRILClassname);
+        Constructor<?> constructor = clazz.getConstructor(Context.class, int.class, int.class);
+        return (T) clazz.cast(constructor.newInstance(context, networkMode, cdmaSubscription));
     }
 
     public static Phone getDefaultPhone() {
