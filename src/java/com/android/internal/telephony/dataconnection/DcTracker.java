ac0890fe7d97a7b821dc2faf13235065d9e5c707
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-14 18:41:03 -0600

    DCTracker: if cdma sim is not avaliable, load from predefined property from build.prop
    * fixes legacy cdma's MMS
    
    Change-Id: Iaad556f46bf8d35483f1c44c054945511d8c4234

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2025,7 +2025,11 @@ public final class DcTracker extends DcTrackerBase {
     private void createAllApnList() {
         mAllApnSettings = new ArrayList<ApnSetting>();
         IccRecords r = mIccRecords.get();
-        String operator = (r != null) ? r.getOperatorNumeric() : "";
+        String homeOperator = "";
+        if (isNvSubscription()){
+            homeOperator = SystemProperties.get("ro.cdma.home.operator.numeric");
+        }
+        String operator = (r != null) ? r.getOperatorNumeric() : homeOperator;
         if (operator != null) {
             String selection = "numeric = '" + operator + "'";
             // query only enabled apn.
@@ -2110,7 +2114,11 @@ public final class DcTracker extends DcTrackerBase {
         }
 
         IccRecords r = mIccRecords.get();
-        String operator = (r != null) ? r.getOperatorNumeric() : "";
+        String homeOperator = "";
+        if (isNvSubscription()){
+            homeOperator = SystemProperties.get("ro.cdma.home.operator.numeric");
+        }
+        String operator = (r != null) ? r.getOperatorNumeric() : homeOperator;
 
         // This is a workaround for a bug (7305641) where we don't failover to other
         // suitable APNs if our preferred APN fails.  On prepaid ATT sims we need to
