5ccf4a49a0a1c5773e5715661db6b7fcf5f9039b
Jake Hamby <jhamby@google.com>
2014-02-06 14:47:14 -0800

    Remove unneeded new RIL command.
    Remove the recently added RIL_REQUEST_SET_RADIO_MODE command and update
    the definition of the RIL_REQUEST_NV_RESET_CONFIG parameter.
    Also, space out the NV item enum values so we can add new items per category.
    
    Bug: 12864208
    Change-Id: I1201c92198a8cd7e169efe7055b58259d975309b

--- src/java/com/android/internal/telephony/RadioNVItems.java
+++ src/java/com/android/internal/telephony/RadioNVItems.java
@@ -25,57 +25,56 @@ package com.android.internal.telephony;
  */
 public interface RadioNVItems {
 
-    // CDMA radio information
-    int RIL_NV_CDMA_MEID = 1;                   // hex MEID
-    int RIL_NV_CDMA_PRL_VERSION = 2;            // CDMA PRL version
-
-    // CDMA mobile account information
+    // CDMA radio and account information (items 1-10)
+    int RIL_NV_CDMA_MEID = 1;                   // CDMA MEID (hex)
+    int RIL_NV_CDMA_MIN = 2;                    // CDMA MIN (MSID)
     int RIL_NV_CDMA_MDN = 3;                    // CDMA MDN
-    int RIL_NV_CDMA_MIN = 4;                    // CDMA MIN (MSID)
+    int RIL_NV_CDMA_ACCOLC = 4;                 // CDMA access overload control
 
-    // Carrier device provisioning
-    int RIL_NV_DEVICE_MSL = 5;                  // device MSL
-    int RIL_NV_RTN_RECONDITIONED_STATUS = 6;    // RTN reconditioned status
-    int RIL_NV_RTN_ACTIVATION_DATE = 7;         // RTN activation date
-    int RIL_NV_RTN_LIFE_TIMER = 8;              // RTN life timer
-    int RIL_NV_RTN_LIFE_CALLS = 9;              // RTN life calls
-    int RIL_NV_RTN_LIFE_DATA_TX = 10;           // RTN life data TX
-    int RIL_NV_RTN_LIFE_DATA_RX = 11;           // RTN life data RX
-    int RIL_NV_OMADM_HFA_LEVEL = 12;            // HFA in progress
+    // Carrier device provisioning (items 11-30)
+    int RIL_NV_DEVICE_MSL = 11;                 // device MSL
+    int RIL_NV_RTN_RECONDITIONED_STATUS = 12;   // RTN reconditioned status
+    int RIL_NV_RTN_ACTIVATION_DATE = 13;        // RTN activation date
+    int RIL_NV_RTN_LIFE_TIMER = 14;             // RTN life timer
+    int RIL_NV_RTN_LIFE_CALLS = 15;             // RTN life calls
+    int RIL_NV_RTN_LIFE_DATA_TX = 16;           // RTN life data TX
+    int RIL_NV_RTN_LIFE_DATA_RX = 17;           // RTN life data RX
+    int RIL_NV_OMADM_HFA_LEVEL = 18;            // HFA in progress
 
-    // Mobile IP profile information
-    int RIL_NV_MIP_PROFILE_NAI = 13;            // NAI realm
-    int RIL_NV_MIP_PROFILE_HOME_ADDRESS = 14;   // MIP home address
-    int RIL_NV_MIP_PROFILE_AAA_AUTH = 15;       // AAA auth
-    int RIL_NV_MIP_PROFILE_HA_AUTH = 16;        // HA auth
-    int RIL_NV_MIP_PROFILE_PRI_HA_ADDR = 17;    // primary HA address
-    int RIL_NV_MIP_PROFILE_SEC_HA_ADDR = 18;    // secondary HA address
-    int RIL_NV_MIP_PROFILE_REV_TUN_PREF = 19;   // reverse TUN pref
-    int RIL_NV_MIP_PROFILE_HA_SPI = 20;         // HA SPI
-    int RIL_NV_MIP_PROFILE_AAA_SPI = 21;        // AAA SPI
-    int RIL_NV_MIP_PROFILE_MN_HA_SS = 22;       // HA shared secret
-    int RIL_NV_MIP_PROFILE_MN_AAA_SS = 23;      // AAA shared secret
+    // Mobile IP profile information (items 31-50)
+    int RIL_NV_MIP_PROFILE_NAI = 31;            // NAI realm
+    int RIL_NV_MIP_PROFILE_HOME_ADDRESS = 32;   // MIP home address
+    int RIL_NV_MIP_PROFILE_AAA_AUTH = 33;       // AAA auth
+    int RIL_NV_MIP_PROFILE_HA_AUTH = 34;        // HA auth
+    int RIL_NV_MIP_PROFILE_PRI_HA_ADDR = 35;    // primary HA address
+    int RIL_NV_MIP_PROFILE_SEC_HA_ADDR = 36;    // secondary HA address
+    int RIL_NV_MIP_PROFILE_REV_TUN_PREF = 37;   // reverse TUN pref
+    int RIL_NV_MIP_PROFILE_HA_SPI = 38;         // HA SPI
+    int RIL_NV_MIP_PROFILE_AAA_SPI = 39;        // AAA SPI
+    int RIL_NV_MIP_PROFILE_MN_HA_SS = 40;       // HA shared secret
+    int RIL_NV_MIP_PROFILE_MN_AAA_SS = 41;      // AAA shared secret
 
-    // CDMA network and band config
-    int RIL_NV_CDMA_BC10 = 24;                  // CDMA band class 10
-    int RIL_NV_CDMA_BC14 = 25;                  // CDMA band class 14
-    int RIL_NV_CDMA_SO68 = 26;                  // CDMA SO68
-    int RIL_NV_CDMA_SO73_COP0 = 27;             // CDMA SO73 COP0
-    int RIL_NV_CDMA_SO73_COP1TO7 = 28;          // CDMA SO73 COP1-7
-    int RIL_NV_CDMA_1X_ADVANCED_ENABLED = 29;   // CDMA 1X Advanced enabled
-    int RIL_NV_CDMA_EHRPD_ENABLED = 30;         // CDMA eHRPD enabled
-    int RIL_NV_CDMA_EHRPD_FORCED = 31;          // CDMA eHRPD forced
+    // CDMA network and band config (items 51-70)
+    int RIL_NV_CDMA_PRL_VERSION = 51;           // CDMA PRL version
+    int RIL_NV_CDMA_BC10 = 52;                  // CDMA band class 10
+    int RIL_NV_CDMA_BC14 = 53;                  // CDMA band class 14
+    int RIL_NV_CDMA_SO68 = 54;                  // CDMA SO68
+    int RIL_NV_CDMA_SO73_COP0 = 55;             // CDMA SO73 COP0
+    int RIL_NV_CDMA_SO73_COP1TO7 = 56;          // CDMA SO73 COP1-7
+    int RIL_NV_CDMA_1X_ADVANCED_ENABLED = 57;   // CDMA 1X Advanced enabled
+    int RIL_NV_CDMA_EHRPD_ENABLED = 58;         // CDMA eHRPD enabled
+    int RIL_NV_CDMA_EHRPD_FORCED = 59;          // CDMA eHRPD forced
 
-    // LTE network and band config
-    int RIL_NV_LTE_BAND_ENABLE_25 = 32;         // LTE band 25 enable
-    int RIL_NV_LTE_BAND_ENABLE_26 = 33;         // LTE band 26 enable
-    int RIL_NV_LTE_BAND_ENABLE_41 = 34;         // LTE band 41 enable
+    // LTE network and band config (items 71-90)
+    int RIL_NV_LTE_BAND_ENABLE_25 = 71;         // LTE band 25 enable
+    int RIL_NV_LTE_BAND_ENABLE_26 = 72;         // LTE band 26 enable
+    int RIL_NV_LTE_BAND_ENABLE_41 = 73;         // LTE band 41 enable
 
-    int RIL_NV_LTE_SCAN_PRIORITY_25 = 35;       // LTE band 25 scan priority
-    int RIL_NV_LTE_SCAN_PRIORITY_26 = 36;       // LTE band 26 scan priority
-    int RIL_NV_LTE_SCAN_PRIORITY_41 = 37;       // LTE band 41 scan priority
+    int RIL_NV_LTE_SCAN_PRIORITY_25 = 74;       // LTE band 25 scan priority
+    int RIL_NV_LTE_SCAN_PRIORITY_26 = 75;       // LTE band 26 scan priority
+    int RIL_NV_LTE_SCAN_PRIORITY_41 = 76;       // LTE band 41 scan priority
 
-    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_25 = 38;    // LTE hidden band 25 priority
-    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_26 = 39;    // LTE hidden band 26 priority
-    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_41 = 40;    // LTE hidden band 41 priority
+    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_25 = 77;    // LTE hidden band 25 priority
+    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_26 = 78;    // LTE hidden band 26 priority
+    int RIL_NV_LTE_HIDDEN_BAND_PRIORITY_41 = 79;    // LTE hidden band 41 priority
 }
