d5349d8c9f2ce4375e4549903694d777ee3b066e
Shriram Ganesh <sganesh@codeaurora.org>
2014-02-05 19:17:49 -0800

    Add handover information as extra in Call Details
    1. Add handover information for a call as extra key with a
    value for LTE to IWLAN failure.
    
    Change-Id: I71428db8849a74047694898f66f538b9ee4190f0
    CRs-Fixed: 601334

--- src/java/com/android/internal/telephony/CallDetails.java
+++ src/java/com/android/internal/telephony/CallDetails.java
@@ -137,6 +137,8 @@ public class CallDetails {
 
     public static final String EXTRAS_IS_CONFERENCE_URI = "isConferenceUri";
     public static final String EXTRAS_PARENT_CALL_ID = "parentCallId";
+    public static final String EXTRAS_HANDOVER_INFORMATION = "handoverInfo";
+    public static final int EXTRA_TYPE_LTE_TO_IWLAN_HO_FAIL = 1;
 
     public int call_type;
     public int call_domain;
