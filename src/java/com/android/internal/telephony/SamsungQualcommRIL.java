d84585fe200467fd859b49a0acb2a787df16c75a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-15 02:15:24 +0000

    SamsungQualcomm: Add support for Samsung's EMS dialing request
    At least some samsungs require a specific emergency dial. Considering
    past history, this is probably all the devices...
    
    Change-Id: Id43728942f2d964a84f7486b1269d2da8305fdf6

--- src/java/com/android/internal/telephony/SamsungQualcommRIL.java
+++ src/java/com/android/internal/telephony/SamsungQualcommRIL.java
@@ -730,6 +730,10 @@ public class SamsungQualcommRIL extends RIL implements CommandsInterface {
     @Override
     public void
     dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
+        if (PhoneNumberUtils.isEmergencyNumber(address)) {
+            dialEmergencyCall(address, clirMode, result);
+            return;
+        }
         if(!dialCode){
             super.dial(address, clirMode, uusInfo, result);
             return;
@@ -801,4 +805,21 @@ public class SamsungQualcommRIL extends RIL implements CommandsInterface {
             }
         }
     }
+
+    static final int RIL_REQUEST_DIAL_EMERGENCY = 10016;
+    public void
+    dialEmergencyCall(String address, int clirMode, Message result) {
+        RILRequest rr;
+        Rlog.v(RILJ_LOG_TAG, "Emergency dial: " + address);
+
+        rr = RILRequest.obtain(RIL_REQUEST_DIAL_EMERGENCY, result);
+        rr.mParcel.writeString(address + "/");
+        rr.mParcel.writeInt(clirMode);
+        rr.mParcel.writeInt(0);  // UUS information is absent
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+
+        send(rr);
+    }
+
 }
