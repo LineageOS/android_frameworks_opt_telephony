6d68ebc00398b3c62604139998ac7d9bc7da28af
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-03-24 20:20:06 +0000

    DcTracker: Don't leave CMD_IS_PROVISIONING_APN dangling
    Change I99344d89235b24689cde14335bf593099169ef0d exited the message
    handler without a reply to the caller, leaving the singlethread queue
    waiting for a reply and blocking other async tasks.
    
    Change-Id: If12361c0037e9ca0662d720acfe55215924b67fd

--- src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -951,6 +951,8 @@ public abstract class DcTrackerBase extends Handler {
                 ApnContext apnContext = mApnContexts.get(PhoneConstants.APN_TYPE_DEFAULT);
                 if (apnContext.getState() == State.DISCONNECTING){
                       log("CMD_IS_PROVISIONING_APN: APN default is DISCONNECTING");
+                      mReplyAc.replyToMessage(msg, DctConstants.CMD_IS_PROVISIONING_APN,
+                              DctConstants.DISABLED);
                       break;
                 }
 
