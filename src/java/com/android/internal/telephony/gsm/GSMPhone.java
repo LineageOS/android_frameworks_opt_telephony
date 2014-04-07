f61aaae6608243be4e0da97a56ff9fed488cb173
Shishir Agrawal <shishir@google.com>
2014-04-07 08:57:55 -0700

    Reset network selection mode to "automatic" for CDMA phones.
    If a phone switches from a GSM phone to a CDMA phone, the network selection
    mode is not changed. Given that CDMA phones do not allow a "manual" network
    selection mode, this causes issues. This CL set the mode to "automatic"
    on every CDMA NV ready and RUIM ready events.
    
    Change-Id: I0166ecfe2abecf26270ae5330ae32af5b09ca57e

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -1003,54 +1003,6 @@ public class GSMPhone extends PhoneBase {
         mCi.getAvailableNetworks(response);
     }
 
-    /**
-     * Small container class used to hold information relevant to
-     * the carrier selection process. operatorNumeric can be ""
-     * if we are looking for automatic selection. operatorAlphaLong is the
-     * corresponding operator name.
-     */
-    private static class NetworkSelectMessage {
-        public Message message;
-        public String operatorNumeric;
-        public String operatorAlphaLong;
-    }
-
-    @Override
-    public void
-    setNetworkSelectionModeAutomatic(Message response) {
-        // wrap the response message in our own message along with
-        // an empty string (to indicate automatic selection) for the
-        // operator's id.
-        NetworkSelectMessage nsm = new NetworkSelectMessage();
-        nsm.message = response;
-        nsm.operatorNumeric = "";
-        nsm.operatorAlphaLong = "";
-
-        // get the message
-        Message msg = obtainMessage(EVENT_SET_NETWORK_AUTOMATIC_COMPLETE, nsm);
-        if (LOCAL_DEBUG)
-            Rlog.d(LOG_TAG, "wrapping and sending message to connect automatically");
-
-        mCi.setNetworkSelectionModeAutomatic(msg);
-    }
-
-    @Override
-    public void
-    selectNetworkManually(OperatorInfo network,
-            Message response) {
-        // wrap the response message in our own message along with
-        // the operator's id.
-        NetworkSelectMessage nsm = new NetworkSelectMessage();
-        nsm.message = response;
-        nsm.operatorNumeric = network.getOperatorNumeric();
-        nsm.operatorAlphaLong = network.getOperatorAlphaLong();
-
-        // get the message
-        Message msg = obtainMessage(EVENT_SET_NETWORK_MANUAL_COMPLETE, nsm);
-
-        mCi.setNetworkSelectionModeManual(network.getOperatorNumeric(), msg);
-    }
-
     @Override
     public void
     getNeighboringCids(Message response) {
@@ -1341,12 +1293,6 @@ public class GSMPhone extends PhoneBase {
                 processIccRecordEvents((Integer)ar.result);
                 break;
 
-            // handle the select network completion callbacks.
-            case EVENT_SET_NETWORK_MANUAL_COMPLETE:
-            case EVENT_SET_NETWORK_AUTOMATIC_COMPLETE:
-                handleSetSelectNetwork((AsyncResult) msg.obj);
-                break;
-
             case EVENT_SET_CLIR_COMPLETE:
                 ar = (AsyncResult)msg.obj;
                 if (ar.exception == null) {
@@ -1424,41 +1370,6 @@ public class GSMPhone extends PhoneBase {
             }
         }
         return false;
-    }
-
-    /**
-     * Used to track the settings upon completion of the network change.
-     */
-    private void handleSetSelectNetwork(AsyncResult ar) {
-        // look for our wrapper within the asyncresult, skip the rest if it
-        // is null.
-        if (!(ar.userObj instanceof NetworkSelectMessage)) {
-            if (LOCAL_DEBUG) Rlog.d(LOG_TAG, "unexpected result from user object.");
-            return;
-        }
-
-        NetworkSelectMessage nsm = (NetworkSelectMessage) ar.userObj;
-
-        // found the object, now we send off the message we had originally
-        // attached to the request.
-        if (nsm.message != null) {
-            if (LOCAL_DEBUG) Rlog.d(LOG_TAG, "sending original message to recipient");
-            AsyncResult.forMessage(nsm.message, ar.result, ar.exception);
-            nsm.message.sendToTarget();
-        }
-
-        // open the shared preferences editor, and write the value.
-        // nsm.operatorNumeric is "" if we're in automatic.selection.
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString(NETWORK_SELECTION_KEY, nsm.operatorNumeric);
-        editor.putString(NETWORK_SELECTION_NAME_KEY, nsm.operatorAlphaLong);
-
-        // commit and log the result.
-        if (! editor.commit()) {
-            Rlog.e(LOG_TAG, "failed to commit network selection preference");
-        }
-
     }
 
     /**
