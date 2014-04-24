a71dd2ba49c57930d313554e49d55d69c32d10cd
Suchand Ghosh <suchan@codeaurora.org>
2014-04-24 14:33:34 +0530

    Fix voicemail count issue.
    Telephony checks voicemail count stored
    in persist memory if SIM record returns
    count zero.
    
    Add code not to check voicemail count stored
    in persist phone memory if card read success
    and valid count is returned from card.
    
    Change-Id: I69ea6e5087dd49a550bbb4b6f6721729e8cd7f15
    CRs-Fixed: 655038

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -317,7 +317,8 @@ public class GSMPhone extends PhoneBase {
             // get voice mail count from SIM
             countVoiceMessages = r.getVoiceMessageCount();
         }
-        if (countVoiceMessages == 0) {
+        //card read error or unknown voicemail count. Check count stored in persist memory.
+        if (countVoiceMessages == -1) {
             countVoiceMessages = getStoredVoiceMessageCount();
         }
         Rlog.d(LOG_TAG, "updateVoiceMail countVoiceMessages = " + countVoiceMessages);
