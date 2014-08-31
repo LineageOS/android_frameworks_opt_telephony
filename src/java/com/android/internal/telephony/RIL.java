b8373873d5a74ae659245e3eb7dae9935e60410e
invisiblek <dan.pasanen@gmail.com>
2014-08-30 19:56:10 -0500

    SamsungQualcommRIL: Handle ResponseCallList in our subclass
    Change-Id: I25e8ae7a41070b7554697439ae8ec81c2fea2131

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -232,7 +232,6 @@ public class RIL extends BaseCommands implements CommandsInterface {
     static final String LOG_TAG = RILJ_LOG_TAG;
     static final boolean RILJ_LOGD = true;
     static final boolean RILJ_LOGV = false; // STOPSHIP if true
-    protected boolean samsungDriverCall = false;
 
     /**
      * Wake lock timeout should be longer than the longest timeout in
@@ -3613,8 +3612,6 @@ public class RIL extends BaseCommands implements CommandsInterface {
             dc.als = p.readInt();
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
-            if(samsungDriverCall)
-                 p.readInt();
             dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
             int np = p.readInt();
