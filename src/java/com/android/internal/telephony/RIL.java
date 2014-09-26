0b1e1f9fccd936da2d54a2447a58005b3a81f5aa
Bobby Wang <bowang@codeaurora.org>
2014-09-26 11:01:04 +0800

    should set user callback message in setInitialAttachApn
    should set user callback message in setInitialAttachApn instead of
    setting null by RIL.java itself
    
    Change-Id: I576ac109ef8230e91b075770f3da97f08f1744d0
    CRs-Fixed: 730595

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -4852,7 +4852,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
     public void setInitialAttachApn(String apn, String protocol, int authType, String username,
             String password, Message result) {
-        RILRequest rr = RILRequest.obtain(RIL_REQUEST_SET_INITIAL_ATTACH_APN, null);
+        RILRequest rr = RILRequest.obtain(RIL_REQUEST_SET_INITIAL_ATTACH_APN, result);
 
         if (RILJ_LOGD) riljLog("Set RIL_REQUEST_SET_INITIAL_ATTACH_APN");
 
