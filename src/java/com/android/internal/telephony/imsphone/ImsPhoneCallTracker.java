f38f5932049a1afae94c1b12111310f0eb476436
Omkar Kolangade <omkark@codeaurora.org>
2014-09-29 14:14:20 -0700

    IMS MWI Feature
    Adding logic to propagate waiting
    voice message count to UI.
    
    Change-Id: I922d8ff3138aefdbf284b5ec9f84afcedaff59a1

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1523,6 +1523,12 @@ public final class ImsPhoneCallTracker extends CallTracker {
 
             if (DBG) log("onFeatureCapabilityChanged: mImsFeatureEnabled=" +  mImsFeatureEnabled);
         }
+
+        @Override
+        public void onVoiceMessageCountChanged(int count) {
+            if (DBG) log("onVoiceMessageCountChanged :: count=" + count);
+            mPhone.mDefaultPhone.setVoiceMessageCount(count);
+        }
     };
 
     /* package */
