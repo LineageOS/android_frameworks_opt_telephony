c6e65b0c404b5514bb90c442e811813102debdbc
Ye Wen <ywen@google.com>
2014-10-21 10:51:38 -0700

    Send HTTP status code back in case of MMS HTTP failure (2/3)
    We need this in SMS app so that we can treat 404 specially.
    
    b/17520290
    
    Change-Id: Id8aa7ec6f688528e02fe43a52e95cee2c028bf1a

--- src/java/android/telephony/SmsManager.java
+++ src/java/android/telephony/SmsManager.java
@@ -1273,8 +1273,10 @@ public final class SmsManager {
     public static final int MMS_ERROR_RETRY = 6;
     public static final int MMS_ERROR_CONFIGURATION_ERROR = 7;
 
-    // Intent extra name for result data
+    // Intent extra name for MMS sending result data in byte array type
     public static final String EXTRA_MMS_DATA = "android.telephony.extra.MMS_DATA";
+    // Intent extra name for HTTP status code for MMS HTTP failure in integer type
+    public static final String EXTRA_MMS_HTTP_STATUS = "android.telephony.extra.MMS_HTTP_STATUS";
 
     /**
      * Update the status of a pending (send-by-IP) MMS message handled by the carrier app.
