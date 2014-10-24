84c5e3fa4756fdc67ff7b802a827b4450ffe52eb
kaiyiz <kaiyiz@codeaurora.org>
2014-10-24 15:51:04 +0800

    Telephony: Add CallFailCause values for clear code
    Add needed CallFailCause values and convert to DisconnectCause
    
    Change-Id: Ib247757d0caed2cb36ae9ccd3e7e332004556db4

--- src/java/com/android/internal/telephony/gsm/CallFailCause.java
+++ src/java/com/android/internal/telephony/gsm/CallFailCause.java
@@ -55,5 +55,42 @@ public interface CallFailCause {
     static final int DIAL_MODIFIED_TO_SS = 245;
     static final int DIAL_MODIFIED_TO_DIAL = 246;
 
+    // Supplementary
+    static final int NO_ROUTE_TO_DESTINAON = 3;
+    static final int CHANNEL_UNACCEPTABLE = 6;
+    static final int OPERATOR_DETERMINED_BARRING = 8;
+    static final int CALL_FAIL_NO_USER_RESPONDING = 18;
+    static final int CALL_FAIL_NO_ANSWER_FROM_USER = 19;
+    static final int CALL_REJECTED = 21;
+    static final int PREEMPTION = 25;
+    static final int CALL_FAIL_DESTINATION_OUT_OF_ORDER = 27;
+    static final int INVALID_NUMBER = 28;
+    static final int FACILITY_REJECTED = 29;
+    static final int NETWORK_OUT_OF_ORDER = 38;
+    static final int ACCESS_INFORMATION_DISCARDED = 43;
+    static final int RESOURCES_UNAVAILABLE_OR_UNSPECIFIED = 47;
+    static final int REQUESTED_FACILITY_NOT_SUBSCRIBED = 50;
+    static final int INCOMING_CALLS_BARRED_WITHIN_CUG = 55;
+    static final int BEARER_CAPABILITY_NOT_AUTHORIZED = 57;
+    static final int SERVICE_OPTION_NOT_AVAILABLE = 63;
+    static final int BEARER_SERVICE_NOT_IMPLEMENTED = 65;
+    static final int REQUESTED_FACILITY_NOT_IMPLEMENTED = 69;
+    static final int ONLY_DIGITAL_INFORMATION_BEARER_AVAILABLE = 70;
+    static final int SERVICE_OR_OPTION_NOT_IMPLEMENTED = 79;
+    static final int INVALID_TRANSACTION_IDENTIFIER = 81;
+    static final int USER_NOT_MEMBER_OF_CUG = 87;
+    static final int INCOMPATIBLE_DESTINATION = 88;
+    static final int INVALID_TRANSIT_NW_SELECTION = 91;
+    static final int SEMANTICALLY_INCORRECT_MESSAGE = 95;
+    static final int INVALID_MANDATORY_INFORMATION = 96;
+    static final int MESSAGE_TYPE_NON_IMPLEMENTED = 97;
+    static final int MESSAGE_TYPE_NOT_COMPATIBLE_WITH_PROTOCOL_STATE = 98;
+    static final int INFORMATION_ELEMENT_NON_EXISTENT = 99;
+    static final int CONDITIONAL_IE_ERROR = 100;
+    static final int MESSAGE_NOT_COMPATIBLE_WITH_PROTOCOL_STATE = 101;
+    static final int RECOVERY_ON_TIMER_EXPIRED = 102;
+    static final int PROTOCOL_ERROR_UNSPECIFIED = 111;
+    static final int INTERWORKING_UNSPECIFIED = 127;
+
     static final int ERROR_UNSPECIFIED = 0xffff;
 }
