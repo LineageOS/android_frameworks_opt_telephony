lt03lte-cafaosp/platform-frameworks-opt-telephony

Telephony: Don't read records from card if not needed.
Specifically handle the scenario where there is an RUIM card
 inserted but we are in NV mode. In this case, don't read
records from the card.
IccCardProxy tells the IccRecord that it will be requiring records
from the card. RuimRecord waits for this indication before fetching
records from the card

Change-Id: I943e3d6151355c9779b6c77d60e862a94e62b0ee

src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -64,6 +64,7 @@ public final class RuimRecords extends IccRecords {
     private String mMin2Min1;
 
     private String mPrlVersion;
+    private boolean mRecordsRequired = false;
     // From CSIM application
     private byte[] mEFpl = null;
     private byte[] mEFli = null;
@@ -632,8 +633,36 @@ public final class RuimRecords extends IccRecords {
         mCi.getCDMASubscription(obtainMessage(EVENT_GET_CDMA_SUBSCRIPTION_DONE));
     }
 
+    /**
+     * Called by IccCardProxy before it requests records.
+     * We use this as a trigger to read records from the card.
+     */
+    void recordsRequired() {
+        if (DBG) log("recordsRequired");
+        mRecordsRequired = true;
+
+        // trigger to retrieve all records
+        fetchRuimRecords();
+    }
 
     private void fetchRuimRecords() {
+        /* Don't read records if we don't expect
+         * anyone to ask for them
+         *
+         * If we have already requested records OR
+         * records are not required by anyone OR
+         * the app is not ready
+         * then bail
+         */
+        if (recordsRequested || !mRecordsRequired
+            || AppState.APPSTATE_READY != mParentApp.getState()) {
+            if (DBG) log("fetchRuimRecords: Abort fetching records recordsRequested = "
+                            + recordsRequested
+                            + " state = " + mParentApp.getState()
+                            + " required = " + mRecordsRequired);
+            return;
+        }
+
         recordsRequested = true;
 
         if (DBG) log("fetchRuimRecords " + recordsToLoad);

LineageOS/android_frameworks_opt_telephony

Change-Id: Ifa95dda2958922901e887b386c55556f947a881d

src/java/com/android/internal/telephony/uicc/RuimRecords.java
@@ -42,6 +42,7 @@ import com.android.internal.telephony.MccTable;
 
 import com.android.internal.telephony.cdma.sms.UserData;
 import com.android.internal.telephony.uicc.IccCardApplicationStatus.AppType;
+import com.android.internal.telephony.uicc.IccCardApplicationStatus.AppState;
 
 
 /**
@@ -58,6 +59,7 @@ public final class RuimRecords extends IccRecords {
     private String mMin2Min1;
 
     private String mPrlVersion;
+    private boolean mRecordsRequired = false;
     // From CSIM application
     private byte[] mEFpl = null;
     private byte[] mEFli = null;
@@ -712,8 +714,36 @@ public final class RuimRecords extends IccRecords {
         mCi.getCDMASubscription(obtainMessage(EVENT_GET_CDMA_SUBSCRIPTION_DONE));
     }
 
+    /**
+     * Called by IccCardProxy before it requests records.
+     * We use this as a trigger to read records from the card.
+     */
+    void recordsRequired() {
+        if (DBG) log("recordsRequired");
+        mRecordsRequired = true;
+
+        // trigger to retrieve all records
+        fetchRuimRecords();
+    }
 
     private void fetchRuimRecords() {
+        /* Don't read records if we don't expect
+         * anyone to ask for them
+         *
+         * If we have already requested records OR
+         * records are not required by anyone OR
+         * the app is not ready
+         * then bail
+         */
+        if (mRecordsRequested || !mRecordsRequired
+            || AppState.APPSTATE_READY != mParentApp.getState()) {
+            if (DBG) log("fetchRuimRecords: Abort fetching records rRecordsRequested = "
+                            + mRecordsRequested
+                            + " state = " + mParentApp.getState()
+                            + " required = " + mRecordsRequired);
+            return;
+        }
+
         mRecordsRequested = true;
 
         if (DBG) log("fetchRuimRecords " + mRecordsToLoad);