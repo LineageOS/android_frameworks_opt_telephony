aff58c5287c3c42de86839574825e61dc6323ff2
Mengjun Leng <mengju@codeaurora.org>
2014-11-07 18:13:29 +0800

    Telephony: Add the logic for disabling to fetch APN from OMH card
    Change-Id: Ie180e03562a3f4da971415f04dbaade507232567
    CRs-Fixed: 748148

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -235,9 +235,14 @@ public final class DcTracker extends DcTrackerBase {
         if (DBG) log(LOG_TAG + ".constructor");
 
         if (p.getPhoneType() == PhoneConstants.PHONE_TYPE_CDMA) {
-            mOmhApt = new CdmaApnProfileTracker((CDMAPhone)p);
-            mOmhApt.registerForModemProfileReady(this,
-                    DctConstants.EVENT_MODEM_DATA_PROFILE_READY, null);
+            final boolean fetchApnFromOmhCard = p.getContext().getResources().
+                    getBoolean(com.android.internal.R.bool.config_fetch_apn_from_omh_card);
+            log(LOG_TAG + " fetchApnFromOmhCard: " + fetchApnFromOmhCard);
+            if (fetchApnFromOmhCard) {
+                mOmhApt = new CdmaApnProfileTracker((CDMAPhone)p);
+                mOmhApt.registerForModemProfileReady(this,
+                        DctConstants.EVENT_MODEM_DATA_PROFILE_READY, null);
+            }
         }
 
         mDataConnectionTracker = this;
