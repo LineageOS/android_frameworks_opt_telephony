b0f0205b87cd84b981304541ed765d674bdf4950
Yashdev Singh <yashdevs@codeaurora.org>
2014-11-11 18:50:01 -0800

    Telephony: Add new api isMmsDataConnectivityPossible.
    - New api for apps preemptive active.
    
    Change-Id: Ic1c06ff2038d5500afc156aa5c0fbd79878f9a25

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -1748,6 +1748,12 @@ public abstract class PhoneBase extends Handler implements Phone {
     }
 
     @Override
+    public boolean isOnDemandDataPossible(String apnType) {
+        return ((mDcTracker != null) &&
+                (mDcTracker.isOnDemandDataPossible(apnType)));
+    }
+
+    @Override
     public boolean isDataConnectivityPossible(String apnType) {
         return ((mDcTracker != null) &&
                 (mDcTracker.isDataPossible(apnType)));
