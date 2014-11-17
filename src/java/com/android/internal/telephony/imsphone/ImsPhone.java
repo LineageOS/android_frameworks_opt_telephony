1c0992f74293e824826996ad3a2aba7abd03c5f8
Suresh Kumar Sugguna <sureshk@codeaurora.org>
2014-11-17 18:27:27 +0530

    IMS: start and stop dtmf tones
    Support of start and stop dtmf tones in Imsphone
    
    Change-Id: I9dec982035e8fdffcd198685caecceffb7e9f406
    CRs-Fixed: 751797

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -564,18 +564,18 @@ public class ImsPhone extends ImsPhoneBase {
     @Override
     public void
     startDtmf(char c) {
-        if (!PhoneNumberUtils.is12Key(c)) {
+        if (!(PhoneNumberUtils.is12Key(c) || (c >= 'A' && c <= 'D'))) {
             Rlog.e(LOG_TAG,
                     "startDtmf called with invalid character '" + c + "'");
         } else {
-            sendDtmf(c);
+            mCT.startDtmf(c);
         }
     }
 
     @Override
     public void
     stopDtmf() {
-        // no op
+        mCT.stopDtmf();
     }
 
     @Override
