096670c456d1affc1b54fccc291a583ccb90a520
Shareef Ali <shareefalis@cyanogenmod.org>
2014-11-28 22:22:30 -0600

    DCTracker: HACK Fix eHRPD/LTE data connection.
    * UICC controller is suppose to send a event that says uicc updated
      We are experiencing a race condition when UICC is init before the
      radio start which cause the sim is not ready response from radio.
    * This forces data when we have cdma voice connection.
    
    Change-Id: Ifccafa148a703a2f9e137733d8450a3a961dca2a

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2683,7 +2683,7 @@ public final class DcTracker extends DcTrackerBase {
     }
 
     private boolean isNvSubscription() {
-        int radioTech = mPhone.getServiceState().getRilDataRadioTechnology();
+        int radioTech = mPhone.getServiceState().getRilVoiceRadioTechnology();
         if (mCdmaSsm == null) {
             return false;
         }
