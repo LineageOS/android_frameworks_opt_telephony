9e88f648bebeab1a971ea8b2ccebbffa850e7d7f
William C. Garrison III <bill@cs.pitt.edu>
2014-12-15 17:11:35 -0500

    Telephony: If no wrapped message, originating address is null
    A Verizon HTC m8 user reported this error:
    
    http://pastebin.com/3LaYGB6X
    
    which means that mWrappedSmsMessage is null, meaning it wasn't created
    successfully by createFromPdu(pdu, format). Let's let getOriginatingAddress()
    report this as a null, and see if createFromPdu(pdu) can create the wrapped
    message properly.
    
    Even if it's still a null, TextUtils.isEmpty will return true, meaning the
    message's address won't be returned (better than crashing com.process.phone).
    
    Change-Id: I419bc3e6045b0cf7d62a348a0be02555e0f7b76e

--- src/java/android/telephony/SmsMessage.java
+++ src/java/android/telephony/SmsMessage.java
@@ -550,7 +550,7 @@ public class SmsMessage {
      * form or null if unavailable
      */
     public String getOriginatingAddress() {
-        return mWrappedSmsMessage.getOriginatingAddress();
+        return mWrappedSmsMessage != null ? mWrappedSmsMessage.getOriginatingAddress() : null;
     }
 
     /**
