6ae60c927025db807dda49726f7f72b02a300635
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-17 21:19:34 +0700

    Forward port Samsung STK support (frameworks/opt/telephony)
    Squashed commit of the following:
    
    commit 7940f7ced44976f03171d09c2ba8b1d29bc9432e
    Author: Flamefire <alex@grundis.de>
    Date:   Sat Nov 15 21:26:33 2014 +0100
    
        Fix crash of Samsung phones
    
        Using an alternative SMS app like chompSMS leads to a crash as
        sendResult is an array without any entries (sendResult[0] is accessed)
        The default SMS app does not show this because mCurrntCmd == null
    
        Change-Id: Icfef6d20f9082dd1f64ed29b67031d2575896513
    
    commit ed0cac138315b4dd5b36c35651ba6d23f1a47ae0
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Tue Dec 11 17:22:30 2012 +0700
    
        Samsung STK: Fix SMS-based STK
    
        SEND_SMS was falling through to SEND_USSD (which used to do nothing
        until USSD support was added) causing an Exception because cmdParams
        is not an instance of SendUSSDParams.
    
        Fix by using type-checking instead of checking if Samsung STK is
        enabled (because SendSMSParams and SendUSSDParams will only be
        created if Samsung STK is enabled.)
    
        Change-Id: I671014e295e9e529aad25a8b6fbd5a2e5788fc44
    
    commit 7a9f16293abc6e216004d0c80822d39dac98db98
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Fri Nov 30 21:35:50 2012 +0700
    
        Samsung STK: Add USSD support
    
        Allow USSD-based STK applications to work
    
        Change-Id: I483f37a44a6c3ed43eefcf979e17b84877d03f93
    
    commit b5ade3b5a527193ffc6e71f6ce55d3a96a83792a
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Fri Nov 30 19:30:08 2012 +0700
    
        Add Samsung STK support (telephony part)
    
        Support for SMS-based SIM applications.
        Partially rewritten the original patch by Jüri Schultz to be less intrusive.
    
        Change-Id: I15d2de1781a2823e24941dd792db3d372578aa9f
    
    Change-Id: I476959cfadab0ab14f5ed32adf51adca1e3b26b4

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3016,6 +3016,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
             case RIL_UNSOL_HARDWARE_CONFIG_CHANGED: ret = responseHardwareConfig(p); break;
             case RIL_UNSOL_ON_SS: ret =  responseSsData(p); break;
             case RIL_UNSOL_STK_CC_ALPHA_NOTIFY: ret =  responseString(p); break;
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: ret = responseInts(p); break; // Samsung STK
 
             default:
                 throw new RuntimeException("Unrecognized unsol response: " + response);
@@ -3439,6 +3440,18 @@ public class RIL extends BaseCommands implements CommandsInterface {
                 if (mCatCcAlphaRegistrant != null) {
                     mCatCcAlphaRegistrant.notifyRegistrant(
                                         new AsyncResult (null, ret, null));
+                }
+                break;
+            // Samsung STK
+            case RIL_UNSOL_STK_SEND_SMS_RESULT:
+                if (Resources.getSystem().
+                        getBoolean(com.android.internal.R.bool.config_samsung_stk)) {
+                    if (RILJ_LOGD) unsljLogRet(response, ret);
+
+                    if (mCatSendSmsResultRegistrant != null) {
+                        mCatSendSmsResultRegistrant.notifyRegistrant(
+                                new AsyncResult (null, ret, null));
+                    }
                 }
                 break;
         }
@@ -4517,6 +4530,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
             case RIL_UNSOL_HARDWARE_CONFIG_CHANGED: return "RIL_UNSOL_HARDWARE_CONFIG_CHANGED";
             case RIL_UNSOL_ON_SS: return "UNSOL_ON_SS";
             case RIL_UNSOL_STK_CC_ALPHA_NOTIFY: return "UNSOL_STK_CC_ALPHA_NOTIFY";
+            case RIL_UNSOL_STK_SEND_SMS_RESULT: return "RIL_UNSOL_STK_SEND_SMS_RESULT";
             default: return "<unknown response>";
         }
     }
