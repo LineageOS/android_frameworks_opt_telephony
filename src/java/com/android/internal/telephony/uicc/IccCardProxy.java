7fea5bea3e1a67a161d6fbcdbbfc76a8e14080a1
Preeti Ahuja <preetia@codeaurora.org>
2014-12-18 12:35:39 -0800

    Broadcast icc card state as NOT_READY if card is not initialized
    When radio turns on, card state (external state ) is updated.
    At this point, if the UiccCard instance is null (UiccCard instance could
    be null at bootup, before we have received a vaild card state from
    UiccController), icc state as ABSENT is broadcasted.
    
    When the card state is being updated, if the UiccCard instance
    is null, broadcast the state as NOT_READY instead of ABSENT.
    
    Change-Id: I86fe1db536e25b8c46e46a0dd009b21869737019
    CRs-Fixed: 773273

--- src/java/com/android/internal/telephony/uicc/IccCardProxy.java
+++ src/java/com/android/internal/telephony/uicc/IccCardProxy.java
@@ -453,7 +453,6 @@ public class IccCardProxy extends Handler implements IccCard {
                 registerUiccCardEvents();
                 updateActiveRecord();
             }
-
             updateExternalState();
         }
     }
@@ -468,7 +467,15 @@ public class IccCardProxy extends Handler implements IccCard {
     }
 
     private void updateExternalState() {
-        if (mUiccCard == null || mUiccCard.getCardState() == CardState.CARDSTATE_ABSENT) {
+
+        // mUiccCard could be null at bootup, before valid card states have
+        // been received from UiccController.
+        if (mUiccCard == null) {
+            setExternalState(State.NOT_READY);
+            return;
+        }
+
+        if (mUiccCard.getCardState() == CardState.CARDSTATE_ABSENT) {
             if (mRadioOn) {
                 setExternalState(State.ABSENT);
             } else {
