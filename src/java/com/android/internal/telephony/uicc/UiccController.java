fa7ee16d6eb2c2f7f8a863f3e80d8a22974e67b2
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-28 18:53:11 +0700

    UiccController: use registerForAvailable only for persist.radio.apm_sim_not_pwdn
    Some QCOM RILs allow the SIM to be loaded in airplane mode, thus
    CAF changed the callback to registerForAvailable, but that does
    not work on other RILs where the SIM information will not be
    loaded successfully.
    
    Change-Id: I9f9124417db2fe983e716a2825fc08d87511f8aa

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -126,7 +126,10 @@ public class UiccController extends Handler {
             Integer index = new Integer(i);
             mCis[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
             // TODO remove this once modem correctly notifies the unsols
-            if (DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt"))) {
+            if (DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt")) ||
+                    SystemProperties.getBoolean("persist.radio.apm_sim_not_pwdn", false)) {
+                // Reading ICC status in airplane mode is only supported in QCOM
+                // RILs when this property is set to true
                 mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
             } else {
                 mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
