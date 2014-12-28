953ec0b9628a53648015a572d657d31e9d031e4f
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-28 20:24:29 +0700

    UiccController: remove registerForAvailable
    CAF added this to be able to load SIM record when in airplane mode,
    but this breaks IMSI loading in some RILs (symptom: cannot view
    data usage stat in Quick Settings).
    
    Change-Id: I9f9124417db2fe983e716a2825fc08d87511f8aa

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -142,7 +142,6 @@ public class UiccController extends Handler {
             Integer index = new Integer(i);
             mCis[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
             // TODO remove this once modem correctly notifies the unsols
-            mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
             mCis[i].registerForNotAvailable(this, EVENT_RADIO_UNAVAILABLE, index);
             mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
             if (mOEMHookSimRefresh) {
