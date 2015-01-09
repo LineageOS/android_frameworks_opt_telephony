31b502cb274be3c20b993d51192b56f0e7a9ae09
Tyler Gunn <tgunn@google.com>
2015-01-09 15:54:23 -0800

    Fix for call timer resetting when starting IMS conference call. 2/4
    - Added support in Conference for specifying the connect time when the
    conference is created.
    
    Bug: 18959443
    Change-Id: Id29f7ba878610a0a4eaf7bbd592f344af4c0ffc4

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCall.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCall.java
@@ -219,6 +219,14 @@ public class ImsPhoneCall extends Call {
 
     /* package */ void
     merge(ImsPhoneCall that, State state) {
+        // This call is the conference host and the "that" call is the one being merged in.
+        // Set the connect time to the earliest of this call and the other call.
+        // This ensures that when an ImsConference is started, its start time will be when the first
+        // call in the conference started.
+        long earliestConnectTime = Math.min(getEarliestConnectTime(),
+                that.getEarliestConnectTime());
+        getFirstConnection().setConnectTime(earliestConnectTime);
+
         ImsPhoneConnection[] cc = that.mConnections.toArray(
                 new ImsPhoneConnection[that.mConnections.size()]);
         for (ImsPhoneConnection c : cc) {
