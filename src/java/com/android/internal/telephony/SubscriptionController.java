160ba015f599e4522b4d913dcf3a6d4d4245d2d5
Junda Liu <junda@google.com>
2015-01-28 14:10:17 -0800

    Check line1number override and use it instead for subscription.
    Bug: b/19143556
    Change-Id: I7bceea2ed39a075e66db2a5487296d4d33e2a941

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -386,6 +386,11 @@ public class SubscriptionController extends ISub.Stub {
                 " status:" + status + " nwMode:" + nwMode);
         }
 
+        String line1Number = mTelephonyManager.getLine1NumberForSubscriber(id);
+        if (!TextUtils.isEmpty(line1Number) && !line1Number.equals(number)) {
+            logd("Line1Number is different: " + line1Number);
+            number = line1Number;
+        }
         return new SubscriptionInfo(id, iccId, simSlotIndex, displayName, carrierName,
                 nameSource, iconTint, number, dataRoaming, iconBitmap, mcc, mnc, countryIso,
                 status, nwMode);
