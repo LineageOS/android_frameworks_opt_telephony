e06dbed5cd29ddab44147e2abf634e7796ad2df5
Sanket Padawe <sanketpadawe@google.com>
2015-02-03 13:58:26 -0800

    Remove phone number being displayed from logs
    Bug: 19251204
    Change-Id: I5391fa2033ddb8cb41e11cb23aa16485369cd13e

--- src/java/com/android/internal/telephony/uicc/IccRecords.java
+++ src/java/com/android/internal/telephony/uicc/IccRecords.java
@@ -137,7 +137,6 @@ public abstract class IccRecords extends Handler implements IccConstants {
                 + " adnCache=" + mAdnCache
                 + " recordsRequested=" + mRecordsRequested
                 + " iccid=" + mIccId
-                + " msisdn=" + mMsisdn
                 + " msisdnTag=" + mMsisdnTag
                 + " voiceMailNum=" + mVoiceMailNum
                 + " voiceMailTag=" + mVoiceMailTag
