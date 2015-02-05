c72d73688ed92930d31e07a2aac9c9dd3fee70b9
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2015-02-05 14:07:00 +0100

    Add dummy SUB record in CDMA NV mode
    Add dummy SUB info record when NV is ready in CDMA NV mode.
    
    CRs-Fixed: 789989
    
    Change-Id: Ie658087efaa9f93a329773d2d8f0601d083f3701

--- src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
+++ src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -77,6 +77,7 @@ public class SubscriptionInfoUpdater extends Handler {
     private static final int EVENT_SIM_RESTRICTED = 8;
 
     private static final String ICCID_STRING_FOR_NO_SIM = "";
+    private static final String ICCID_STRING_FOR_NV = "DUMMY_NV_ID";
     /**
      *  int[] sInsertSimState maintains all slots' SIM inserted status currently,
      *  it may contain 4 kinds of values:
@@ -535,6 +536,15 @@ public class SubscriptionInfoUpdater extends Handler {
             updateSubscriptionInfoByIccId();
         }
         updateCarrierServices(slotId, simState);
+    }
+
+    public void updateSubIdForNV(int slotId) {
+        mIccId[slotId] = ICCID_STRING_FOR_NV;
+        logd("[updateSubIdForNV]+ Start");
+        if (isAllIccIdQueryDone()) {
+            logd("[updateSubIdForNV]+ updating");
+            updateSubscriptionInfoByIccId();
+        }
     }
 
     /**
