d2727fcc699c59345c904f8d5896a9cc0f9166e7
Muhammed Siju <msiju@codeaurora.org>
2015-02-09 19:11:34 +0800

    IMS: Add support for sending ADD_PARTICIPANTS response to apps.
    Applications pass Message callback object for ADD_PARTICIPANT
    request. The corresponding success/failure response is sent back
    to applications via the Message callback. Only the callback
    object for latest request is cached, so it is the responsibility
    of caller to wait for the response for previous request before
    sending a new request.
    
    Change-Id: I71af4c3eaa0fc787742c9109ccaefa43531e1713
    CRs-Fixed: 757978

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -913,6 +913,13 @@ public interface Phone {
     public void addParticipant(String dialString) throws CallStateException;
 
     /**
+     * Initiate to add a participant in an IMS call.
+     *
+     * @exception CallStateException operation is not supported.
+     */
+    public void addParticipant(String dialString, Message onComplete) throws CallStateException;
+
+    /**
      * Handles PIN MMI commands (PIN/PIN2/PUK/PUK2), which are initiated
      * without SEND (so <code>dial</code> is not appropriate).
      *
