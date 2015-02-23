a95110b8de340ab088e88b9b602fafac15e34c61
Shishir Agrawal <shishir@google.com>
2015-02-23 15:28:35 -0800

    DcTracker: Cleanup connections when IccRecords is destroyed.
    The IccRecords are destroyed when the SIM goes through a state change like a
    refresh reset. Attempting to setup data connections when in this case can lead
    to incorrect APNs being passed to the radio.
    
    The change will cleanup all data connections when IccRecords are disposed off.
    New data connections will not be setup because SIM ready checks are done
    before setting them up.
    
    Bug: 19379406
    Change-Id: Ide24ec3ab08bd8e76d4a98a47b4f0509987c11eb

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -109,6 +109,7 @@ public interface Phone {
     static final String REASON_CONNECTED = "connected";
     static final String REASON_SINGLE_PDN_ARBITRATION = "SinglePdnArbitration";
     static final String REASON_DATA_SPECIFIC_DISABLED = "specificDisabled";
+    static final String REASON_SIM_NOT_READY = "simNotReady";
 
     // Used for band mode selection methods
     static final int BM_UNSPECIFIED = 0; // selected by baseband automatically
