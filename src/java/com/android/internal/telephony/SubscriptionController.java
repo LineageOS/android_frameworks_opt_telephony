b7c44b3fe5944bccd34ede0d6c6fd9e9aceca949
Sandeep Gutta <sangutta@codeaurora.org>
2015-03-17 21:58:06 +0530

    MSIM: Change default fallback subId value
    Some times getPhoneId(DEFAULT_SUBSCRIPTION_ID) returning
    INVALID_PHONE_INDEX which causing some error application layer.
    
    Change mDefaultFallbackSubId default value to DUMMY_SUB_ID_BASE,
    with this change getPhoneId() always returns phone Id as '0' for all
    valid subIds.
    
    Change DUMMY_SUB_ID_BASE range.
    
    Change-Id: If5ac1b830fefcd7cbb043261e231677db8ba6951

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -139,9 +139,14 @@ public class SubscriptionController extends ISub.Stub {
     protected TelephonyManager mTelephonyManager;
     protected CallManager mCM;
 
+    // When no valid SIM cards present on device, framework returns DUMMY subIds
+    // with range starting from DUMMY_SUB_ID_BASE.
+    private static final int DUMMY_SUB_ID_BASE = SubscriptionManager.MAX_SUBSCRIPTION_ID_VALUE
+        - PhoneConstants.MAX_PHONE_COUNT_TRI_SIM;
+
     // FIXME: Does not allow for multiple subs in a slot and change to SparseArray
     private static HashMap<Integer, Integer> mSlotIdxToSubId = new HashMap<Integer, Integer>();
-    private static int mDefaultFallbackSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
+    private static int mDefaultFallbackSubId = DUMMY_SUB_ID_BASE;
     private static int mDefaultPhoneId = 0;
     private boolean mSubInfoReady = false;
 
@@ -156,9 +161,6 @@ public class SubscriptionController extends ISub.Stub {
 
     private DdsScheduler mScheduler;
     private DdsSchedulerAc mSchedulerAc;
-
-    // Dummy subIds are used when no SIMs present on device
-    private static final int DUMMY_SUB_ID_BASE = 5000;
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -1095,7 +1097,7 @@ public class SubscriptionController extends ISub.Stub {
         }
 
         if (subId >= DUMMY_SUB_ID_BASE) {
-            logd("getPhoneId,  received summy subId " + subId);
+            logd("getSlotId,  received dummy subId " + subId);
             return subId - DUMMY_SUB_ID_BASE;
         }
 
