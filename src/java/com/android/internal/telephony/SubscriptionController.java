7a55a6e111d313218f7517d8e26894f67ef32553
Muhammed Siju <msiju@codeaurora.org>
2015-04-02 19:09:05 +0530

    Fix DDS switch issue from GSM to CDMA.
    CDMA data remains in attached state irrespective of ALLOW_DATA
    true/false requests. Hence when switching DDS from G to C, there
    won't be any attached event on C subscription and
    DcSwitchStateMachine remains in Attaching state.
    To fix this, directly move from Attaching to Attached state if
    data is already registered.
    
    Change DDS switch implementation to set the new DDS setting
    only after DctController completes switch operation.
    
    Remove extra setDataAllowed call from DcSwitchStateMachine
    Attaching state.
    
    Change-Id: I4ee3ca59e874c6ac2de8d46d3e5f9358d9cf0b0d
    CRs-Fixed: 812478

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1428,7 +1428,9 @@ public class SubscriptionController extends ISub.Stub {
             reqStatus = PhoneConstants.SUCCESS;
         }
         mScheduler.updateCurrentDds(null);
-        broadcastDefaultDataSubIdChanged(reqStatus);
+        broadcastDefaultDataSubIdChanged(subId);
+
+        updateAllDataConnectionTrackers();
     }
 
     public void setDefaultDataSubId(int subId) {
@@ -1442,13 +1444,6 @@ public class SubscriptionController extends ISub.Stub {
                     EVENT_SET_DEFAULT_DATA_DONE, null);
         }
         mDctController.setDefaultDataSubId(subId);
-
-        Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION, subId);
-        broadcastDefaultDataSubIdChanged(subId);
-
-        // FIXME is this still needed?
-        updateAllDataConnectionTrackers();
     }
 
     public void setDataSubId(int subId) {
