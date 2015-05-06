3dce1ff45fb6c2e3c5c0447842c31f43c08941ce
Muhammed Siju <msiju@codeaurora.org>
2015-05-07 00:23:44 +0530

    Fix duplicate events received in DcTracker.
    Move registering of phone events to constructor to avoid
    duplicate registrations.
    Unregister from phone events while disposing DcTracker.
    
    Change-Id: I6ecf1227627b64b2cb505c120059cdcba6da70eb
    CRs-Fixed: 834049

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -170,6 +170,7 @@ public final class DcTracker extends DcTrackerBase {
         if (DBG) log("GsmDCT.constructor");
 
         mDataConnectionTracker = this;
+        registerForAllEvents();
         update();
         mApnObserver = new ApnChangeObserver();
         p.getContext().getContentResolver().registerContentObserver(
@@ -256,6 +257,7 @@ public final class DcTracker extends DcTrackerBase {
             mCdmaSsm.dispose(this);
         }
 
+        unregisterForAllEvents();
         destroyDataConnections();
     }
     protected void unregisterForAllEvents() {
@@ -2927,7 +2929,6 @@ public final class DcTracker extends DcTrackerBase {
     public void update() {
         log("update sub = " + mPhone.getSubId());
         log("update(): Active DDS, register for all events now!");
-        registerForAllEvents();
         onUpdateIcc();
 
         mUserDataEnabled = getDataEnabled();
