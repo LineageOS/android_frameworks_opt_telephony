efddb3b5776f308fb7b4671d95fe8327d739ee38
emancebo <emancebo@cyngn.com>
2015-05-27 19:01:19 -0700

    msim: order getSubInfo list by slot ID
    If the user removes their preferred SIM we will fall back to an active SIM returned by
    getSubInfo.  Currently this list is not ordered.  Order by slot ID so that we fall back
    to slot 0 by default, this way behavior is at least consistent.
    
    Change-Id: I6657dee2b2ac79cee6dd920442b3983cc916feaf
    (cherry picked from commit 10701b7f5e9a4c70983f457c5d83b1f20044d96c)

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -434,7 +434,7 @@ public class SubscriptionController extends ISub.Stub {
         }
         ArrayList<SubscriptionInfo> subList = null;
         Cursor cursor = mContext.getContentResolver().query(SubscriptionManager.CONTENT_URI,
-                null, selection, selectionArgs, null);
+                null, selection, selectionArgs, SubscriptionManager.SIM_ID);
         try {
             if (cursor != null) {
                 while (cursor.moveToNext()) {
