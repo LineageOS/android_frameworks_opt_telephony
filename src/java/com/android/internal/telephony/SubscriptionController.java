29048d570d429a456b74ba813951d4a45295fa23
Ricardo Cerqueira <ricardo@cyngn.com>
2015-06-10 01:26:44 +0100

    Revert "SubscriptionController: getPhoneId() should never return negative values"
    This reverts commit 2e374b88cba1803edcb540e5c64f50aa468cc573.
    
    Change-Id: I49ba469ba13737b4a456e19b340511cc46d0b94c

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -825,7 +825,7 @@ public class SubscriptionController extends ISub.Stub {
                             if (DBG) {
                                 logdl("[addSubInfoRecord] one sim set defaults to subId=" + subId);
                             }
-                            //setDefaultDataSubId(subId);
+                            setDefaultDataSubId(subId);
                             setDataSubId(subId);
                             setDefaultSmsSubId(subId);
                             setDefaultVoiceSubId(subId);
@@ -1191,16 +1191,14 @@ public class SubscriptionController extends ISub.Stub {
             subId = getDefaultSubId();
         }
 
-        if (subId >= DUMMY_SUB_ID_BASE) {
-            return subId - DUMMY_SUB_ID_BASE;
-        } else if (subId < 0) {
-            return (int) (-1 - subId);
-        }
-
         if (!SubscriptionManager.isValidSubscriptionId(subId)) {
             return SubscriptionManager.INVALID_PHONE_INDEX;
         }
 
+        if (subId >= DUMMY_SUB_ID_BASE) {
+            logd("getPhoneId,  received dummy subId " + subId);
+            return subId - DUMMY_SUB_ID_BASE;
+        }
 
         // FIXME: Assumes phoneId == slotId
         for (Entry<Integer, Integer> entry: mSlotIdxToSubId.entrySet()) {
