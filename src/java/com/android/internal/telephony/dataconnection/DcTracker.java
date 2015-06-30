1517b0920679cfbdaec14b00666f59d2ca905dc7
Sungmin Choi <sungmin.choi@lge.com>
2015-06-30 19:50:25 +0900

    Add getPreferredApn() in buildWaitingApns()
    Now, only createAllApnList() calls getPreferredApn().
    Unfortunately, in this time, getSubId() may not be proper value.
    So getPreferredApn() could not get value which we want/set as preferred apn.
    As a result, preferred apn is null and wrong apn info can be sent to modem
    and be used by SETUP_DATA_CALL procedure.
    To resolve this, just add getPreferredApn() in buildWaitingApns().
    
    Bug: 22159434
    Change-Id: Iee92325aa91c5c470328055746c321065a9829d4
    
    Fixed that phone service crashed during boot up issue.
    
    bug: 22357173
    Change-Id: I3b76a74c698e22c887b3482a3bdcbc34982ec658

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -3048,6 +3048,9 @@ public final class DcTracker extends DcTrackerBase {
             if (DBG) log("buildWaitingApns: usePreferred NotFoundException set to true");
             usePreferred = true;
         }
+        if (usePreferred) {
+            mPreferredApn = getPreferredApn();
+        }
         if (DBG) {
             log("buildWaitingApns: usePreferred=" + usePreferred
                     + " canSetPreferApn=" + mCanSetPreferApn
@@ -3099,7 +3102,7 @@ public final class DcTracker extends DcTrackerBase {
                 }
             }
         } else {
-            loge("mAllApnSettings is empty!");
+            loge("mAllApnSettings is null!");
         }
         if (DBG) log("buildWaitingApns: X apnList=" + apnList);
         return apnList;
@@ -3140,8 +3143,8 @@ public final class DcTracker extends DcTrackerBase {
     }
 
     private ApnSetting getPreferredApn() {
-        if (mAllApnSettings.isEmpty()) {
-            log("getPreferredApn: X not found mAllApnSettings.isEmpty");
+        if (mAllApnSettings == null || mAllApnSettings.isEmpty()) {
+            log("getPreferredApn: mAllApnSettings is " + ((mAllApnSettings == null)?"null":"empty"));
             return null;
         }
 
