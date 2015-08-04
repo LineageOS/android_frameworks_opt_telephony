5f15c3f0db4c606f1c816f6c865d189f098e4f91
Etan Cohen <etancohen@google.com>
2015-08-03 17:25:20 -0700

    Only use IMS for E911 if VoLTE is supported on device/carrier.
    Bug: 22777920
    Change-Id: I5f79c36a7e9b89f70b69b0cab098d8b0a288b1b9

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -810,7 +810,8 @@ public class GSMPhone extends PhoneBase {
                  && (imsPhone.isVolteEnabled() || imsPhone.isVowifiEnabled())
                  && (imsPhone.getServiceState().getState() == ServiceState.STATE_IN_SERVICE);
 
-        boolean useImsForEmergency = imsPhone != null
+        boolean useImsForEmergency = ImsManager.isVolteEnabledByPlatform(mContext
+                && imsPhone != null
                 && isEmergency
                 &&  mContext.getResources().getBoolean(
                         com.android.internal.R.bool.useImsAlwaysForEmergencyCall)
