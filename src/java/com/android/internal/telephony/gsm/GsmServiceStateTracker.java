01a32108dd9e69d5cd9faf55d32ce5dc0e650e5c
jumoog <github@jumoog.io>
2015-09-05 11:19:42 +0000

    allow to force roaming state false
    if you travel alot you care about type of mobile connectivity you’re getting
    and you don't care about roaming state
    
    Change-Id: Ia53143d9c8d406b6681670eff59f12748be62c15

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -169,6 +169,8 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
     static final int PS_NOTIFICATION = 888;  // Id to update and cancel PS restricted
     static final int CS_NOTIFICATION = 999;  // Id to update and cancel CS restricted
 
+    private boolean disableRoamingState = SystemProperties.getBoolean("config.disable_roaming_state", false);
+
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -1022,7 +1024,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                 " mNewReasonDataDenied=" + mNewReasonDataDenied);
         }
 
-        if (Build.IS_DEBUGGABLE && SystemProperties.getBoolean(PROP_FORCE_ROAMING, false)) {
+        if (disableRoamingState) {
             mNewSS.setVoiceRoaming(true);
             mNewSS.setDataRoaming(true);
         }
@@ -1610,6 +1612,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                 return true;
             }
         }
+
         return false;
     }
 
