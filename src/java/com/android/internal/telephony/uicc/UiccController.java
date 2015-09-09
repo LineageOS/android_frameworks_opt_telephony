af1cd8cbac506462e91d024f9c7c864d6002495b
Wileen Chiu <wileenc@codeaurora.org>
2015-09-09 15:19:38 -0700

    Use radio availability to sync card status instead of radio on
    - In cases where RIL initializes more quickly than frameworks,
    the card state changed notification is missed by frameworks.
    To avoid missing card states, we register for radio on or
    available. However, this is decided based on a property set
    on encrypted devices.
    - On undecrypted devices, we ALWAYS register for radio ON.
    This breaks the user case where if we boot up in airplane
    mode, the card state is never queried and the device
    thinks there is no SIM.
    - Make UiccController sync the card state on radio available
    in all cases
    
    Change-Id: I369e8faabde3baa6f834a511812d24eb0b9de340
    CRs-Fixed: 881465

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -125,12 +125,7 @@ public class UiccController extends Handler {
         for (int i = 0; i < mCis.length; i++) {
             Integer index = new Integer(i);
             mCis[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
-            // TODO remove this once modem correctly notifies the unsols
-            if (DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt"))) {
-                mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
-            } else {
-                mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
-            }
+            mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
             mCis[i].registerForNotAvailable(this, EVENT_RADIO_UNAVAILABLE, index);
             mCis[i].registerForIccRefresh(this, EVENT_SIM_REFRESH, index);
         }
