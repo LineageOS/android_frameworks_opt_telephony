00e45e4150766f9969e9088454c533d18a30b820
Wileen Chiu <wileenc@codeaurora.org>
2015-09-11 15:07:29 -0700

    Revert "Telephony: Skip restoring network selection"
    This reverts commit bedbb9a42e69ac2a001fd1b22516dd7d09f3102e.
    
    Change-Id: Id3269a22b7ff24d28b65bf817c94aa6d45ef05f7

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -2428,9 +2428,7 @@ public abstract class PhoneBase extends Handler implements Phone {
         mRadioCapability.set(rc);
 
         if (SubscriptionManager.isValidSubscriptionId(getSubId())) {
-            boolean restoreSelection = !mContext.getResources().getBoolean(
-                com.android.internal.R.bool.skip_restoring_network_selection);
-            sendSubscriptionSettings(restoreSelection);
+            sendSubscriptionSettings(true);
         }
     }
 
