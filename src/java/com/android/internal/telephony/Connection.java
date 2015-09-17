ec3fe6cd37a79b3c5894b92a41fa2b79b05cb964
Tyler Gunn <tgunn@google.com>
2015-09-17 09:59:27 -0700

    Propagate telecom call ID to internal telephony connection.
    Provide visibility of the telecom call ID in the internal telephony
    connections.
    
    Bug: 23357902
    Change-Id: I6400026b6d02c7d5e740092a1736b4efbd3a3ae7

--- src/java/com/android/internal/telephony/Connection.java
+++ src/java/com/android/internal/telephony/Connection.java
@@ -86,6 +86,12 @@ public abstract class Connection {
     public static final int AUDIO_QUALITY_STANDARD = 1;
     public static final int AUDIO_QUALITY_HIGH_DEFINITION = 2;
 
+    /**
+     * The telecom internal call ID associated with this connection.  Only to be used for debugging
+     * purposes.
+     */
+    private String mTelecomCallId;
+
     //Caller Name Display
     protected String mCnapName;
     protected int mCnapNamePresentation  = PhoneConstants.PRESENTATION_ALLOWED;
@@ -128,6 +134,23 @@ public abstract class Connection {
     public Call.State mPreHandoverState = Call.State.IDLE;
 
     /* Instance Methods */
+
+    /**
+     * @return The telecom internal call ID associated with this connection.  Only to be used for
+     * debugging purposes.
+     */
+    public String getTelecomCallId() {
+        return mTelecomCallId;
+    }
+
+    /**
+     * Sets the telecom call ID associated with this connection.
+     *
+     * @param telecomCallId The telecom call ID.
+     */
+    public void setTelecomCallId(String telecomCallId) {
+        mTelecomCallId = telecomCallId;
+    }
 
     /**
      * Gets address (e.g. phone number) associated with connection.
@@ -713,6 +736,7 @@ public abstract class Connection {
     public String toString() {
         StringBuilder str = new StringBuilder(128);
 
+        str.append(" callId: " + getTelecomCallId());
         if (Rlog.isLoggable(LOG_TAG, Log.DEBUG)) {
             str.append("addr: " + getAddress())
                     .append(" pres.: " + getNumberPresentation())
