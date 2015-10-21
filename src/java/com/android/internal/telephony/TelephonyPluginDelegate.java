de07581cca78c619248b592c00c268937c6e5235
Andalam Parthasarathi <andala@codeaurora.org>
2015-10-21 14:10:41 +0530

    Telephony plugin for GsmServiceStateTracker
     - Add makeGsmServiceStateTracker method in TelephonyPlugin
          to have  provision of extending it from proprietary space.
     - Added telephony delegate function and created QtiGsmServiceStateTracker
       in proprietary jar file
    
    Change-Id: If048d14a4cc614e34afdbe1e1bf41023702a5cb1
    CRs-Fixed: 902503

--- src/java/com/android/internal/telephony/TelephonyPluginDelegate.java
+++ src/java/com/android/internal/telephony/TelephonyPluginDelegate.java
@@ -35,6 +35,8 @@ import android.telephony.Rlog;
 import com.android.internal.telephony.ConfigResourceUtil;
 import com.android.internal.telephony.dataconnection.DcTracker;
 import com.android.internal.telephony.dataconnection.DctController;
+import com.android.internal.telephony.gsm.GSMPhone;
+import com.android.internal.telephony.gsm.GsmServiceStateTracker;
 import com.android.internal.telephony.TelephonyPluginBase;
 import com.android.internal.telephony.DefaultTelephonyPlugin;
 import com.android.internal.R;
@@ -135,5 +137,9 @@ public class TelephonyPluginDelegate {
     public void initExtTelephonyClasses(Context context,
             Phone[] phoneProxy, CommandsInterface[] commandsInterfaces) {
         sPlugin.initExtTelephonyClasses(context, phoneProxy, commandsInterfaces);
+    }
+
+    public GsmServiceStateTracker makeGsmServiceStateTracker(GSMPhone phone) {
+        return sPlugin.makeGsmServiceStateTracker(phone);
     }
 }
