442909901389d128147755286fadeff425d55985
Wileen Chiu <wileenc@codeaurora.org>
2015-10-21 14:17:23 -0700

    Change default cdma subscription to RUIM
    - modem has made changes for the default
    cdma subscription to be RUIM, however, the
    default cdma subscription for telephony is
    still NV. The UI is showing as NV after the
    first boot up, which does not match modem
    - Change the default subscription value to RUIM
    
    Change-Id: I2301788ae083f477e0bfd2cecf221e8e002e51f1
    CRs-Fixed: 927032

--- src/java/com/android/internal/telephony/cdma/CdmaSubscriptionSourceManager.java
+++ src/java/com/android/internal/telephony/cdma/CdmaSubscriptionSourceManager.java
@@ -44,7 +44,7 @@ public class CdmaSubscriptionSourceManager extends Handler {
     public static final int SUBSCRIPTION_SOURCE_UNKNOWN = -1;
     public static final int SUBSCRIPTION_FROM_RUIM      = 0; /* CDMA subscription from RUIM */
     public static final int SUBSCRIPTION_FROM_NV        = 1; /* CDMA subscription from NV */
-    public static final int PREFERRED_CDMA_SUBSCRIPTION = SUBSCRIPTION_FROM_NV;
+    public static final int PREFERRED_CDMA_SUBSCRIPTION = SUBSCRIPTION_FROM_RUIM;
 
     private static CdmaSubscriptionSourceManager sInstance;
     private static final Object sReferenceCountMonitor = new Object();
