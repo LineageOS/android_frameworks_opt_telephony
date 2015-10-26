93873299c49938a828c4390c4a7ef7f73b09cee1
LuK1337 <priv.luk@gmail.com>
2015-10-26 22:59:24 +0100

    resources: Build it for all targets
    * Dialer is using it's resources for Settings and incall view,
    without it we get FCs or system crash in case of incoming calls.
    
    Change-Id: I4b5d1922cac1a76115db378ccb1e831c4a675b75

--- resources/Android.mk
+++ resources/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_PACKAGE_NAME := telresources
 LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 include $(BUILD_PACKAGE)
 include $(call all-makefiles-under,$(LOCAL_PATH))
