c6676c9a97efb78950c8f71ca7330d0bb4b24efd
Ajay Nambi <anambi@codeaurora.org>
2015-12-03 13:20:05 -0800

    Add p2 option in OpenLogicalChannel
    - Adding support for P2 parameter
    - Needed to support OMAPI 3.0 defined
    by SIM Alliance
    
    Test: Basic telephony sanity
    Bug: 27360148
    Change-Id: I51ab0821f84dca8fff90c912ebbfc9f1129dcbe5

--- src/java/com/android/internal/telephony/CommandsInterface.java
+++ src/java/com/android/internal/telephony/CommandsInterface.java
@@ -1786,10 +1786,11 @@ public interface CommandsInterface {
      * Input parameters equivalent to TS 27.007 AT+CCHO command.
      *
      * @param AID Application id. See ETSI 102.221 and 101.220.
+     * @param p2 P2 parameter (described in ISO 7816-4).
      * @param response Callback message. response.obj will be an int [1] with
      *            element [0] set to the id of the logical channel.
      */
-    public void iccOpenLogicalChannel(String AID, Message response);
+    public void iccOpenLogicalChannel(String AID, int p2, Message response);
 
     /**
      * Close a previously opened logical channel to the SIM.
