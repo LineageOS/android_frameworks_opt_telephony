28678cf38af0dd3d01f6e3df6db6b286ab11807b
Benergy Meenan Ravuri <bravuri@codeaurora.org>
2015-12-31 17:37:17 +0530

    IMS: Fix null pointer exception for call forwarding
    Check if ICCrecords are present, before setting the call forwarding
    flag in ICCrecrods.
    
    Change-Id: I7fe632acd4a9b42caf8c8bf6fee7d2cd97c41102
    CRs-Fixed: 955679

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -1525,7 +1525,10 @@ public abstract class PhoneBase extends Handler implements Phone {
 
     public void setVoiceCallForwardingFlag(int line, boolean enable, String number) {
         setCallForwardingIndicatorInSharedPref(enable);
-        mIccRecords.get().setVoiceCallForwardingFlag(line, enable, number);
+        IccRecords r = mIccRecords.get();
+        if (r != null) {
+            r.setVoiceCallForwardingFlag(line, enable, number);
+        }
     }
 
     protected void setVoiceCallForwardingFlag(IccRecords r, int line, boolean enable,
