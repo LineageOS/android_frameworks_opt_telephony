548a72d1a91700caf2e73e9c8b7a76530d6771a7
Patrick Lower <devvortex@gmail.com>
2016-01-04 11:32:17 -0500

    MMS: Update apnProfileID for MMS only apn.
    Based on system property (ro.telephony.mms_data_profile) set at device level
    
    Change-Id: I6b4ba90dfc5272e3e81330d479b9347bebfc177f

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -589,6 +589,10 @@ public class DcTracker extends Handler {
 
     public boolean mImsRegistrationState = false;
 
+    /** MMS Data Profile Device Override */
+    private static final int MMS_DATA_PROFILE = SystemProperties.getInt(
+            "ro.telephony.mms_data_profile", RILConstants.DATA_PROFILE_DEFAULT);
+
     //***** Constructor
     public DcTracker(Phone phone) {
         super();
@@ -4024,6 +4028,8 @@ public class DcTracker extends Handler {
             return RILConstants.DATA_PROFILE_DEFAULT; // DEFAULT for now
         } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_DUN)) {
             return RILConstants.DATA_PROFILE_TETHERED;
+        } else if (TextUtils.equals(apnType, PhoneConstants.APN_TYPE_MMS)) {
+            return MMS_DATA_PROFILE;
         } else {
             return RILConstants.DATA_PROFILE_DEFAULT;
         }
