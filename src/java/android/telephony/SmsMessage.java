596cfe48c1ea25c41c39f3a7197f2fb231324147
Adnan Begovic <adnan@cyngn.com>
2016-01-21 11:15:03 -0800

    Telephony: If no wrapped message, time stamp is also null.
      Similarily to I419bc3e6045b0cf7d62a348a0be02555e0f7b76e if the underlying
      createFromPdu fails to create a wrapped message properly, handle the failure
      by not bringing down the system.
    
    TICKET: CYNGNOS-1646
    Change-Id: Id5cd18b15becb78da4c3ba0f93d322361c742e9a

--- src/java/android/telephony/SmsMessage.java
+++ src/java/android/telephony/SmsMessage.java
@@ -635,7 +635,8 @@ public class SmsMessage {
      * Returns the service centre timestamp in currentTimeMillis() format
      */
     public long getTimestampMillis() {
-        return mWrappedSmsMessage.getTimestampMillis();
+        return mWrappedSmsMessage != null ? 
+                mWrappedSmsMessage.getTimestampMillis() : System.currentTimeMillis();
     }
 
     /**
