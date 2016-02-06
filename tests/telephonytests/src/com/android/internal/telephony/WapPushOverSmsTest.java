03986793ad391a2c01d1fcb4b0fabeb8492bd81a
Amit Mahajan <amitmahajan@google.com>
2016-02-05 17:48:54 -0800

    Fix for WapPushOverSmsTest
    Bug: 25691379
    Change-Id: I1fada16caf8297c988a5517f79674c40295e581a

--- tests/telephonytests/src/com/android/internal/telephony/WapPushOverSmsTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/WapPushOverSmsTest.java
@@ -51,6 +51,7 @@ public class WapPushOverSmsTest extends TelephonyTest {
     @Test @SmallTest
     public void testDispatchWapPdu() {
         doReturn(true).when(mWspTypeDecoder).decodeUintvarInteger(anyInt());
+        doReturn(true).when(mWspTypeDecoder).decodeContentType(anyInt());
         doReturn((long)2).when(mWspTypeDecoder).getValue32();
         doReturn(2).when(mWspTypeDecoder).getDecodedDataLength();
         doReturn(WspTypeDecoder.CONTENT_TYPE_B_PUSH_CO).when(mWspTypeDecoder).getValueString();
@@ -68,14 +69,28 @@ public class WapPushOverSmsTest extends TelephonyTest {
 
         ArgumentCaptor<Intent> intentArgumentCaptor = ArgumentCaptor.forClass(Intent.class);
         verify(mInboundSmsHandler).dispatchIntent(intentArgumentCaptor.capture(),
-                eq(android.Manifest.permission.RECEIVE_MMS), eq(AppOpsManager.OP_RECEIVE_MMS),
-                any(Bundle.class), isNull(BroadcastReceiver.class), eq(UserHandle.SYSTEM));
+                eq(android.Manifest.permission.RECEIVE_WAP_PUSH),
+                eq(AppOpsManager.OP_RECEIVE_WAP_PUSH),
+                any(Bundle.class),
+                isNull(BroadcastReceiver.class),
+                eq(UserHandle.SYSTEM));
         Intent intent = intentArgumentCaptor.getValue();
         assertEquals(Telephony.Sms.Intents.WAP_PUSH_DELIVER_ACTION, intent.getAction());
         assertEquals(0xFF, intent.getIntExtra("transactionId", 0));
         assertEquals(0x06, intent.getIntExtra("pduType", 0));
-        assertEquals(new byte[]{(byte) 0xFF, (byte) 0xFF}, intent.getByteArrayExtra("header"));
-        assertEquals(pdu, intent.getByteArrayExtra("data"));
+
+        byte[] header = intent.getByteArrayExtra("header");
+        assertEquals(2, header.length);
+        for (int i = 0; i < header.length; i++) {
+            assertEquals((byte)0xFF, header[i]);
+        }
+
+        byte[] data = intent.getByteArrayExtra("data");
+        assertEquals(pdu.length, data.length);
+        for (int i = 0; i < pdu.length; i++) {
+            assertEquals(pdu[i], data[i]);
+        }
+
         assertEquals(mWspTypeDecoder.getContentParameters(),
                 intent.getSerializableExtra("contentTypeParameters"));
     }
