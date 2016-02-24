1cc0b02ab5e5fc42ab94c89110d937e506deb6a7
Gabriele M <moto.falcon.git@gmail.com>
2016-02-24 17:46:49 +0100

    Allow to ignore presentation indicator of outgoing calls [2/3]
    With some mobile network operators, the presentation indicator of
    outgoing calls is always set to either "unknown" or "restricted".
    As consequence, the dialed number doesn't show up in clear in the
    call history. Allow to ignore the presentation indicator of outgoing
    calls to never hide the dialed numbers.
    
    Change-Id: Ifb5a11d3b46e22d7cdc502d447a55b307226ff71

--- src/java/com/android/internal/telephony/gsm/GsmConnection.java
+++ src/java/com/android/internal/telephony/gsm/GsmConnection.java
@@ -24,6 +24,7 @@ import android.os.PersistableBundle;
 import android.os.PowerManager;
 import android.os.Registrant;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.telephony.CarrierConfigManager;
 import android.telephony.DisconnectCause;
 import android.telephony.Rlog;
@@ -598,7 +599,12 @@ public class GsmConnection extends Connection {
 
         if (Phone.DEBUG_PHONE) log("--dssds----"+mCnapName);
         mCnapNamePresentation = dc.namePresentation;
-        mNumberPresentation = dc.numberPresentation;
+
+        boolean connectedLineIdentification =
+                Settings.Global.getInt(mOwner.mPhone.getContext().getContentResolver(),
+                        Settings.Global.CONNECTED_LINE_IDENTIFICATION, 1) != 0;
+        if (mIsIncoming || connectedLineIdentification)
+            mNumberPresentation = dc.numberPresentation;
 
         if (newParent != mParent) {
             if (mParent != null) {
