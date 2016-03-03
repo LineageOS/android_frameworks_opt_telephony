e8eb48626c3e6ad93c1839b1a9e87030c4b33ce4
Jack Yu <jackyu@google.com>
2016-03-02 22:33:15 -0800

    Moved data related unit test cases to the sub dir.
    bug: 25691379
    Change-Id: I2477dd0a16fbbac8ff79f864f0fd59f86e758b18

--- tests/telephonytests/src/com/android/internal/telephony/dataconnection/ApnSettingTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/dataconnection/ApnSettingTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.telephony;
+package com.android.internal.telephony.dataconnection;
 
 import android.test.suitebuilder.annotation.SmallTest;
 
