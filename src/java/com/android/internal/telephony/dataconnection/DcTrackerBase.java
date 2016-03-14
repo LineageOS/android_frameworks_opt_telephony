4a6e7c5d98a223cb5642107a011413d4e101c18b
Ravindra <c_rthat@codeaurora.org>
2016-03-14 16:29:35 +0530

    Fix to resolve data call issue for sim hot swap case
    Update the setting for MOBILE_DATA irrespective of mUserDataEnabled
    memeber varible because there are scenarios where the value of this
    variable will be true for dummy subId which prevents updating the
    actual value to data base.
    
    Change-Id: I3324703c6c1df76a3b5ffc505ea8239d2c1c9bad
    CRs-Fixed: 990158

--- src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -1385,16 +1385,16 @@ public abstract class DcTrackerBase extends Handler {
 
     protected void onSetUserDataEnabled(boolean enabled, int subId) {
         synchronized (mDataEnabledLock) {
+            // For single SIM phones, this is a per phone property.
+            if (TelephonyManager.getDefault().getSimCount() == 1) {
+                Settings.Global.putInt(mResolver, Settings.Global.MOBILE_DATA, enabled ? 1 : 0);
+            } else {
+                Settings.Global.putInt(mResolver, Settings.Global.MOBILE_DATA + subId,
+                        enabled ? 1 : 0);
+            }
+
             if (mUserDataEnabled != enabled) {
                 mUserDataEnabled = enabled;
-
-                // For single SIM phones, this is a per phone property.
-                if (TelephonyManager.getDefault().getSimCount() == 1) {
-                    Settings.Global.putInt(mResolver, Settings.Global.MOBILE_DATA, enabled ? 1 : 0);
-                } else {
-                    Settings.Global.putInt(mResolver, Settings.Global.MOBILE_DATA + subId,
-                            enabled ? 1 : 0);
-                }
                 if (getDataOnRoamingEnabled() == false &&
                         mPhone.getServiceState().getDataRoaming() == true) {
                     if (enabled) {
