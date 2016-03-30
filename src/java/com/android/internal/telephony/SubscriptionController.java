1100529dee51e11f6e44a38c16de3fa2a9645564
Adnan Begovic <adnan@cyngn.com>
2016-03-30 15:16:12 -0700

    telephony: Workaround default sms sub id not being set.
      In the scenario where default sms sub id isn't set externally
      the user can be stuck in an endless loop of prompts if they
      select "Always Ask". This is bandaid to fix bad UX..
    
    TICKET: CYNGNOS-2185
    Change-Id: I70ef343eccc289d0ca58c59646f88b292ca03ca9
    (cherry picked from commit e0e82e22e773461926b1ee9535946ee70f5b0330)

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -775,6 +775,19 @@ public class SubscriptionController extends ISub.Stub {
                                 setDefaultSmsSubId(subId);
                                 setDefaultVoiceSubId(subId);
                             }
+
+                            // FIXME: Workaround the scenario where default sms subid is not
+                            // being set externally
+                            // CYNGNOS-2185
+                            int phoneId = SubscriptionController.getInstance().getPhoneId(
+                                    getDefaultSmsSubId());
+                            if (phoneId < 0 || phoneId >= TelephonyManager.getDefault()
+                                    .getPhoneCount()) {
+                                Rlog.i(LOG_TAG, "Subscription is invalid. Set default to " + subId);
+                                setDefaultSmsSubId(subId);
+                                PhoneFactory.setSMSPromptEnabled(true);
+                            }
+
                         } else {
                             if (DBG) {
                                 logdl("[addSubInfoRecord] currentSubId != null"
