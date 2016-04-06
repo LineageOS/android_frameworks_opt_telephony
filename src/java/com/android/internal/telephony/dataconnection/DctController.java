fea40e5c879a0173a8822e9d44c4810602ca58cb
Susheel nyamala <snyamala@codeaurora.org>
2016-04-06 17:44:27 +0530

    Fix ims pdn request issue
    Make onSubInfoReady protected for inheritance
    
    Change-Id: Ifa8763909cf4e0d636e73fbdd952e8ce2d4eec95

--- src/java/com/android/internal/telephony/dataconnection/DctController.java
+++ src/java/com/android/internal/telephony/dataconnection/DctController.java
@@ -588,7 +588,7 @@ public class DctController extends Handler {
         return phoneId;
     }
 
-    private void onSubInfoReady() {
+    protected void onSubInfoReady() {
         logd("onSubInfoReady mPhoneNum=" + mPhoneNum);
         UiccController uiccController = UiccController.getInstance();
         for (int i = 0; i < mPhoneNum; ++i) {
