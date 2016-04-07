969ac7ae43bf89ee65f266e699e916f6eb2dd1c1
Sooraj Sasindran <ssasin@codeaurora.org>
2016-04-07 15:02:22 -0700

    MSIM: Add support for Manual Provisioning
    Add support for Manual Provisioning.
    
    CRs-Fixed: 980800
    Change-Id: I8b5fc722ec29e175f59d7bd21ed41db517112cf7

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -230,6 +230,9 @@ public class PhoneFactory {
                             sPhoneSwitcher, sc, sSubscriptionMonitor, Looper.myLooper(),
                             sContext, i, sPhones[i].mDcTracker);
                 }
+
+                telephonyComponentFactory.makeExtTelephonyClasses(
+                        context, sPhones, sCommandsInterfaces);
             }
         }
     }
