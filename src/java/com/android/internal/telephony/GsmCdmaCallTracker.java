7223dbc5ae3736b7e502fea0f094432f95955cb5
Prasath Balakrishnan <bpras@codeaurora.org>
2016-04-14 16:05:13 +0530

    Process HANGUP before DIAL response
    There is an occurance where DIAL response would be delayed
    by lower layers (like silent redial) for upto 30s. Until then,
    callId would not be available at Telephony and user triggered
    hangup is not processed.
    Connection Hangup logic is modified to accomodate above case to
    send HANGUP request to RIL with parameter '-1'.
    
    Change-Id: I4b1a0c3af0560f99db2a2c35d056817d6364817c
    CRs-Fixed: 608714

--- src/java/com/android/internal/telephony/GsmCdmaCallTracker.java
+++ src/java/com/android/internal/telephony/GsmCdmaCallTracker.java
@@ -1113,11 +1113,14 @@ public class GsmCdmaCallTracker extends CallTracker {
         }
 
         if (conn == mPendingMO) {
-            // We're hanging up an outgoing call that doesn't have it's
-            // GsmCdma index assigned yet
+            // Re-start Ecm timer when an uncompleted emergency call ends
+            if (mIsEcmTimerCanceled) {
+                handleEcmTimer(GsmCdmaPhone.RESTART_ECM_TIMER);
+            }
 
-            if (Phone.DEBUG_PHONE) log("hangup: set hangupPendingMO to true");
-            mHangupPendingMO = true;
+            // Allow HANGUP to RIL during pending MO is present
+            log("hangup conn with callId '-1' as there is no DIAL response yet ");
+            mCi.hangupConnection(-1, obtainCompleteMessage());
         } else if (!isPhoneTypeGsm()
                 && conn.getCall() == mRingingCall
                 && mRingingCall.getState() == GsmCdmaCall.State.WAITING) {
