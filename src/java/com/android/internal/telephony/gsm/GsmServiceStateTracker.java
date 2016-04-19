3d93b856e96345730ee5cd6f5f4fe7ce0a5895b2
Ricardo Cerqueira <ricardo@cyngn.com>
2016-04-19 20:57:53 +0100

    GsmSS: Use per-slot resources for carrier-bound options
    The decision of whether an MVNO operator is roaming or not comes from
    an mcc/mnc-specific resource. Relying on the global system resources
    to provide the appropriate result for each of the slots on a multi-SIM
    device is just wrong, since each SIM will have (and need) its own
    separate preferences and arrays of compatible/blocked carriers.
    
    Ref CYNGNOS-2534, CYNGNOS-3190
    
    Change-Id: If3b45a4a4702cf45bc1d965e26c8df7ef3ff783b
    (cherry picked from commit 658eea80130de80e74dee4a8a608ce419258ff5b)

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1652,8 +1652,15 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
      */
     private boolean isOperatorConsideredNonRoaming(ServiceState s) {
         String operatorNumeric = s.getOperatorNumeric();
-        String[] numericArray = mPhone.getContext().getResources().getStringArray(
+        String[] numericArray;
+        int subId = mPhone.getSubId();
+        if (subId >= 0) {
+            numericArray = SubscriptionManager.getResourcesForSubId(mPhone.getContext(),subId)
+                 .getStringArray(com.android.internal.R.array.config_operatorConsideredNonRoaming);
+        } else {
+            numericArray = mPhone.getContext().getResources().getStringArray(
                     com.android.internal.R.array.config_operatorConsideredNonRoaming);
+        }
 
         if (numericArray.length == 0 || operatorNumeric == null) {
             return false;
@@ -1669,8 +1676,16 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
 
     private boolean isOperatorConsideredRoaming(ServiceState s) {
         String operatorNumeric = s.getOperatorNumeric();
-        String[] numericArray = mPhone.getContext().getResources().getStringArray(
+        String[] numericArray;
+        int subId = mPhone.getSubId();
+        if (subId >= 0) {
+            numericArray = SubscriptionManager.getResourcesForSubId(mPhone.getContext(),subId)
+                .getStringArray(
                     com.android.internal.R.array.config_sameNamedOperatorConsideredRoaming);
+        } else {
+            numericArray = mPhone.getContext().getResources().getStringArray(
+                    com.android.internal.R.array.config_sameNamedOperatorConsideredRoaming);
+        }
 
         if (numericArray.length == 0 || operatorNumeric == null) {
             return false;
