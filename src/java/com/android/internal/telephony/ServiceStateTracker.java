71c61acd0bb993a4d5de207156bdc5effb38bd60
Ruthwar Kumar Ambeer <ruthwar@codeaurora.org>
2016-05-06 16:05:09 +0530

    Fix Acess modifiers as part of extending ServicestateTracker.
    In the process of extending ServicestateTracker class to
    QtiServiceStateTracker, Modify Access modifier types of
    few variables and method from private to protected to
    access from child class.
    
    Change-Id: I04c05d432094c4d46216fcca58f776b0e6e7863c

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -345,7 +345,7 @@ public class ServiceStateTracker extends Handler {
     };
 
     //Common
-    private GsmCdmaPhone mPhone;
+    protected GsmCdmaPhone mPhone;
     public CellLocation mCellLoc;
     private CellLocation mNewCellLoc;
     public static final int MS_PER_HOUR = 60 * 60 * 1000;
@@ -1577,7 +1577,7 @@ public class ServiceStateTracker extends Handler {
         return cdmaRoaming && !isSameOperatorNameFromSimAndSS(s);
     }
 
-    void handlePollStateResultMessage(int what, AsyncResult ar) {
+    protected void handlePollStateResultMessage(int what, AsyncResult ar) {
         int ints[];
         String states[];
         switch (what) {
