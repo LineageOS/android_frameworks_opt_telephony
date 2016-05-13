4334c4573917baf53042d6ba4f67cfbca27323a2
Mengjun Leng <mengju@codeaurora.org>
2016-05-13 10:38:39 +0800

    Telephony: Get SIM card capacity of SMS
    Add implementation to get the capacity of stored
    SMS on ICC card
    
    Change-Id: I349381bb52c34ebae2655f54049acc613bcc9b0d
    CRs-Fixed: 2177387

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -1381,6 +1381,10 @@ public class ImsPhone extends ImsPhoneBase {
         return mDefaultPhone.getPhoneId();
     }
 
+    public IccRecords getIccRecords() {
+        return mDefaultPhone.getIccRecords();
+    }
+
     private CallForwardInfo getCallForwardInfo(ImsCallForwardInfo info) {
         CallForwardInfo cfInfo = new CallForwardInfo();
         cfInfo.status = info.getStatus();
