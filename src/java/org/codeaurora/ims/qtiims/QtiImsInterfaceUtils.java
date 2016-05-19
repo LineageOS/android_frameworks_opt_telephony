8e0b6eeafda163aa014a77ea4c75dc6d906b782a
Muhammed Siju <msiju@codeaurora.org>
2016-05-19 14:28:43 +0530

    IMS-VT: Remove unused QTI_IMS_CALL_RAT_EXTRA_KEY extra.
    QTI_IMS_CALL_RAT_EXTRA_KEY is being replaced by
    ImsCallProfile.EXTRA_CALL_RAT_TYPE.
    
    Change-Id: I8e418fb1166783b35a65c010abec63e915781713
    CRs-Fixed: 1000005

--- src/java/org/codeaurora/ims/qtiims/QtiImsInterfaceUtils.java
+++ src/java/org/codeaurora/ims/qtiims/QtiImsInterfaceUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015, 2016 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -51,9 +51,6 @@ public class QtiImsInterfaceUtils {
 
     /* Default error value */
     public static final int QTI_IMS_REQUEST_ERROR = 1;
-
-    /* Call RAT extra key */
-    public static final String QTI_IMS_CALL_RAT_EXTRA_KEY = "callRadioTech";
 
     /**
      * Definitions for the call transfer type. For easier implementation,
