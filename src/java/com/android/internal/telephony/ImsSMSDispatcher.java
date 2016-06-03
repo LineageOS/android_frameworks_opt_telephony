676e73ec8586d1a8f2b471780d198e556ea95ecf
Amit Mahajan <amitmahajan@google.com>
2016-06-03 15:28:58 -0700

    Do not create SmsMessage obj if it cannot be parsed correctly.
    Bug: 29123941
    Change-Id: Ic77220c05e4a712a16ac32563ddb45a318452d71

--- src/java/com/android/internal/telephony/ImsSMSDispatcher.java
+++ src/java/com/android/internal/telephony/ImsSMSDispatcher.java
@@ -25,6 +25,7 @@ import android.os.Message;
 import android.provider.Telephony.Sms.Intents;
 import android.telephony.Rlog;
 
+import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.telephony.cdma.CdmaInboundSmsHandler;
 import com.android.internal.telephony.cdma.CdmaSMSDispatcher;
 import com.android.internal.telephony.gsm.GsmInboundSmsHandler;
@@ -208,8 +209,9 @@ public class ImsSMSDispatcher extends SMSDispatcher {
         }
     }
 
+    @VisibleForTesting
     @Override
-    protected void injectSmsPdu(byte[] pdu, String format, PendingIntent receivedIntent) {
+    public void injectSmsPdu(byte[] pdu, String format, PendingIntent receivedIntent) {
         Rlog.d(TAG, "ImsSMSDispatcher:injectSmsPdu");
         try {
             // TODO We need to decide whether we should allow injecting GSM(3gpp)
@@ -218,9 +220,14 @@ public class ImsSMSDispatcher extends SMSDispatcher {
                     android.telephony.SmsMessage.createFromPdu(pdu, format);
 
             // Only class 1 SMS are allowed to be injected.
-            if (msg.getMessageClass() != android.telephony.SmsMessage.MessageClass.CLASS_1) {
-                if (receivedIntent != null)
+            if (msg == null ||
+                    msg.getMessageClass() != android.telephony.SmsMessage.MessageClass.CLASS_1) {
+                if (msg == null) {
+                    Rlog.e(TAG, "injectSmsPdu: createFromPdu returned null");
+                }
+                if (receivedIntent != null) {
                     receivedIntent.send(Intents.RESULT_SMS_GENERIC_ERROR);
+                }
                 return;
             }
 
