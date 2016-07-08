a8f5a859f8e4a15902d729af5d2edc9a9433ba41
Jack Yu <jackyu@google.com>
2016-07-08 14:28:39 -0700

    Disable VT when users turn off data or hit data limit
    Disable VT when users turn off data or hit data limit. If
    there are ongoing VT calls, we'll need to downgrade them
    to VoLTE calls.
    
    bug: 27316521
    Change-Id: I7988d0475583e7fec50888eaee1e5a0d4d5b8cc6

--- tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
@@ -348,6 +348,7 @@ public abstract class TelephonyTest {
         doReturn(mIccCardProxy).when(mPhone).getIccCard();
         doReturn(mServiceState).when(mPhone).getServiceState();
         doReturn(mServiceState).when(mImsPhone).getServiceState();
+        doReturn(mPhone).when(mImsPhone).getDefaultPhone();
         doReturn(true).when(mPhone).isPhoneTypeGsm();
         doReturn(PhoneConstants.PHONE_TYPE_GSM).when(mPhone).getPhoneType();
         doReturn(mCT).when(mPhone).getCallTracker();
