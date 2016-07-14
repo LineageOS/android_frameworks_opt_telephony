e77e7eb8f47d4acf93663dff7691f8fbc9ecba51
Roman Birg <roman@cyngn.com>
2016-07-13 19:28:51 -0700

    DctController: fix mismerge with getTopPriorityRequestPhoneId()
    Add back logic from change-id
    I57fee028d9ac0e91d94eb70f8ea75c7fcb5f7087
    
    Change-Id: Ice8026fa75126903bdb756c663008b68fafef60b
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/java/com/android/internal/telephony/dataconnection/DctController.java
+++ src/java/com/android/internal/telephony/dataconnection/DctController.java
@@ -560,12 +560,24 @@ public class DctController extends Handler {
         int priority = -1;
         int subId;
 
+        int activePhoneId = -1;
+        for (int i=0; i<mDcSwitchStateMachine.length; i++) {
+            if (!mDcSwitchAsyncChannel[i].isIdleSync()) {
+                activePhoneId = i;
+                break;
+            }
+        }
+
         for (RequestInfo requestInfo : mRequestInfos.values()) {
             logd("getTopPriorityRequestPhoneId requestInfo=" + requestInfo);
             if (requestInfo.priority > priority) {
                 priority = requestInfo.priority;
                 topSubId = requestInfo.request.networkCapabilities.getNetworkSpecifier();
                 retRequestInfo = requestInfo;
+            } else if (priority == requestInfo.priority) {
+                if (requestInfo.executedPhoneId == activePhoneId) {
+                   topSubId = requestInfo.request.networkCapabilities.getNetworkSpecifier();
+               }
             }
         }
         if (TextUtils.isEmpty(topSubId)) {
