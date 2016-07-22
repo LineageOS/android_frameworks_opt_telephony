db5b6a50186bc3fc8d42c50f4d23c91b05412ec6
Tyler Gunn <tgunn@google.com>
2016-07-22 14:10:50 -0700

    Handle video session modify responses while paused.
    There is a limitation in the modem implementation whereby when the remote
    party has paused video (eg sent dialer to the background) any session
    modify requests which are sent are responded to, but the video state of
    the connection is NOT updated until the video state becomes unpaused
    again.
    
    To handle this, ImsPhoneConnection will now listen for session modify
    responses received via the VideoProvider.  When paused, valid
    responses to requests will be used to update the current video state of
    the ImsPhoneConnection.  When the video becomes unpaused, we will resume
    using the modem reported video state.
    
    Bug: 29381561
    Change-Id: I1ac854dbc292affb3bc0bb9ce98b2fea33014cbb

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -68,6 +68,7 @@ import com.android.ims.ImsServiceClass;
 import com.android.ims.ImsSuppServiceNotification;
 import com.android.ims.ImsUtInterface;
 import com.android.ims.internal.IImsVideoCallProvider;
+import com.android.ims.internal.ImsVideoCallProvider;
 import com.android.ims.internal.ImsVideoCallProviderWrapper;
 import com.android.internal.telephony.Call;
 import com.android.internal.telephony.CallStateException;
@@ -1452,6 +1453,15 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
             ImsPhoneConnection conn = findConnection(imsCall);
             if (DBG) log("cause = " + cause + " conn = " + conn);
 
+            if (conn != null) {
+                android.telecom.Connection.VideoProvider videoProvider = conn.getVideoProvider();
+                if (videoProvider instanceof ImsVideoCallProviderWrapper) {
+                    ImsVideoCallProviderWrapper wrapper = (ImsVideoCallProviderWrapper)
+                            videoProvider;
+
+                    wrapper.removeImsVideoProviderCallback(conn);
+                }
+            }
             if (mOnHoldToneId == System.identityHashCode(conn)) {
                 if (conn != null && mOnHoldToneStarted) {
                     mPhone.stopOnHoldTone(conn);
@@ -2325,6 +2335,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
             conn.setVideoProvider(imsVideoCallProviderWrapper);
             imsVideoCallProviderWrapper.registerForDataUsageUpdate
                     (this, EVENT_VT_DATA_USAGE_UPDATE, imsCall);
+            imsVideoCallProviderWrapper.addImsVideoProviderCallback(conn);
         }
     }
 
