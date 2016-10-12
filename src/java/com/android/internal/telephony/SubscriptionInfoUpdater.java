886320653558f9938200c441be84ac64342fd9a6
Gabriele M <moto.falcon.git@gmail.com>
2016-10-12 15:26:00 +0200

    Update carrier services on EVENT_NV_READY
    This changes ensures that CarrierConfigLoader uses the right ICC
    serial to load the right CarrierConfig and should fix CarrierService.
    
    Change-Id: Ie8daa6e0bfc06b750d38a80c439edbc3a18ada4d

--- src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
+++ src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -609,6 +609,7 @@ public class SubscriptionInfoUpdater extends Handler {
             logd("[updateSubIdForNV]+ updating");
             updateSubscriptionInfoByIccId();
         }
+        updateCarrierServices(slotId, IccCardConstants.INTENT_VALUE_ICC_LOADED);
     }
 
     protected synchronized void updateSubscriptionInfoByIccId(int slotIndex,
