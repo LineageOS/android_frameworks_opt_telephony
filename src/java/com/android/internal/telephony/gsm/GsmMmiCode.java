7f121b4aa6286f3a413ef68097912fb4b5e3641d
pkanwar <pkanwar@google.com>
2016-10-14 19:30:12 -0700

    Provide an API to make USSD calls and read the responses.
    Test: will be added in a subsequent CL.
    Bug: 30973910
    Change-Id: I0d826eaadd1bd5a0c23cfad651c0423f7b7cc5f6
    Merged-In: I0d826eaadd1bd5a0c23cfad651c0423f7b7cc5f6

--- src/java/com/android/internal/telephony/gsm/GsmMmiCode.java
+++ src/java/com/android/internal/telephony/gsm/GsmMmiCode.java
@@ -135,6 +135,7 @@ public final class GsmMmiCode extends Handler implements MmiCode {
     State mState = State.PENDING;
     CharSequence mMessage;
     private boolean mIsSsInfo = false;
+    private ResultReceiver mCallbackReceiver;
 
 
     //***** Class Variables
@@ -182,9 +183,13 @@ public final class GsmMmiCode extends Handler implements MmiCode {
      *
      * Please see flow chart in TS 22.030 6.5.3.2
      */
+    public static GsmMmiCode newFromDialString(String dialString, GsmCdmaPhone phone,
+            UiccCardApplication app) {
+        return newFromDialString(dialString, phone, app, null);
+    }
 
-    public static GsmMmiCode
-    newFromDialString(String dialString, GsmCdmaPhone phone, UiccCardApplication app) {
+    public static GsmMmiCode newFromDialString(String dialString, GsmCdmaPhone phone,
+            UiccCardApplication app, ResultReceiver wrappedCallback) {
         Matcher m;
         GsmMmiCode ret = null;
 
@@ -201,6 +206,7 @@ public final class GsmMmiCode extends Handler implements MmiCode {
             ret.mSic = makeEmptyNull(m.group(MATCH_GROUP_SIC));
             ret.mPwd = makeEmptyNull(m.group(MATCH_GROUP_PWD_CONFIRM));
             ret.mDialingNumber = makeEmptyNull(m.group(MATCH_GROUP_DIALING_NUMBER));
+            ret.mCallbackReceiver = wrappedCallback;
             // According to TS 22.030 6.5.2 "Structure of the MMI",
             // the dialing number should not ending with #.
             // The dialing number ending # is treated as unique USSD,
@@ -621,6 +627,11 @@ public final class GsmMmiCode extends Handler implements MmiCode {
         return mPoundString == null
                     && mDialingNumber != null && mDialingNumber.length() <= 2;
 
+    }
+
+    @Override
+    public String getDialString() {
+        return mPoundString;
     }
 
     static private boolean
@@ -1061,7 +1072,6 @@ public final class GsmMmiCode extends Handler implements MmiCode {
         // response does not complete this MMI code...we wait for
         // an unsolicited USSD "Notify" or "Request".
         // The matching up of this is done in GsmCdmaPhone.
-
         mPhone.mCi.sendUSSD(ussdMessage,
             obtainMessage(EVENT_USSD_COMPLETE, this));
     }
@@ -1595,6 +1605,10 @@ public final class GsmMmiCode extends Handler implements MmiCode {
             }
         }
         return sb;
+    }
+
+    public ResultReceiver getUssdCallbackReceiver() {
+        return this.mCallbackReceiver;
     }
 
     /***
