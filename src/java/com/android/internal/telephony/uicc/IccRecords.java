14545cbe38d5a17c4243a5e6113337b7bbb50a6c
Nathan Harold <nharold@google.com>
2016-10-31 16:27:34 -0700

    Add PLMN Selection Info to SIM Records
    Pull new paramters from SIMs and add them to SIMRecords
    dumpsys for debugging purposes.
    
    -Add OPLMNw/ACT
    -Add HPLMNw/ACT
    -Add FPLMN
    -Add PLMNw/ACT
    
    Bug: 32277497
    Test: none
    Change-Id: I28bd67e3667d5c6ba1046c43f24d8e02ce7b8313

--- src/java/com/android/internal/telephony/uicc/IccRecords.java
+++ src/java/com/android/internal/telephony/uicc/IccRecords.java
@@ -88,6 +88,13 @@ public abstract class IccRecords extends Handler implements IccConstants {
     protected String mGid2;
     protected String mPrefLang;
 
+    protected PlmnActRecord[] mHplmnActRecords;
+    protected PlmnActRecord[] mOplmnActRecords;
+    protected PlmnActRecord[] mPlmnActRecords;
+
+    protected String[] mEhplmns;
+    protected String[] mFplmns;
+
     private final Object mLock = new Object();
 
     // ***** Constants
