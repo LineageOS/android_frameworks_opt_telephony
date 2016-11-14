ee7aab0fa45794ea0bd074c36259d25713ea7930
fionaxu <fionaxu@google.com>
2016-11-14 13:15:23 -0800

    add SimActivationTracker for voice/data act state
    - new activationtracker module to keep track of states
    - interact with notifier/phoneStateListeners
    - add unit test for simActivationTracker
    
    Bug: 31061369
    
    Test: runtest --path frameworks/opt/telephony/tests/telephonytests/src/com/android/internal/telephony/SimActivationTrackerTest.java
    
    Merged-in: CHANGE-ID I1d12901a3aafd2e02337febeff6ebca4ee86d674
    Change-Id: I4a4ad0f36a575fedd530a70e654a9fd58c092dc0

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -47,6 +47,7 @@ import android.telephony.Rlog;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 import android.telephony.VoLteServiceState;
 import android.text.TextUtils;
 
@@ -236,7 +237,7 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
     private int mCallRingContinueToken;
     private int mCallRingDelay;
     private boolean mIsVoiceCapable = true;
-
+    private SimActivationTracker mSimActivationTracker;
     // Keep track of whether or not the phone is in Emergency Callback Mode for Phone and
     // subclasses
     protected boolean mIsPhoneInEcmState = false;
@@ -508,6 +509,7 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
         mUiccController.registerForIccChanged(this, EVENT_ICC_CHANGED, null);
         mCarrierSignalAgent = mTelephonyComponentFactory.makeCarrierSignalAgent(this);
         mCarrierActionAgent = mTelephonyComponentFactory.makeCarrierActionAgent(this);
+        mSimActivationTracker = mTelephonyComponentFactory.makeSimActivationTracker(this);
         if (getPhoneType() != PhoneConstants.PHONE_TYPE_SIP) {
             mCi.registerForSrvccStateChanged(this, EVENT_SRVCC_STATE_CHANGED, null);
         }
@@ -1530,6 +1532,32 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
     }
 
     /**
+     * Update voice activation state
+     */
+    public void setVoiceActivationState(int state) {
+        mSimActivationTracker.setVoiceActivationState(state);
+    }
+    /**
+     * Update data activation state
+     */
+    public void setDataActivationState(int state) {
+        mSimActivationTracker.setDataActivationState(state);
+    }
+
+    /**
+     * Returns voice activation state
+     */
+    public int getVoiceActivationState() {
+        return mSimActivationTracker.getVoiceActivationState();
+    }
+    /**
+     * Returns data activation state
+     */
+    public int getDataActivationState() {
+        return mSimActivationTracker.getDataActivationState();
+    }
+
+    /**
      * Update voice mail count related fields and notify listeners
      */
     public void updateVoiceMail() {
@@ -2044,6 +2072,14 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
 
     public void notifyOtaspChanged(int otaspMode) {
         mNotifier.notifyOtaspChanged(this, otaspMode);
+    }
+
+    public void notifyVoiceActivationStateChanged(int state) {
+        mNotifier.notifyVoiceActivationStateChanged(this, state);
+    }
+
+    public void notifyDataActivationStateChanged(int state) {
+        mNotifier.notifyDataActivationStateChanged(this, state);
     }
 
     public void notifySignalStrength() {
@@ -3443,6 +3479,17 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
         if (getCallTracker() != null) {
             try {
                 getCallTracker().dump(fd, pw, args);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            pw.flush();
+            pw.println("++++++++++++++++++++++++++++++++");
+        }
+
+        if (mSimActivationTracker != null) {
+            try {
+                mSimActivationTracker.dump(fd, pw, args);
             } catch (Exception e) {
                 e.printStackTrace();
             }
