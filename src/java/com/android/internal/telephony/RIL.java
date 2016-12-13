13f470a89d58ae0536798fa777ef9e14ef5a958b
Joshua Choo <joshuous@gmail.com>
2016-12-13 17:11:30 +0000

    Fix crash when importing contacts from SIM in Contacts app
    The RIL_UNSOL_PCO_DATA case will be executed unintentionally after the RIL_UNSOL_RESPONSE_ADN_RECORDS case. Add a missing break at the end of the RIL_UNSOL_RESPONSE_ADN.
    
    Change-Id: Icfe1b77521eb8323934755b554ce6e510451f8bf

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3691,6 +3691,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
                     mAdnRecordsInfoRegistrants.notifyRegistrants(
                                             new AsyncResult(null, ret, null));
                 }
+                break;
             case RIL_UNSOL_PCO_DATA:
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
