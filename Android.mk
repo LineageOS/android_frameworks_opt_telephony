126619a4dbd78767a7de304f3c3e262711d78df6
Nathan Harold <nharold@google.com>
2017-03-06 13:29:25 -0800

    Revert "Add the frameworks/services dependency to frameworks/opt"
    This reverts commit fafffe1a47bdfecb23e33ea610966dd3103ce782.
    
    Bug: 203414389
    Test: compilation aosp_sailfish, aosp_fugu
    Change-Id: I6965b04f022fd6eb762831d50a8fbbd48432022d

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src/java) \
 	$(call all-logtags-files-under, src/java) \
 	$(call all-proto-files-under, proto)
 
-LOCAL_JAVA_LIBRARIES := voip-common ims-common services
+LOCAL_JAVA_LIBRARIES := voip-common ims-common
 LOCAL_STATIC_JAVA_LIBRARIES := android.hardware.radio@1.0-java-static \
     android.hardware.radio.deprecated@1.0-java-static
 LOCAL_MODULE_TAGS := optional
