7e1fba5b0d8ab3815fdbd1e66f22ca3ab7decb83
Jayachandran C <jayachandranc@google.com>
2017-03-21 17:23:20 -0700

    Clear mAutoAttachOnCreation on Radio Off, Radio Unavailable or SIM not ready
    mAutoAttachOnCreation allows data call for nwTypeChanged after
    airplane mode toggle or radio reset but before modem actaully
    registers on service. This causes data leak on roaming networks
    for shorter time even though data roaming is disabled.
    
    Bug: 31266202
    Change-Id: Ia83146a0dafd9e2967d3e99829eea0ec08218b8a

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2415,6 +2415,8 @@ public class DcTracker extends Handler {
         cleanUpAllConnections(true, Phone.REASON_SIM_NOT_READY);
         mAllApnSettings = null;
         mAutoAttachOnCreationConfig = false;
+        // Clear auto attach as modem is expected to do a new attach once SIM is ready
+        mAutoAttachOnCreation.set(false);
     }
 
     private void onSetDependencyMet(String apnType, boolean met) {
@@ -2813,6 +2815,9 @@ public class DcTracker extends Handler {
         // next time the radio comes on
 
         mReregisterOnReconnectFailure = false;
+
+        // Clear auto attach as modem is expected to do a new attach
+        mAutoAttachOnCreation.set(false);
 
         if (mPhone.getSimulatedRadioControl() != null) {
             // Assume data is connected on the simulator
