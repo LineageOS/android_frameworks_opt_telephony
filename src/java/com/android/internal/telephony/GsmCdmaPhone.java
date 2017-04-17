LineageOS/android_frameworks_opt_telephony

commit 94ae0fe784f0701d3c3bb3c7575362dc6684300b

GsmCdmaPhone: Return dummy ICCID serial for NV sub
In case of NV sub there's no ICC, so both getIccSerialNumber()
and getFullIccSerialNumber() return null.

Since getFullIccSerialNumber() is used to set/get the ID of
each PhoneAccountHandle, devices with NV subs can't register
proper phone accounts other than the emergency one.

getIccSerialNumber() is instead used by CarrierConfigLoader
to store and load configs.

Fix both the problems forcing dummy serials for NV subscriptions.

Change-Id: I610115e9d9f5b3b86b36fddcf13dbed6387e377c

src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -142,6 +142,7 @@ public class GsmCdmaPhone extends Phone {
     };
     public static final String PROPERTY_CDMA_HOME_OPERATOR_NUMERIC =
             "ro.cdma.home.operator.numeric";
+    private static final String DUMMY_NV_ICC_SERIAL = "DUMMY_NV_ICC_SERIAL";
 
     //CDMALTE
     /** PHONE_TYPE_CDMA_LTE in addition to RuimRecords needs access to SIMRecords and
@@ -744,6 +745,9 @@ public class GsmCdmaPhone extends Phone {
 
     @Override
     public String getIccSerialNumber() {
+        if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
+            return DUMMY_NV_ICC_SERIAL;
+        }
         IccRecords r = mIccRecords.get();
         if (!isPhoneTypeGsm() && r == null) {
             // to get ICCID form SIMRecords because it is on MF.
@@ -754,6 +758,9 @@ public class GsmCdmaPhone extends Phone {
 
     @Override
     public String getFullIccSerialNumber() {
+        if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
+            return DUMMY_NV_ICC_SERIAL;
+        }
         IccRecords r = mIccRecords.get();
         if (!isPhoneTypeGsm() && r == null) {
             // to get ICCID form SIMRecords because it is on MF.

commit 71c8c9583cd58434557856b3a657ab531f491a1d

GsmCdmaPhone: Fix GSM SIM card ICCID on NV sub CDMA devices
The commit 94ae0fe784f0701d3c3bb3c7575362dc6684300b
("GsmCdmaPhone: Return dummy ICCID serial for NV sub")
broke the reported ICCID for GSM SIM cards when used
in global capable NV subscription CDMA devices.

GSM SIM ICCID shouldn't depend on the selected CDMA
subscription source in any way.

Change-Id: Ief48eba854f7c954eaa5132742fe00dc8aa9be13

src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -741,11 +741,11 @@ public class GsmCdmaPhone extends Phone {
 
     @Override
     public String getIccSerialNumber() {
-        if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
-            return DUMMY_NV_ICC_SERIAL;
-        }
         IccRecords r = mIccRecords.get();
         if (!isPhoneTypeGsm() && r == null) {
+            if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
+                return DUMMY_NV_ICC_SERIAL;
+            }
             // to get ICCID form SIMRecords because it is on MF.
             r = mUiccController.getIccRecords(mPhoneId, UiccController.APP_FAM_3GPP);
         }
@@ -754,11 +754,11 @@ public class GsmCdmaPhone extends Phone {
 
     @Override
     public String getFullIccSerialNumber() {
-        if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
-            return DUMMY_NV_ICC_SERIAL;
-        }
         IccRecords r = mIccRecords.get();
         if (!isPhoneTypeGsm() && r == null) {
+            if (mCdmaSubscriptionSource == CDMA_SUBSCRIPTION_NV) {
+                return DUMMY_NV_ICC_SERIAL;
+            }
             // to get ICCID form SIMRecords because it is on MF.
             r = mUiccController.getIccRecords(mPhoneId, UiccController.APP_FAM_3GPP);
         }