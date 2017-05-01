7c29cabeb38ae8cb4f5764c2dc0a768de22d2f92
fionaxu <fionaxu@google.com>
2017-05-01 16:13:14 -0700

    support locale change for notification channels
    - create all notification channels when phone process started to provide
    an consistent UX experience
    - support locale changes
    
    Test: Manual test with different languages
    Bug: 37911731
    Change-Id: Ib2d2bb27d307a3570923ca176f7d02f0c1985a43

--- src/java/com/android/internal/telephony/PhoneFactory.java
+++ src/java/com/android/internal/telephony/PhoneFactory.java
@@ -43,6 +43,7 @@ import com.android.internal.telephony.sip.SipPhone;
 import com.android.internal.telephony.sip.SipPhoneFactory;
 import com.android.internal.telephony.uicc.IccCardProxy;
 import com.android.internal.telephony.uicc.UiccController;
+import com.android.internal.telephony.util.NotificationChannelController;
 import com.android.internal.util.IndentingPrintWriter;
 
 import java.io.FileDescriptor;
@@ -80,6 +81,7 @@ public class PhoneFactory {
     static private SubscriptionMonitor sSubscriptionMonitor;
     static private TelephonyNetworkFactory[] sTelephonyNetworkFactories;
     static private ImsResolver sImsResolver;
+    static private NotificationChannelController sNotificationChannelController;
 
     static private final HashMap<String, LocalLog>sLocalLogs = new HashMap<String, LocalLog>();
 
@@ -231,6 +233,8 @@ public class PhoneFactory {
 
                 sProxyController = ProxyController.getInstance(context, sPhones,
                         sUiccController, sCommandsInterfaces, sPhoneSwitcher);
+
+                sNotificationChannelController = new NotificationChannelController(context);
 
                 sTelephonyNetworkFactories = new TelephonyNetworkFactory[numPhones];
                 for (int i = 0; i < numPhones; i++) {
