1efb7b36f0afeb4c8236e6fb4fe7fbf87f4dd7e6
qiongz <qiongz@codeaurora.org>
2017-06-14 15:42:56 +0800

    Fix imsi info error when insert some china operator card.
    Imsi only show mccmnc when the phone type is cdma phone since the
    length of return from RIL is 0 but not NULL for mMin. if mMin is
    NULL or length is 0, mPhone.getSubscriberId() will get NULL when
    the phone type is cdma, hence the flow of get imsi will move on
    to retrieve the imsi from IccRecords.
    
    Change-Id: Idda892c736db046d8924bb45a5fbb2a5f2a6d248
    CRs-Fixed: 2212375

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -1745,7 +1745,7 @@ public class ServiceStateTracker extends Handler {
                 .getSystemService(Context.TELEPHONY_SERVICE))
                 .getSimOperatorNumericForPhone(mPhone.getPhoneId());
 
-        if (!TextUtils.isEmpty(operatorNumeric) && getCdmaMin() != null) {
+        if (!TextUtils.isEmpty(operatorNumeric) && !TextUtils.isEmpty(getCdmaMin())) {
             return (operatorNumeric + getCdmaMin());
         } else {
             return null;
