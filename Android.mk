ffac118f2399d342c4ca4ddb5167667e52db5adf
Sooraj Sasindran <sasindran@google.com>
2017-07-20 02:43:14 -0700

    Enhance network scan request
    BUG=63865942
    
    Test: Verified by making changes in settings app to call the api
          and confirmed that the new information is delivered to vendor
          code
    Merged-In: I32a2e935f43ed66f56582a24662627dcaeacb263
    Change-Id: I32a2e935f43ed66f56582a24662627dcaeacb263

--- Android.mk
+++ Android.mk
@@ -29,6 +29,7 @@ LOCAL_JAVA_LIBRARIES := voip-common ims-common services
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android.hardware.radio-V1.0-java \
     android.hardware.radio-V1.1-java \
+    android.hardware.radio-V1.2-java \
     android.hardware.radio.deprecated-V1.0-java \
     android.hidl.base-V1.0-java
 
