7a411d4797fb60914f5f6230bde24fc25b12f920
Tyler Gunn <tgunn@google.com>
2017-08-17 08:47:08 -0700

    Support auto-downgrade of video calls when mobile data is metered.
    The previous code supported downgrading video calls (either to audio,
    or pausing video, or terminating the call, depending on carrier reqs)
    when mobile data is disabled.
    
    These changes provide support for downgrading a video call over wifi
    when it hands over to LTE (that work is in onCallHandover).  The rest
    of the changes in ImsPhoneCallTracker were to refactor the downgrade
    code so it could be reused.
    
    In ImsPhoneConnection, added support for temporarily disabling VT on the
    local side.  This way, if a wifi video call downgrades to audio-only due
    to mobile data being disabled, the "upgrade to video" button will not
    be available allowing the user to upgrade back to video (thereby defeating
    the fact data is disabled).
    
    Test: Manual
    Bug: 36900451
    Change-Id: Ib29c7b2fb31f4dcf505609bedfbaca0be7f8f265
    Merged-In: I07eb69d83a727ddd21992832753d8ecc187dbfd7

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
@@ -107,6 +107,14 @@ public class ImsPhoneConnection extends Connection implements
     private ImsRttTextHandler mRttTextHandler;
     private android.telecom.Connection.RttTextStream mRttTextStream;
 
+    /**
+     * Used as an override to determine whether video is locally available for this call.
+     * This allows video availability to be overridden in the case that the modem says video is
+     * currently available, but mobile data is off and the carrier is metering data for video
+     * calls.
+     */
+    private boolean mIsVideoEnabled = true;
+
     //***** Event Constants
     private static final int EVENT_DTMF_DONE = 1;
     private static final int EVENT_PAUSE_DONE = 2;
@@ -242,11 +250,15 @@ public class ImsPhoneConnection extends Connection implements
         return (a == null) ? (b == null) : (b != null && a.startsWith (b));
     }
 
-    private static int applyLocalCallCapabilities(ImsCallProfile localProfile, int capabilities) {
-        Rlog.w(LOG_TAG, "applyLocalCallCapabilities - localProfile = "+localProfile);
+    private int applyLocalCallCapabilities(ImsCallProfile localProfile, int capabilities) {
+        Rlog.i(LOG_TAG, "applyLocalCallCapabilities - localProfile = " + localProfile);
         capabilities = removeCapability(capabilities,
                 Connection.Capability.SUPPORTS_VT_LOCAL_BIDIRECTIONAL);
 
+        if (!mIsVideoEnabled) {
+            Rlog.i(LOG_TAG, "applyLocalCallCapabilities - disabling video (overidden)");
+            return capabilities;
+        }
         switch (localProfile.mCallType) {
             case ImsCallProfile.CALL_TYPE_VT:
                 // Fall-through
@@ -1185,6 +1197,7 @@ public class ImsPhoneConnection extends Connection implements
         Rlog.i(LOG_TAG, "ImsPhoneConnection: changeToPausedState - setting paused bit; "
                 + "newVideoState=" + VideoProfile.videoStateToString(newVideoState));
         updateVideoState(newVideoState);
+        mShouldIgnoreVideoStateChanges = true;
     }
 
     public void changeToUnPausedState() {
@@ -1192,5 +1205,17 @@ public class ImsPhoneConnection extends Connection implements
         Rlog.i(LOG_TAG, "ImsPhoneConnection: changeToUnPausedState - unsetting paused bit; "
                 + "newVideoState=" + VideoProfile.videoStateToString(newVideoState));
         updateVideoState(newVideoState);
+        mShouldIgnoreVideoStateChanges = false;
+    }
+
+    public void handleDataEnabledChange(boolean isDataEnabled) {
+        mIsVideoEnabled = isDataEnabled;
+        Rlog.i(LOG_TAG, "handleDataEnabledChange: isDataEnabled=" + isDataEnabled
+                + "; updating local video availability.");
+        updateMediaCapabilities(getImsCall());
+        if (mImsVideoCallProviderWrapper != null) {
+            mImsVideoCallProviderWrapper.setIsVideoEnabled(
+                    hasCapabilities(Connection.Capability.SUPPORTS_VT_LOCAL_BIDIRECTIONAL));
+        }
     }
 }
