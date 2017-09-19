7d97ddc2a1d84bccf6dbd422211556ce7314c915
Tyler Gunn <tgunn@google.com>
2017-09-19 11:37:44 -0700

    Expand call forwarding logging.
    Cleaned up logging code surrounding call forwarding indication changes.
    Diagnosing failures to update the CFI indicator was not possible due
    to missing logging.
    
    Test: Manually togged CFI to confirm log messages.
    Bug: 65760595
    Merged-In: I4d06c842c3d21304e1df719a6d37085781f22e21
    Change-Id: I94d546486a08547d0b1958708884b5e1725e322d

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -2548,6 +2548,7 @@ public class GsmCdmaPhone extends Phone {
     private void processIccRecordEvents(int eventCode) {
         switch (eventCode) {
             case IccRecords.EVENT_CFI:
+                logi("processIccRecordEvents: EVENT_CFI");
                 notifyCallForwardingIndicator();
                 break;
         }
