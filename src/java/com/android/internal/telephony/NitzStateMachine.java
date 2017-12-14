96ccee367104e326a975c78d48346724518ecaec
Neil Fuller <nfuller@google.com>
2017-12-14 20:33:45 +0000

    Remove phone type-specific time detection logic
    Remove more time detection logic that appears
    to treat NITZ updates differently on non-GSM
    devices but only if auto-time is disabled.
    
    The logic only runs if NITZ has been received
    so it is assumed unnecessary for device types
    that don't support NITZ.
    
    The check was added in commit ec2e4a8f12e5b6
    when Cdma/CdmaLte/Gsm ServiceStateTracker classes
    were combined.
    
    Removing the check left a redundant auto-time
    check, which has also been removed.
    
    Bug: 63743683
    Test: build / boot / see above
    Change-Id: I46f81275b3c4103b62527475a957437aa9881824

--- src/java/com/android/internal/telephony/NitzStateMachine.java
+++ src/java/com/android/internal/telephony/NitzStateMachine.java
@@ -448,7 +448,7 @@ public class NitzStateMachine {
                 long adjustedCurrentTimeMillis = newNitzData.getCurrentTimeInMillis();
                 adjustedCurrentTimeMillis += millisSinceNitzReceived;
 
-                if (!mPhone.isPhoneTypeGsm() || mTimeServiceHelper.isTimeDetectionEnabled()) {
+                if (mTimeServiceHelper.isTimeDetectionEnabled()) {
                     String tmpLog = "NITZ: newNitaData=" + newNitzData
                             + " nitzReceiveTime=" + nitzReceiveTime
                             + " Setting time of day to " + adjustedCurrentTimeMillis
@@ -459,31 +459,29 @@ public class NitzStateMachine {
                         Rlog.d(LOG_TAG, tmpLog);
                     }
                     mTimeLog.log(tmpLog);
-                    if (mTimeServiceHelper.isTimeDetectionEnabled()) {
-                        // Update system time automatically
-                        long gained = adjustedCurrentTimeMillis - System.currentTimeMillis();
-                        long timeSinceLastUpdate =
-                                mDeviceState.elapsedRealtime() - mSavedAtTime;
-                        int nitzUpdateSpacing = mDeviceState.getNitzUpdateSpacingMillis();
-                        int nitzUpdateDiff = mDeviceState.getNitzUpdateDiffMillis();
-                        if ((mSavedAtTime == 0) || (timeSinceLastUpdate > nitzUpdateSpacing)
-                                || (Math.abs(gained) > nitzUpdateDiff)) {
-                            if (DBG) {
-                                Rlog.d(LOG_TAG, "NITZ: Auto updating time of day to "
-                                        + adjustedCurrentTimeMillis
-                                        + " NITZ receive delay=" + millisSinceNitzReceived
-                                        + "ms gained=" + gained + "ms from " + newNitzData);
-                            }
-
-                            setAndBroadcastNetworkSetTime(adjustedCurrentTimeMillis);
-                        } else {
-                            if (DBG) {
-                                Rlog.d(LOG_TAG, "NITZ: ignore, a previous update was "
-                                        + timeSinceLastUpdate + "ms ago and gained="
-                                        + gained + "ms");
-                            }
-                            return;
+                    // Update system time automatically
+                    long gained = adjustedCurrentTimeMillis - System.currentTimeMillis();
+                    long timeSinceLastUpdate =
+                            mDeviceState.elapsedRealtime() - mSavedAtTime;
+                    int nitzUpdateSpacing = mDeviceState.getNitzUpdateSpacingMillis();
+                    int nitzUpdateDiff = mDeviceState.getNitzUpdateDiffMillis();
+                    if ((mSavedAtTime == 0) || (timeSinceLastUpdate > nitzUpdateSpacing)
+                            || (Math.abs(gained) > nitzUpdateDiff)) {
+                        if (DBG) {
+                            Rlog.d(LOG_TAG, "NITZ: Auto updating time of day to "
+                                    + adjustedCurrentTimeMillis
+                                    + " NITZ receive delay=" + millisSinceNitzReceived
+                                    + "ms gained=" + gained + "ms from " + newNitzData);
                         }
+
+                        setAndBroadcastNetworkSetTime(adjustedCurrentTimeMillis);
+                    } else {
+                        if (DBG) {
+                            Rlog.d(LOG_TAG, "NITZ: ignore, a previous update was "
+                                    + timeSinceLastUpdate + "ms ago and gained="
+                                    + gained + "ms");
+                        }
+                        return;
                     }
                 }
                 saveNitzTime(adjustedCurrentTimeMillis);
