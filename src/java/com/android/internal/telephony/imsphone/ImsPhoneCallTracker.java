0745a9c00d47dcfe515884900b27b50b152d0ba6
Ravi Paluri <rpaluri@codeaurora.org>
2018-01-10 15:46:20 +0530

    IMS-VT: Set whether video is enabled locally or not.
    For ViLTE calls:
    
    1. UE rejects MT upgrade requests when mobile data is
    data is turned OFF and vice-versa.
    
    2. Lower layer indicate that UE is video capable when
    mobile data is turned ON and vice-versa.
    
    In issue case, when processing mobile data toggle,
    UE is put in state indicating video is not locally
    enabled thereby rejecting MT upgrade requests though
    lower layers indicated that video is enabled.
    
    To fix this, always inform video provider whether
    video is locally enabled or not when updating media
    capabilities
    
    Bug: 72143006
    Test: Manual
    Change-Id: Iccdb5881cef10cedab8d5ff1aad82cc1a1bdb034

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -3449,6 +3449,10 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
         }
     }
 
+    public boolean isViLteDataMetered() {
+        return mIsViLteDataMetered;
+    }
+
     /**
      * Handler of data enabled changed event
      * @param enabled True if data is enabled, otherwise disabled.
