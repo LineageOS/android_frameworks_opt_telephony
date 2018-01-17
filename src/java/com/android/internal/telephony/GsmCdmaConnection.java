Dreams-Nougat/platform_frameworks_opt_telephony

Shows "no answer" when phone dials a non-answering number.
When call is failed with cause code 19, map it to
DisconnectCause.TIMED_OUT.

Test: manual
Bug: xxxxxxxx
Change-Id: Ib03b652293926a977599c449d8807771d7790165

src/java/com/android/internal/telephony/GsmCdmaConnection.java
@@ -481,6 +481,9 @@ public class GsmCdmaConnection extends Connection {
             case CallFailCause.CDMA_ACCESS_BLOCKED:
                 return DisconnectCause.CDMA_ACCESS_BLOCKED;
 
+            case CallFailCause.NO_ANSWER_FROM_USER:
+                return DisconnectCause.TIMED_OUT;
+
             case CallFailCause.ERROR_UNSPECIFIED:
             case CallFailCause.NORMAL_CLEARING:
             default:

LineageOS/android_frameworks_opt_telephony

Bug: 32007762

src/java/com/android/internal/telephony/GsmCdmaConnection.java
@@ -504,6 +504,9 @@ public class GsmCdmaConnection extends Connection {
             case CallFailCause.NORMAL_UNSPECIFIED:
                 return DisconnectCause.NORMAL_UNSPECIFIED;
 
+            case CallFailCause.USER_ALERTING_NO_ANSWER:
+                return DisconnectCause.TIMED_OUT;
+
             case CallFailCause.ERROR_UNSPECIFIED:
             case CallFailCause.NORMAL_CLEARING:
             default: