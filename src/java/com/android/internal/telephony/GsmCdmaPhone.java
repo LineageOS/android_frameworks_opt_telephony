df5c849bf7a5eb5049956f616899e6b6a06c1d5c
Sneh Bansal <snehb@codeaurora.org>
2018-01-22 15:53:53 +0530

    Telephony: Enable data call on CSIM.
    - Let DCT get mcc/mnc from phone object.
    - GsmCdmaPhone would return mcc/mnc as follow...
        - If phoneType is GSM then mcc/mnc from SimRecords
        - If phoneType is CdmaLte then
            If CdmaSubscriptionSource is NV
                mcc/mnc from ro.cdma.home.operator.numeric
            Else
                If SimRecords present then mcc/mnc from this recored.
                Else mcc/mnc from RuimRecords.
    
    - ApnSettings UI would always display list of APNs based on mcc/mnc of GsmCdmaPhone.
        - CDMA operators having LTE
            - APN UI would list LTE apn profile since mcc/mnc is from SimRecords.
        - Legacy CDMA operators
            - APN UI would be empty since such simcard would not have Sim/Usim app.
            - OEM must add profile in apns-conf.xml for such operators.
    
    CRs-Fixed: 2178887
    Change-Id: Ib8fd65eb8b18661d76a71ee24f1e732fb467d5c4

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -2656,6 +2656,7 @@ public class GsmCdmaPhone extends Phone {
                 }
                 mUiccApplication.set(newUiccApplication);
                 mIccRecords.set(newUiccApplication.getIccRecords());
+                logd("mIccRecords = " + mIccRecords);
                 registerForIccRecordEvents();
                 mIccPhoneBookIntManager.updateIccRecords(mIccRecords.get());
             }
@@ -3455,6 +3456,8 @@ public class GsmCdmaPhone extends Phone {
         pw.println("GsmCdmaPhone extends:");
         super.dump(fd, pw, args);
         pw.println(" mPrecisePhoneType=" + mPrecisePhoneType);
+        pw.println(" mSimRecords=" + mSimRecords);
+        pw.println(" mIsimUiccRecords=" + mIsimUiccRecords);
         pw.println(" mCT=" + mCT);
         pw.println(" mSST=" + mSST);
         pw.println(" mPendingMMIs=" + mPendingMMIs);
@@ -3510,7 +3513,8 @@ public class GsmCdmaPhone extends Phone {
     /**
      * @return operator numeric.
      */
-    private String getOperatorNumeric() {
+    @Override
+    public String getOperatorNumeric() {
         String operatorNumeric = null;
         if (isPhoneTypeGsm()) {
             IccRecords r = mIccRecords.get();
