7218c62d0404f2b12c29dc875b10d819fa9597f1
Sneh Bansal <snehb@codeaurora.org>
2018-01-25 12:17:37 +0530

    Telephony: Send INITIAL_ATTACH only when it is applicable.
    Issue is that SET_INITIAL_ATTACH is being sent whenever subId changes.
    
    During bootup subId does change before records gets loaded. This results
    in SET_INITIAL_ATTACH with incorrect APN setting/profile.
    
    Solution is to send SET_INITIAL_ATTACH only when cdmaSubscriptionSource
    is RUIM based and records have been loaded.
    
    Change-Id: I68280db4831935326c732943e5b763d3b3a4c3fe
    
    Fix to send initial attach apn
    
    If there is no IA/default apn then do not perform initial
    attach with any other available apns.
    
    Change-Id: Ie3241747eb44df9070f2cb4874f13878f8ca3d9b
    CRs-Fixed: 2178888

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -561,7 +561,7 @@ public class DcTracker extends Handler {
     // member variables
     private final Phone mPhone;
     private final UiccController mUiccController;
-    private final AtomicReference<IccRecords> mIccRecords = new AtomicReference<IccRecords>();
+    protected final AtomicReference<IccRecords> mIccRecords = new AtomicReference<IccRecords>();
     private DctConstants.Activity mActivity = DctConstants.Activity.NONE;
     private DctConstants.State mState = DctConstants.State.IDLE;
     private final Handler mDataConnectionTracker;
@@ -2022,6 +2022,11 @@ public class DcTracker extends Handler {
             }
         }
 
+        if ((iaApnSetting == null) && (defaultApnSetting == null) &&
+                !allowInitialAttachForOperator()) {
+            log("Abort Initial attach");
+            return;
+        }
         // The priority of apn candidates from highest to lowest is:
         //   1) APN_TYPE_IA (Initial Attach)
         //   2) mPreferredApn, i.e. the current preferred apn
@@ -2052,6 +2057,10 @@ public class DcTracker extends Handler {
                             initialAttachApnSetting.equals(getPreferredApn())),
                     mPhone.getServiceState().getDataRoamingFromRegistration(), null);
         }
+    }
+
+    protected boolean allowInitialAttachForOperator() {
+        return true;
     }
 
     /**
