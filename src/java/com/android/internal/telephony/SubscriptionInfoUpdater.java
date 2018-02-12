456c631c86d3190f19593a539c32b4bb679bee6f
Sandeep Gutta <sangutta@codeaurora.org>
2018-02-12 13:56:27 +0530

    MSIM: Perform code clean-up
    Make required changes to use vanilla implementation for
    handling SIM ERROR scenario.
    
    Change few method access specifiers and remove unused constant.
    
    Change-Id: Ifffc1cbff72316ec6885e0f4eb0a7b89dadcc96d
    CRs-Fixed: 2179165

--- src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
+++ src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -279,7 +279,7 @@ public class SubscriptionInfoUpdater extends Handler {
                 break;
 
             case EVENT_SIM_IO_ERROR:
-                handleSimAbsentOrError(msg.arg1, IccCardConstants.INTENT_VALUE_ICC_CARD_IO_ERROR);
+                handleSimError(msg.arg1);
                 break;
 
             case EVENT_SIM_RESTRICTED:
@@ -586,7 +586,7 @@ public class SubscriptionInfoUpdater extends Handler {
         }
     }
 
-    private void handleSimError(int slotId) {
+    protected void handleSimError(int slotId) {
         if (mIccId[slotId] != null && !mIccId[slotId].equals(ICCID_STRING_FOR_NO_SIM)) {
             logd("SIM" + (slotId + 1) + " Error ");
         }
