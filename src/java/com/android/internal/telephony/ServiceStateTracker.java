081bacdc40c52ea1fb099f414ff37ca0347db016
fionaxu <fionaxu@google.com>
2018-03-23 16:08:28 -0700

    DO NOT MERGE hide restricted notification from unregistered nw
    under poor cell coverage, devices might roams to other
    network with unregistered service. We should not handle
    those invalid restricted service indications from uncamped
    networks. Add condition check before showing the notification
    
    Bug: 70168086
    Test: Build
    Change-Id: I07637ccdbacff494038d4fc758904c430d7e6817

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -3587,8 +3587,23 @@ public class ServiceStateTracker extends Handler {
             // cancel previous post notification
             notificationManager.cancel(notificationId);
         } else {
+            boolean show = false;
+            if (mNewSS.isEmergencyOnly() && notifyType == CS_EMERGENCY_ENABLED) {
+                // if reg state is emergency only, always show restricted emergency notification.
+                show = true;
+            } else if (notifyType == CS_REJECT_CAUSE_ENABLED) {
+                // always show notification due to CS reject irrespective of service state.
+                show = true;
+            } else if (mNewSS.getState() == ServiceState.STATE_IN_SERVICE) {
+                // for non in service states, we have system UI and signal bar to indicate limited
+                // service. No need to show notification again. This also helps to mitigate the
+                // issue if phone go to OOS and camp to other networks and received restricted ind.
+                show = true;
+            }
             // update restricted state notification
-            notificationManager.notify(notificationId, mNotification);
+            if (show) {
+                notificationManager.notify(notificationId, mNotification);
+            }
         }
     }
 
