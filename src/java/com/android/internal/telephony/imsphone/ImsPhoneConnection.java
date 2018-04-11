cba4fcff9ce005be628ebf652f687c9449528693
Brad Ebinger <breadley@google.com>
2018-04-11 11:28:39 -0700

    Update docs for Message IPC and use Messenger.
    1) Update docs for sendDtmf usage.
    2) Update sendDtmf to include a Messenger in Message#replyTo
    for IPC communication.
    
    Bug: 77870347
    Test: Manual, Telephony Unit Tests.
    Merged-In: I457f7ff4836923f2d7319840468decca47183f80
    Change-Id: I76e965c8e231bf9d3fe97b42fccbb564378c65b6

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Messenger;
 import android.os.PersistableBundle;
 import android.os.PowerManager;
 import android.os.Registrant;
@@ -79,6 +80,7 @@ public class ImsPhoneConnection extends Connection implements
 
     private UUSInfo mUusInfo;
     private Handler mHandler;
+    private Messenger mHandlerMessenger;
 
     private PowerManager.WakeLock mPartialWakeLock;
 
@@ -171,6 +173,7 @@ public class ImsPhoneConnection extends Connection implements
 
         mOwner = ct;
         mHandler = new MyHandler(mOwner.getLooper());
+        mHandlerMessenger = new Messenger(mHandler);
         mImsCall = imsCall;
 
         if ((imsCall != null) && (imsCall.getCallProfile() != null)) {
@@ -495,7 +498,9 @@ public class ImsPhoneConnection extends Connection implements
     private boolean
     processPostDialChar(char c) {
         if (PhoneNumberUtils.is12Key(c)) {
-            mOwner.sendDtmf(c, mHandler.obtainMessage(EVENT_DTMF_DONE));
+            Message dtmfComplete = mHandler.obtainMessage(EVENT_DTMF_DONE);
+            dtmfComplete.replyTo = mHandlerMessenger;
+            mOwner.sendDtmf(c, dtmfComplete);
         } else if (c == PhoneNumberUtils.PAUSE) {
             // From TS 22.101:
             // It continues...
