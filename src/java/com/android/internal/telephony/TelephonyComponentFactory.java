70317bbe77329d389e771a2e1ffa99f9a87dec9d
TheScarastic <warabhishek@gmail.com>
2018-04-25 13:34:05 +0530

    TelephonyComponentFactory: Overload makeSubscriptionInfoUpdater
     * Required for compatibility with O-MR1 blobs
    
    Change-Id: I2eaf297b4ccaede620c099d575a694ce63a32030

--- src/java/com/android/internal/telephony/TelephonyComponentFactory.java
+++ src/java/com/android/internal/telephony/TelephonyComponentFactory.java
@@ -225,6 +225,12 @@ public class TelephonyComponentFactory {
         return new SubscriptionInfoUpdater(BackgroundThread.get().getLooper(), context, phones, ci);
     }
 
+    public SubscriptionInfoUpdater makeSubscriptionInfoUpdater(Looper looper, Context context,
+            Phone[] phones, CommandsInterface[] ci) {
+        Rlog.d(LOG_TAG, "makeSubscriptionInfoUpdater");
+        return new SubscriptionInfoUpdater(looper, context, phones, ci);
+    }
+
     public void makeExtTelephonyClasses(Context context,
             Phone[] phones, CommandsInterface[] commandsInterfaces) {
         Rlog.d(LOG_TAG, "makeExtTelephonyClasses");
