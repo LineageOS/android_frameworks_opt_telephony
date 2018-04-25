944e3114dc708d3cc39dced9549cdd440b656503
fionaxu <fionaxu@google.com>
2018-04-25 14:30:11 -0700

    DO NOT MERGE fix the merge conflict for Ratratcheter
    Bug: 65695609
    Test: runtest --path ServiceStateTrackerTest.java
    Change-Id: I7f501ba901f6ea5fa243990639429664d2f98ab8

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -2672,7 +2672,7 @@ public class ServiceStateTracker extends Handler {
         // until cell change or device is OOS
         boolean isDataInService = mNewSS.getDataRegState() == ServiceState.STATE_IN_SERVICE;
 
-        if (!hasLocationChanged && isDataInService) {
+        if (isDataInService) {
             mRatRatcheter.ratchet(mSS, mNewSS, hasLocationChanged);
         }
 
