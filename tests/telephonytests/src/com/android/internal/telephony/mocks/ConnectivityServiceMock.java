9b9e465d008a32236640d5c2b5e231befa789678
Benedict Wong <benedictwong@google.com>
2018-05-17 16:48:27 -0700

    DO NOT MERGE: Update ConnectivityServiceMock for VPN meteredness bug
    This patch updates the ConnectivityServiceMock as part of the fix for
    VPN meteredness checks.
    
    Bug: 78644887
    Test: Flashed and tested on Walleye
    Change-Id: I3d331d5f2d07e476bec4b199c695fd28755f031d
    (cherry picked from commit 3555e5d07d0ec1d7fc1ed58b0fc729283884d6fe)

--- tests/telephonytests/src/com/android/internal/telephony/mocks/ConnectivityServiceMock.java
+++ tests/telephonytests/src/com/android/internal/telephony/mocks/ConnectivityServiceMock.java
@@ -499,6 +499,11 @@ public class ConnectivityServiceMock extends IConnectivityManager.Stub
         throw new RuntimeException("not implemented");
     }
 
+    @Override
+    public boolean isActiveNetworkMeteredForUid(int uid) {
+        throw new RuntimeException("not implemented");
+    }
+
     public boolean requestRouteToHostAddress(int networkType, byte[] hostAddress) {
         throw new RuntimeException("not implemented");
     }
