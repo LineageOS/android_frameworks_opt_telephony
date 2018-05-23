e5400c3bc7efee5bfb3475f746fc4eb8284e8197
Brad Ebinger <breadley@google.com>
2018-05-23 20:22:04 +0000

    Revert "Report OUT_OF_SERVICE when service is lost but still IMS registered"
    This reverts commit 9790b2bacac12a99b1e8e17b251ef0305cdb4df4.
    
    Reason for revert: Broke VZW APM + WFC off VT use cases.
    Bug: 72483562
    Change-Id: Ib3832722b5ad8a7a90da1dd7244733c5604d1162

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -2865,6 +2865,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 public void onRegistered(
                         @ImsRegistrationImplBase.ImsRegistrationTech int imsRadioTech) {
                     if (DBG) log("onImsConnected imsRadioTech=" + imsRadioTech);
+                    mPhone.setServiceState(ServiceState.STATE_IN_SERVICE);
                     mPhone.setImsRegistered(true);
                     mMetrics.writeOnImsConnectionState(mPhone.getPhoneId(),
                             ImsConnectionState.State.CONNECTED, null);
@@ -2874,6 +2875,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 public void onRegistering(
                         @ImsRegistrationImplBase.ImsRegistrationTech int imsRadioTech) {
                     if (DBG) log("onImsProgressing imsRadioTech=" + imsRadioTech);
+                    mPhone.setServiceState(ServiceState.STATE_OUT_OF_SERVICE);
                     mPhone.setImsRegistered(false);
                     mMetrics.writeOnImsConnectionState(mPhone.getPhoneId(),
                             ImsConnectionState.State.PROGRESSING, null);
@@ -2883,6 +2885,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 public void onDeregistered(ImsReasonInfo imsReasonInfo) {
                     if (DBG) log("onImsDisconnected imsReasonInfo=" + imsReasonInfo);
                     resetImsCapabilities();
+                    mPhone.setServiceState(ServiceState.STATE_OUT_OF_SERVICE);
                     mPhone.setImsRegistered(false);
                     mPhone.processDisconnectReason(imsReasonInfo);
                     mMetrics.writeOnImsConnectionState(mPhone.getPhoneId(),
