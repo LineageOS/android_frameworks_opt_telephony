c6f0135072f879222b48b8d62964a7b6d63105a7
Sukanya Rajkhowa <sukanyar@quicinc.com>
2018-06-13 18:17:28 +0530

    IMS: Send call barring request over IMS only if UT is enabled
    In the existing Implementation, call barring request is sent
    over IMS socket either if IMS is in service or UT is enabled.
    So the request is sent over IMS even if UT is not enabled when
    IMS is in service.
    
    However modem is trying the request over CS when UT is not enabled
    and the request is failing since password is not passed for the
    call barring requests sent over IMS.
    
    Test: Manual
    Bug: 110187445
    Change-Id: I0b9e2bc7dac381ac1d00887757954465c1beddc3

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -1856,9 +1856,7 @@ public class GsmCdmaPhone extends Phone {
             int serviceClass) {
         if (isPhoneTypeGsm()) {
             Phone imsPhone = mImsPhone;
-            if ((imsPhone != null)
-                    && ((imsPhone.getServiceState().getState() == ServiceState.STATE_IN_SERVICE)
-                    || imsPhone.isUtEnabled())) {
+            if ((imsPhone != null) && imsPhone.isUtEnabled()) {
                 imsPhone.getCallBarring(facility, password, onComplete, serviceClass);
                 return;
             }
@@ -1873,9 +1871,7 @@ public class GsmCdmaPhone extends Phone {
             Message onComplete, int serviceClass) {
         if (isPhoneTypeGsm()) {
             Phone imsPhone = mImsPhone;
-            if ((imsPhone != null)
-                    && ((imsPhone.getServiceState().getState() == ServiceState.STATE_IN_SERVICE)
-                    || imsPhone.isUtEnabled())) {
+            if ((imsPhone != null) && imsPhone.isUtEnabled()) {
                 imsPhone.setCallBarring(facility, lockState, password, onComplete, serviceClass);
                 return;
             }
