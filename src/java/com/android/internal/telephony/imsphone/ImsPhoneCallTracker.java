8faa098be97968f748d99c482764863e519f9fc8
Wileen Chiu <wileenc@quicinc.com>
2018-07-01 14:07:40 -0700

    IMS: Propagate media profile changed message
    - RTT Voice information will be passed to the Ui via
      the media profile changed message
    
    - We fire a connection event EVENT_RTT_AUDIO_INDICATION_CHANGED
      to notify the UI (DialerCall.java) of the new audio information
    
    Test: Manual
    Bug: 110976450
    Change-Id: I01b3bf1a791f8af85ff1f1cdf7107bc7bf8a88db

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -2951,6 +2951,14 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
             }
         }
 
+        @Override
+        public void onRttAudioIndicatorChanged(ImsCall imsCall, ImsStreamMediaProfile profile) {
+          ImsPhoneConnection conn = findConnection(imsCall);
+            if (conn != null) {
+                conn.onRttAudioIndicatorChanged(profile);
+            }
+        }
+
         /**
          * Handles a change to the multiparty state for an {@code ImsCall}.  Notifies the associated
          * {@link ImsPhoneConnection} of the change.
--- src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
@@ -1040,6 +1040,14 @@ public class ImsPhoneConnection extends Connection implements
         mRttTextHandler.sendToInCall(message);
     }
 
+    public void onRttAudioIndicatorChanged(ImsStreamMediaProfile profile) {
+        Bundle extras = new Bundle();
+        extras.putBoolean(android.telecom.Connection.EXTRA_IS_RTT_AUDIO_PRESENT,
+                profile.getRttAudioSpeech());
+        onConnectionEvent(android.telecom.Connection.EVENT_RTT_AUDIO_INDICATION_CHANGED,
+                extras);
+    }
+
     public void setCurrentRttTextStream(android.telecom.Connection.RttTextStream rttTextStream) {
         synchronized (this) {
             mRttTextStream = rttTextStream;
