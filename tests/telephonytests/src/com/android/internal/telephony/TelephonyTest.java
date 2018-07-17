2a3a9846fdbe3711726ba5e270a34aa2a9b75f39
Tyler Gunn <tgunn@google.com>
2018-07-17 13:55:27 -0700

    Handle access class denials when an IMS call disconnects.
    Handle CS_RESTRICTED, CS_RESTRICTED_EMERGENCY, CS_RESTRICTED_NORMAL
    disconnect reasons for IMS calls.
    
    Bug: 36163492
    Test: Added new unit tests for these cases.
    Change-Id: I62a1f4ff53cdd54985343503cce089b48efc6914

--- tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
@@ -198,6 +198,8 @@ public abstract class TelephonyTest {
     protected SubscriptionInfoUpdater mSubInfoRecordUpdater;
     @Mock
     protected LocaleTracker mLocaleTracker;
+    @Mock
+    protected RestrictedState mRestrictedState;
 
     protected ImsCallProfile mImsCallProfile;
     protected TelephonyManager mTelephonyManager;
@@ -458,6 +460,7 @@ public abstract class TelephonyTest {
                 nullable(Intent[].class), nullable(String[].class), anyInt(),
                 nullable(Bundle.class), anyInt());
         mSST.mSS = mServiceState;
+        mSST.mRestrictedState = mRestrictedState;
         mServiceManagerMockedServices.put("connectivity_metrics_logger", mConnMetLoggerBinder);
 
         //SIM
