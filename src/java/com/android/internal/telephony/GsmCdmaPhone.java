dc2df9c83180aa53c147d939a0d9219bd3a56d10
Lalit Maganti <lalitm@google.com>
2018-09-27 10:33:38 +0000

    Revert "new TelephonyManager API for preferred network mode"
    This reverts commit 73517c1fbab3a4c70d27d707597a7a790f89c45b.
    
    Reason for revert: Reason for revert: Causing failures on git_pi-dev-plus-aosp for docs and aosp_sailfish.
    
    Change-Id: Ic468c614d3155623cb8394a689e82bea48751817
    Merged-In: I902510e04a2a1e6fcc64320dc739ba8b7ea9d9f8

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -2845,8 +2845,8 @@ public class GsmCdmaPhone extends Phone {
          *  selection is disallowed. So we should force auto select mode.
          */
         if (isManualSelProhibitedInGlobalMode()
-                && ((nwMode == TelephonyManager.NETWORK_MODE_LTE_CDMA_EVDO_GSM_WCDMA)
-                        || (nwMode == TelephonyManager.NETWORK_MODE_GLOBAL)) ){
+                && ((nwMode == Phone.NT_MODE_LTE_CDMA_EVDO_GSM_WCDMA)
+                        || (nwMode == Phone.NT_MODE_GLOBAL)) ){
             logd("Should force auto network select mode = " + nwMode);
             return true;
         } else {
