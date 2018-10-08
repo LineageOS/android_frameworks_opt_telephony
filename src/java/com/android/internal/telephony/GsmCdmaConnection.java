b33949e92f63578ce897271fd9f42806d4d2b24f
Umashankar Godachi <umasha@codeaurora.org>
2018-10-08 15:57:47 +0530

    Fix to release partial wakelock for SRVCC handover.
    During SRVCC handover partial wakelock is acquired while
    creating new GsmCdmaConnection, but is not released till
    the call is disconnected, leading to Application processor
    not going to sleep.
    
    Fix: Release wakelock considering the ACTIVE,HOLDING pre
         -handover states.
    
    Change-Id: I650a1a9bb7df16ebf2e6803635d38bad5db7c56c
    CRs-Fixed: 2328958

--- src/java/com/android/internal/telephony/GsmCdmaConnection.java
+++ src/java/com/android/internal/telephony/GsmCdmaConnection.java
@@ -1039,7 +1039,7 @@ public class GsmCdmaConnection extends Connection {
         }
     }
 
-    private void releaseWakeLock() {
+    void releaseWakeLock() {
         if (mPartialWakeLock != null) {
             synchronized (mPartialWakeLock) {
                 if (mPartialWakeLock.isHeld()) {
