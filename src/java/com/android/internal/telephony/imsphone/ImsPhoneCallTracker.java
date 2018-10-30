641a245e42dafc62b5664f48be71544d96405232
Ashit Sood <sooda@codeaurora.org>
2018-10-30 11:03:12 -0700

    IMS: Update disconnect reason for IMS calls.
    - Updating disconnect reason when IMS calls
      are disconnected due to access class
      denials.
    
    Change-Id: I51e68ae3fa04042c6bf46d7238da6dac99092109
    CRs-Fixed: 2338994

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -2154,7 +2154,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
             case ImsReasonInfo.CODE_UNOBTAINABLE_NUMBER:
                 return DisconnectCause.UNOBTAINABLE_NUMBER;
 
-            case ImsReasonInfo.CODE_UNSPECIFIED:
+            case ImsReasonInfo.CODE_RADIO_INTERNAL_ERROR:
                 if (mPhone.getDefaultPhone().getServiceStateTracker().mRestrictedState
                         .isCsRestricted()) {
                     return DisconnectCause.CS_RESTRICTED;
