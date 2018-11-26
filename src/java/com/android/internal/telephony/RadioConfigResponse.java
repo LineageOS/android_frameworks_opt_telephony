fe2ca33758d8b5733a03c395dc97c4ed2b1e3d34
Malcolm Chen <refuhoo@google.com>
2018-11-26 09:05:36 -0800

    Migration of IRadioConfig 1.1 to 1.2.
    To better test CBRS, we want IRadioConfig 1.1 to be Android P
    plus CBRS HAL interfaces, while 1.2 will be 1.1 plus all other
    Android Q interfaces. So we are moving everything currently
    defined in android.hardware.radio.config.V1_1 to V1_2.
    
    Bug: 117805040
    Test: build and telephony unittest
    Change-Id: I520fd666ceb14a0e742e1949d2cf1a7a69debc27

--- src/java/com/android/internal/telephony/RadioConfigResponse.java
+++ src/java/com/android/internal/telephony/RadioConfigResponse.java
@@ -18,7 +18,7 @@ package com.android.internal.telephony;
 
 import android.hardware.radio.V1_0.RadioError;
 import android.hardware.radio.V1_0.RadioResponseInfo;
-import android.hardware.radio.config.V1_1.IRadioConfigResponse;
+import android.hardware.radio.config.V1_2.IRadioConfigResponse;
 import android.telephony.Rlog;
 
 import com.android.internal.telephony.uicc.IccSlotStatus;
@@ -65,12 +65,12 @@ public class RadioConfigResponse extends IRadioConfigResponse.Stub {
     /**
      * Response function for IRadioConfig.getSimSlotsStatus().
      */
-    public void getSimSlotsStatusResponse_1_1(RadioResponseInfo responseInfo,
-            ArrayList<android.hardware.radio.config.V1_1.SimSlotStatus> slotStatus) {
+    public void getSimSlotsStatusResponse_1_2(RadioResponseInfo responseInfo,
+            ArrayList<android.hardware.radio.config.V1_2.SimSlotStatus> slotStatus) {
         RILRequest rr = mRadioConfig.processResponse(responseInfo);
 
         if (rr != null) {
-            ArrayList<IccSlotStatus> ret = RadioConfig.convertHalSlotStatus_1_1(slotStatus);
+            ArrayList<IccSlotStatus> ret = RadioConfig.convertHalSlotStatus_1_2(slotStatus);
             if (responseInfo.error == RadioError.NONE) {
                 // send response
                 RadioResponse.sendMessageResponse(rr.mResult, ret);
@@ -83,7 +83,7 @@ public class RadioConfigResponse extends IRadioConfigResponse.Stub {
                         + responseInfo.error);
             }
         } else {
-            Rlog.e(TAG, "getSimSlotsStatusResponse_1_1: Error " + responseInfo.toString());
+            Rlog.e(TAG, "getSimSlotsStatusResponse_1_2: Error " + responseInfo.toString());
         }
     }
 
