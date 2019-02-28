939dbd7101bd4bcb00b65f29310394cd4a9acfc8
Pengfei Xiao <pengfeix@codeaurora.org>
2019-02-28 11:34:01 +0800

    Set service class with SERVICE_CLASS_NONE over CS
    Change-Id: I7eeba79a1631fe8609f3735344b95d5b49315f04
    CRs-Fixed: 2406871

--- src/java/com/android/internal/telephony/GsmCdmaPhone.java
+++ src/java/com/android/internal/telephony/GsmCdmaPhone.java
@@ -1991,7 +1991,8 @@ public class GsmCdmaPhone extends Phone {
                 imsPhone.getCallBarring(facility, password, onComplete, serviceClass);
                 return;
             }
-            mCi.queryFacilityLock(facility, password, serviceClass, onComplete);
+            mCi.queryFacilityLock(facility, password,
+                    CommandsInterface.SERVICE_CLASS_NONE, onComplete);
         } else {
             loge("getCallBarringOption: not possible in CDMA");
         }
@@ -2006,7 +2007,8 @@ public class GsmCdmaPhone extends Phone {
                 imsPhone.setCallBarring(facility, lockState, password, onComplete, serviceClass);
                 return;
             }
-            mCi.setFacilityLock(facility, lockState, password, serviceClass, onComplete);
+            mCi.setFacilityLock(facility, lockState, password,
+                    CommandsInterface.SERVICE_CLASS_NONE, onComplete);
         } else {
             loge("setCallBarringOption: not possible in CDMA");
         }
