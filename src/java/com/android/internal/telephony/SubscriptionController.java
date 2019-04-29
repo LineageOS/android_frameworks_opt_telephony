c2179e46390d5740316b192caca8600384d1fe9f
Sandeep Gutta <sangutta@codeaurora.org>
2019-04-29 14:17:22 +0530

    Use appropriate method to query UiccCard object
    Use getUiccCardForPhone(), the appropriate method to query the
    UiccCard object by using the PhoneId as input value.
    
    Change-Id: Iacd5d23ab2584c5515cbccbba550d6fbce194e95
    CRs-Fixed: 2440889

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -999,7 +999,7 @@ public class SubscriptionController extends ISub.Stub {
             int subscriptionType) {
         String fullIccId;
         Phone phone = PhoneFactory.getPhone(slotIndex);
-        UiccCard uiccCard = UiccController.getInstance().getUiccCardForSlot(slotIndex);
+        UiccCard uiccCard = UiccController.getInstance().getUiccCardForPhone(slotIndex);
         if (phone != null && uiccCard != null) {
             fullIccId = phone.getFullIccSerialNumber();
             if (TextUtils.isEmpty(fullIccId)) {
