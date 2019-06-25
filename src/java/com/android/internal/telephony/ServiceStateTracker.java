ab2abcb59fe63850837ea78c69b994d4f826b61f
Jordan Liu <jminjie@google.com>
2019-06-25 16:36:19 -0700

    Use locale to decide out-of-service message
    Instead of using the resource with mcc/mnc overlay (which doesn't work
    when no SIM is inserted), we use LocaleTracker and a new resource which
    is a list of the countries where the out-of-service message is
    no-service. (e.g. Germany does not allow emergency calls without a SIM).
    
    Bug: 126651774
    Test: atest ServiceStateTrackerTest
    Merged-In: I1887f3c72a594b61f45867332e1c4bce62b41612
    Change-Id: I1887f3c72a594b61f45867332e1c4bce62b41612
    (cherry picked from commit e8aab0f2bda68b5a3258e16251ed22f3aa078ee3)

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -2626,9 +2626,7 @@ public class ServiceStateTracker extends Handler {
                 showPlmn = true;
 
                 // Force display no service
-                final boolean forceDisplayNoService = mPhone.getContext().getResources().getBoolean(
-                        com.android.internal.R.bool.config_display_no_service_when_sim_unready)
-                        && !mIsSimReady;
+                final boolean forceDisplayNoService = shouldForceDisplayNoService() && !mIsSimReady;
                 if (!forceDisplayNoService && Phone.isEmergencyCallOnly()) {
                     // No service but emergency call allowed
                     plmn = Resources.getSystem().
@@ -2737,6 +2735,28 @@ public class ServiceStateTracker extends Handler {
                 .setShowPlmn(showPlmn)
                 .build());
         log("updateSpnDisplayLegacy-");
+    }
+
+    /**
+     * Checks whether force to display "no service" to the user based on the current country.
+     *
+     * This method should only be used when SIM is unready.
+     *
+     * @return {@code True} if "no service" should be displayed.
+     */
+    public boolean shouldForceDisplayNoService() {
+        String[] countriesWithNoService = mPhone.getContext().getResources().getStringArray(
+                com.android.internal.R.array.config_display_no_service_when_sim_unready);
+        if (ArrayUtils.isEmpty(countriesWithNoService)) {
+            return false;
+        }
+        String currentCountry = mLocaleTracker.getCurrentCountry();
+        for (String country : countriesWithNoService) {
+            if (country.equalsIgnoreCase(currentCountry)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     protected void setPowerStateToDesired() {
