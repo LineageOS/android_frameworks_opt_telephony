13de460571287299cc194b75b64e1568f23ff056
Mengjun Leng <mengju@codeaurora.org>
2019-07-10 10:54:41 +0800

    Make method setDataProfilesAsNeeded as protected
    Modify API setDataProfilesAsNeeded as a protected method, so that
    the injection of DcTracker can reach it.
    
    Change-Id: I0b0999787a6316fc94e69c617b00af1245a24676
    CRs-Fixed: 2487878

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -3081,7 +3081,7 @@ public class DcTracker extends Handler {
         return true;
     }
 
-    private void setDataProfilesAsNeeded() {
+    protected void setDataProfilesAsNeeded() {
         if (DBG) log("setDataProfilesAsNeeded");
 
         ArrayList<DataProfile> dataProfileList = new ArrayList<>();
