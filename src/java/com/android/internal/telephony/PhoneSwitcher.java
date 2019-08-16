97f3b4e0a9b9d3ec6e9f526c39b3a8389a7dd227
Susheel Nyamala <snyamala@codeaurora.org>
2019-08-16 18:03:03 +0530

    Add support for overriding methods from PhoneSwitcher
    Grant protected access to methods which are extended
    
    Change-Id: I88868b4cbc37edaec30f3e6c79542ddc55803ba8
    CRs-Fixed: 2522678

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -1054,7 +1054,7 @@ public class PhoneSwitcher extends Handler {
         mPreferredDataSubId = mSubscriptionController.getSubIdUsingPhoneId(mPreferredDataPhoneId);
     }
 
-    private void transitionToEmergencyPhone() {
+    protected void transitionToEmergencyPhone() {
         if (mPreferredDataPhoneId != DEFAULT_EMERGENCY_PHONE_ID) {
             log("No active subscriptions: resetting preferred phone to 0 for emergency");
             mPreferredDataPhoneId = DEFAULT_EMERGENCY_PHONE_ID;
@@ -1265,10 +1265,6 @@ public class PhoneSwitcher extends Handler {
         return (phone.getForegroundCall().getState() == Call.State.ACTIVE
                 || phone.getForegroundCall().getState() == Call.State.ALERTING
                 || phone.getBackgroundCall().getState() == Call.State.HOLDING);
-    }
-
-    protected boolean isCallActive(int phoneId) {
-        return false;
     }
 
     private void updateHalCommandToUse() {
