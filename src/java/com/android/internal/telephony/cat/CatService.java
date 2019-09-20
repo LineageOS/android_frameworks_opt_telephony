4668b242fb4af425365b9095393a81194c7ae948
Malcolm Chen <refuhoo@google.com>
2019-09-19 18:31:51 -0700

    Replace getPhoneCount with getMaxPhoneCount upon object allocation.
    As first step for smooth single SIM to DSDS switch, for DSDS capable
    deviced we always allocate objects as if it's in DSDS mode. For example
    there will be two Phone objects.
    Later we'll evaluate to make the allocations dynamic to save memory.
    
    Bug: 141388730
    Test: unittest and manual
    Change-Id: I9a99853a22de0a4a78fd4ca622faddcdba3f8e06
    Merged-In: I9a99853a22de0a4a78fd4ca622faddcdba3f8e06

--- src/java/com/android/internal/telephony/cat/CatService.java
+++ src/java/com/android/internal/telephony/cat/CatService.java
@@ -221,7 +221,7 @@ public class CatService extends Handler implements AppInterface {
 
         synchronized (sInstanceLock) {
             if (sInstance == null) {
-                int simCount = TelephonyManager.getDefault().getSimCount();
+                int simCount = TelephonyManager.getDefault().getMaxPhoneCount();
                 sInstance = new CatService[simCount];
                 for (int i = 0; i < simCount; i++) {
                     sInstance[i] = null;
