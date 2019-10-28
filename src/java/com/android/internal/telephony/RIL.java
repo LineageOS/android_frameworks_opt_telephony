1c2c0c380a445f51182f600199f7c33a8ed82127
Arne Coucheron <arco68@gmail.com>
2019-10-29 00:26:15 +0100

    2G wants proper signal strength too
    Change-Id: Ibe3acd8e214075199127b4c114a222b26aaf8498

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -6681,11 +6681,14 @@ public class RIL extends BaseCommands implements CommandsInterface {
             }
         }
         switch (voiceRat) {
+            case ServiceState.RIL_RADIO_TECHNOLOGY_GPRS: /* fallthrough */
+            case ServiceState.RIL_RADIO_TECHNOLOGY_EDGE: /* fallthrough */
             case ServiceState.RIL_RADIO_TECHNOLOGY_UMTS: /* fallthrough */
             case ServiceState.RIL_RADIO_TECHNOLOGY_HSDPA: /* fallthrough */
             case ServiceState.RIL_RADIO_TECHNOLOGY_HSUPA: /* fallthrough */
             case ServiceState.RIL_RADIO_TECHNOLOGY_HSPA: /* fallthrough */
             case ServiceState.RIL_RADIO_TECHNOLOGY_HSPAP: /* fallthrough */
+            case ServiceState.RIL_RADIO_TECHNOLOGY_GSM: /* fallthrough */
                 break;
             default:
                 // If we are not currently on WCDMA/HSPA, then we don't need to do a fixup.
