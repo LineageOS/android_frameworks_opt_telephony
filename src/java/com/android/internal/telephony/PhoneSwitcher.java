d12620b08be09838f7614e06b8920cdfcd963217
Alex Koskovich <zvnexus@outlook.com>
2019-10-30 17:55:19 -0400

    Restore getSubIdFromNetworkRequest method
    Commit 856a6604da2f1001aef0dcaa8906a987a559e0af removed it but
    QTI telephony still requires this method to be present.
    
    Change-Id: I5ee25f3337dfd1958ba16340f25712ddd9486857

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -999,6 +999,11 @@ public class PhoneSwitcher extends Handler {
         return phoneId;
     }
 
+    protected int getSubIdFromNetworkRequest(NetworkRequest networkRequest) {
+        NetworkSpecifier specifier = networkRequest.networkCapabilities.getNetworkSpecifier();
+        return getSubIdFromNetworkSpecifier(specifier);
+    }
+
     protected int getSubIdFromNetworkSpecifier(NetworkSpecifier specifier) {
         if (specifier == null) {
             return DEFAULT_SUBSCRIPTION_ID;
