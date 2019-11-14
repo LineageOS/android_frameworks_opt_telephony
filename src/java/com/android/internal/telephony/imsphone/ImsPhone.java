ee2afb34c1c14a5a46748c096cdcf7da212e3fad
Shuo Qian <shuoq@google.com>
2019-11-13 17:42:45 -0800

    Add test function for emergency number db update
    1) get emergency number db version
    2) override emergency db file partition
    
    Test: atest TelephonyHostTest#testEmergencyNumberDb
    (Though the condition is RVC, I manually triggered all the running with QT build,
     and ensured each Assert is hit)
    https://paste.googleplex.com/4679407325151232
    Bug: 136027884
    
    Change-Id: I3a2f5a668d84804c80f94e2227c8e0d6c51e5153

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -242,6 +242,11 @@ public class ImsPhone extends ImsPhoneBase {
     }
 
     @Override
+    public int getEmergencyNumberDbVersion() {
+        return getEmergencyNumberTracker().getEmergencyNumberDbVersion();
+    }
+
+    @Override
     public EmergencyNumberTracker getEmergencyNumberTracker() {
         return mDefaultPhone.getEmergencyNumberTracker();
     }
