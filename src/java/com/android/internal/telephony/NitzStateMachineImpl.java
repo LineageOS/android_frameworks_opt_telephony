8fe9e371dc5e45fd18b8b5d49c50f3dc5734674b
Neil Fuller <nfuller@google.com>
2019-12-05 13:13:05 +0000

    Remove broadcast of NETWORK_SET_TIMEZONE intent
    android.intent.action.NETWORK_SET_TIMEZONE is not used by anything in
    the platform so it can be removed. It was previously broadcast when
    telephony made a time zone change, presumably as a way to coordinate
    time zone detection logic if there were other, lower-priority detection
    mechanisms on the device.
    
    All the APK usages appear to be only manifest entries for
    <protected-broadcast> in apps that don't send the intent themselves,
    which doesn't make a lot of sense.
    
    Note: There is a separate commit to remove the broadcast from the system
    server time zone detection logic.
    
    Bug: 140712361
    Test: treehugger
    Change-Id: I5a84580620dc74c1ab1f73a56379c2256f13ef0e

--- src/java/com/android/internal/telephony/NitzStateMachineImpl.java
+++ src/java/com/android/internal/telephony/NitzStateMachineImpl.java
@@ -253,7 +253,7 @@ public final class NitzStateMachineImpl implements NitzStateMachine {
             // Set state as needed.
             if (zoneId != null) {
                 if (mTimeServiceHelper.isTimeZoneDetectionEnabled()) {
-                    setAndBroadcastNetworkSetTimeZone(zoneId, logMsg);
+                    setTimeZone(zoneId, logMsg);
                 } else {
                     if (DBG) {
                         logMsg += " [Not setting device time zone]";
@@ -501,7 +501,7 @@ public final class NitzStateMachineImpl implements NitzStateMachine {
         }
     }
 
-    private void setAndBroadcastNetworkSetTimeZone(String zoneId, String logMessage) {
+    private void setTimeZone(String zoneId, String logMessage) {
         logMessage += " [Setting device time zone to zoneId=" + zoneId + "]";
         if (DBG) {
             Rlog.d(LOG_TAG, logMessage);
@@ -528,7 +528,7 @@ public final class NitzStateMachineImpl implements NitzStateMachine {
         String logMsg = "handleAutoTimeZoneEnabled: "
                 + " mSavedTimeZoneId=" + mSavedTimeZoneId;
         if (mSavedTimeZoneId != null) {
-            setAndBroadcastNetworkSetTimeZone(mSavedTimeZoneId, logMsg);
+            setTimeZone(mSavedTimeZoneId, logMsg);
         } else {
             if (DBG) {
                 logMsg += " [Not setting device time zone]";
@@ -587,7 +587,7 @@ public final class NitzStateMachineImpl implements NitzStateMachine {
 
             String zoneId = lookupResult.zoneId;
             if (mTimeServiceHelper.isTimeZoneDetectionEnabled()) {
-                setAndBroadcastNetworkSetTimeZone(zoneId, logMsg);
+                setTimeZone(zoneId, logMsg);
             } else {
                 if (DBG) {
                     logMsg += " [Not setting device time zone]";
