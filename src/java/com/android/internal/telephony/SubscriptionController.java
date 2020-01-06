a97c13bf2472321ac2bb32bf5dff045612cabf59
Shuo Qian <shuoq@google.com>
2020-01-06 13:55:14 -0800

    Change the permission for getActiveSubscriptionInfoForIccId for system API
    Test: cts
    Bug: 122015219
    Change-Id: I451ed3bd331fa42141c24c2950637b183c7f995c

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -557,19 +557,8 @@ public class SubscriptionController extends ISub.Stub {
     @Override
     public SubscriptionInfo getActiveSubscriptionInfoForIccId(String iccId, String callingPackage,
             String callingFeatureId) {
-        // Query the subscriptions unconditionally, and then check whether the caller has access to
-        // the given subscription.
-        final SubscriptionInfo si = getActiveSubscriptionInfoForIccIdInternal(iccId);
-
-        final int subId = si != null
-                ? si.getSubscriptionId() : SubscriptionManager.INVALID_SUBSCRIPTION_ID;
-        if (!TelephonyPermissions.checkCallingOrSelfReadPhoneState(
-                mContext, subId, callingPackage, callingFeatureId,
-                "getActiveSubscriptionInfoForIccId")) {
-            return null;
-        }
-
-        return si;
+        enforceReadPrivilegedPhoneState("getActiveSubscriptionInfoForIccId");
+        return getActiveSubscriptionInfoForIccIdInternal(iccId);
     }
 
     /**
