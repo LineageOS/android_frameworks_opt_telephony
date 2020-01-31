b60e98825da6dcf41f2c11bb578bc372fc641077
yinxu <yinxu@google.com>
2020-01-31 10:34:03 -0800

    Remove the usage of hidden APIs from NetworkInfo
    Bug: 148674994
    Test: Telephony Unit Tests
    Change-Id: I635fc707b614eddef320cdc84c83a05840f95977

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -1655,7 +1655,6 @@ public class DataConnection extends StateMachine {
                                 + " drs=" + mDataRegState
                                 + " mRilRat=" + mRilRat);
                     }
-                    updateNetworkInfo();
                     updateNetworkInfoSuspendState();
                     if (mNetworkAgent != null) {
                         mNetworkAgent.sendNetworkCapabilities(getNetworkCapabilities(),
@@ -1668,7 +1667,6 @@ public class DataConnection extends StateMachine {
                 case EVENT_DATA_CONNECTION_ROAM_OFF:
                 case EVENT_DATA_CONNECTION_METEREDNESS_CHANGED:
                 case EVENT_DATA_CONNECTION_OVERRIDE_CHANGED:
-                    updateNetworkInfo();
                     if (mNetworkAgent != null) {
                         mNetworkAgent.sendNetworkCapabilities(getNetworkCapabilities(),
                                 DataConnection.this);
@@ -1691,19 +1689,6 @@ public class DataConnection extends StateMachine {
 
             return retVal;
         }
-    }
-
-    private void updateNetworkInfo() {
-        final ServiceState state = mPhone.getServiceState();
-
-        NetworkRegistrationInfo nri = state.getNetworkRegistrationInfo(
-                NetworkRegistrationInfo.DOMAIN_PS, mTransportType);
-        int subtype = TelephonyManager.NETWORK_TYPE_UNKNOWN;
-        if (nri != null) {
-            subtype = nri.getAccessNetworkTechnology();
-        }
-
-        mNetworkInfo.setSubtype(subtype, TelephonyManager.getNetworkTypeName(subtype));
     }
 
     private void updateNetworkInfoSuspendState() {
@@ -2037,8 +2022,6 @@ public class DataConnection extends StateMachine {
                     mApnSetting != null
                         ? mApnSetting.canHandleType(ApnSetting.TYPE_DEFAULT) : false);
 
-            updateNetworkInfo();
-
             // If we were retrying there maybe more than one, otherwise they'll only be one.
             notifyAllWithEvent(null, DctConstants.EVENT_DATA_SETUP_COMPLETE,
                     Phone.REASON_CONNECTED);
@@ -2054,7 +2037,6 @@ public class DataConnection extends StateMachine {
 
             mNetworkInfo.setDetailedState(NetworkInfo.DetailedState.CONNECTED,
                     mNetworkInfo.getReason(), null);
-            mNetworkInfo.setExtraInfo(mApnSetting.getApnName());
             updateTcpBufferSizes(mRilRat);
 
             final NetworkAgentConfig.Builder configBuilder = new NetworkAgentConfig.Builder();
@@ -2275,7 +2257,6 @@ public class DataConnection extends StateMachine {
                 case EVENT_DATA_CONNECTION_ROAM_OFF:
                 case EVENT_DATA_CONNECTION_METEREDNESS_CHANGED:
                 case EVENT_DATA_CONNECTION_OVERRIDE_CHANGED: {
-                    updateNetworkInfo();
                     if (mNetworkAgent != null) {
                         mNetworkAgent.sendNetworkCapabilities(getNetworkCapabilities(),
                                 DataConnection.this);
@@ -2303,7 +2284,6 @@ public class DataConnection extends StateMachine {
                 }
                 case EVENT_DATA_CONNECTION_VOICE_CALL_STARTED:
                 case EVENT_DATA_CONNECTION_VOICE_CALL_ENDED: {
-                    updateNetworkInfo();
                     updateNetworkInfoSuspendState();
                     if (mNetworkAgent != null) {
                         mNetworkAgent.sendNetworkCapabilities(getNetworkCapabilities(),
