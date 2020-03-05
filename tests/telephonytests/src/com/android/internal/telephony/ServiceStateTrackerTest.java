37e8ef7827c656a30c360e7f9874936a6669bb69
Honggang Luo <honggang.luo@sony.com>
2020-03-05 14:43:15 +0800

    Fix "No service" is not shown without SIM
    "Emergency calls only" is shown instead of "No service" in statusbar
    even if within the area specified by
    "config_display_no_service_when_sim_unready" in the following cases.
    
    1. Power on the device without SIM.
       When updateSpnDisplay() is called, the country code may not be
       updated yet. So the plmn keeps as "Emergency calls only".
    
    2. Remove SIM card from the device which supports hot swap SIM.
       After removing the SIM, the UiccApplication becomes null.
       If UiccApplication is null, mIsSimReady is not updated to false.
       "No service" is not shown when mIsSimReady is true,
       so the problem occur.
    
    How to fix the problem:
    1. When last known country code is changed, update plmn by
       updateSpnDisplay().
    2. If UiccApplication is null, update mIsSimReady to false.
    
    Test: manual - "No service" is shown in lock screen and statusbar
    without SIM.
    Test: auto - Passed ServiceStateTrackerTest.
    Bug: 151881495
    
    Merged-in: I8115fdd0b4760069a309a55ddd8b579785d7ce46
    Change-Id: I8115fdd0b4760069a309a55ddd8b579785d7ce46
    (cherry picked from commit 250fb97f09dc82b4f9015d12522b35c8fed7abe5)

--- tests/telephonytests/src/com/android/internal/telephony/ServiceStateTrackerTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/ServiceStateTrackerTest.java
@@ -2564,7 +2564,7 @@ public class ServiceStateTrackerTest extends TelephonyTest {
     @Test
     public void testShouldForceDisplayNoService_forceBasedOnLocale() {
         // set up unaffected locale (US) and clear the resource
-        doReturn("us").when(mLocaleTracker).getCurrentCountry();
+        doReturn("us").when(mLocaleTracker).getLastKnownCountryIso();
         mContextFixture.putStringArrayResource(
                 com.android.internal.R.array.config_display_no_service_when_sim_unready,
                 new String[0]);
@@ -2574,11 +2574,11 @@ public class ServiceStateTrackerTest extends TelephonyTest {
         mContextFixture.putStringArrayResource(
                 com.android.internal.R.array.config_display_no_service_when_sim_unready,
                 new String[]{"de"});
-        doReturn("us").when(mLocaleTracker).getCurrentCountry();
+        doReturn("us").when(mLocaleTracker).getLastKnownCountryIso();
         assertFalse(sst.shouldForceDisplayNoService());
 
         // mock the locale to Germany
-        doReturn("de").when(mLocaleTracker).getCurrentCountry();
+        doReturn("de").when(mLocaleTracker).getLastKnownCountryIso();
         assertTrue(sst.shouldForceDisplayNoService());
     }
 
