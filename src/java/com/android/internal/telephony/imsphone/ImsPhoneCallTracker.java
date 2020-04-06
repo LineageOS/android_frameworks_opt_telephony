935d79d1b4a96bea638dfee8b2ec59e6d8690732
Tyler Gunn <tgunn@google.com>
2020-04-06 15:05:55 -0700

    Add support for ignoring CEP on conference peer.
    Normally, CEP data received on the peer is accepted without question.
    Some carriers, however, pass this data even though it's not desired to show
    the conference state on the peer.
    
    This change adds support for a new carrier config option to indicate if
    the peer data should be used.
    
    Also this CL enhances support for CEP testing on the device.
    TelephonyTester supports manual injection of CEP data into an IMS call.
    In the past this was done by injecting through the ImsCall.  This, however,
    is problematic because there is a telephony command which can be used to
    disable CEP for test purposes:
    adb shell cmd phone ims conference-event-package disable
    
    Since CEP is disabled in ImsPhoneCallTracker, injecting the test CEP
    data through the test IMS call won't work.  This change makes it possible
    to inject the CEP data either via the ImsCall (existing) or through
    the ImsPhoneCallTracker to ensure it happens even if the override has
    disabled it.
    
    This ensures that in manual testing you can disable CEP and still inject
    test CEP data onto the device.
    
    Test: Manual test with CEP injection intents.
    Test: Added new ImsPhoneCallTracker tests for the new carrier config.
    Bug: 150860282
    Fixes: 154245604
    Merged-In: I3957531afc4bc8adc2c27254db78fec753864bfc
    Change-Id: I5135a3378299974df0eafbfaac9d826256af5f7d

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -60,6 +60,7 @@ import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.telephony.emergency.EmergencyNumber;
 import android.telephony.ims.ImsCallProfile;
+import android.telephony.ims.ImsConferenceState;
 import android.telephony.ims.ImsMmTelManager;
 import android.telephony.ims.ImsReasonInfo;
 import android.telephony.ims.ImsStreamMediaProfile;
@@ -483,6 +484,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
     private boolean mIsViLteDataMetered = false;
     private boolean mAlwaysPlayRemoteHoldTone = false;
     private boolean mAutoRetryFailedWifiEmergencyCall = false;
+    private boolean mSupportCepOnPeer = true;
     // Tracks the state of our background/foreground calls while a call hold/swap operation is
     // in progress. Values listed above.
     private HoldSwapState mHoldSwitchingState = HoldSwapState.INACTIVE;
@@ -1368,6 +1370,8 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 CarrierConfigManager.KEY_ALWAYS_PLAY_REMOTE_HOLD_TONE_BOOL);
         mAutoRetryFailedWifiEmergencyCall = carrierConfig.getBoolean(
                 CarrierConfigManager.KEY_AUTO_RETRY_FAILED_WIFI_EMERGENCY_CALL);
+        mSupportCepOnPeer = carrierConfig.getBoolean(
+                CarrierConfigManager.KEY_SUPPORT_IMS_CONFERENCE_EVENT_PACKAGE_ON_PEER_BOOL);
 
         String[] mappings = carrierConfig
                 .getStringArray(CarrierConfigManager.KEY_IMS_REASONINFO_MAPPING_STRING_ARRAY);
@@ -3295,6 +3299,11 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
                 return;
             }
 
+            if (!mSupportCepOnPeer && !call.isConferenceHost()) {
+                logi("onConferenceParticipantsStateChanged - ignore CEP on peer");
+                return;
+            }
+
             ImsPhoneConnection conn = findConnection(call);
             if (conn != null) {
                 updateConferenceParticipantsTiming(participants);
@@ -4112,6 +4121,7 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
         pw.println(" mCallQualityMetrics=" + mCallQualityMetrics);
         pw.println(" mCallQualityMetricsHistory=" + mCallQualityMetricsHistory);
         pw.println(" mIsConferenceEventPackageHandlingEnabled=" + mIsConferenceEventPackageEnabled);
+        pw.println(" mSupportCepOnPeer=" + mSupportCepOnPeer);
         pw.println(" Event Log:");
         pw.increaseIndent();
         mOperationLocalLog.dump(pw);
@@ -4794,6 +4804,22 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
     @Override
     public ImsPhone getPhone() {
         return mPhone;
+    }
+
+    @VisibleForTesting
+    public void setSupportCepOnPeer(boolean isSupported) {
+        mSupportCepOnPeer = isSupported;
+    }
+
+    /**
+     * Injects a test conference state into an ongoing IMS call.
+     * @param state The injected state.
+     */
+    public void injectTestConferenceState(@NonNull ImsConferenceState state) {
+        List<ConferenceParticipant> participants = ImsCall.parseConferenceState(state);
+        for (ImsPhoneConnection connection : getConnections()) {
+            connection.updateConferenceParticipants(participants);
+        }
     }
 
     /**
