1b81f5ee78a7bf4ac237fc5cf106e72f1d722b88
Daniel Bright <dbright@google.com>
2020-06-22 17:07:01 -0700

    Complete the revert on the wake lock on call tracker
    On fallback from Gsm to LTE, the wakelock needs to be released on the underlying connection.
    This should have gone in the previous CL.
    
    Bug: 159462297
    Test: atest FrameworksTelephonyTests
    Test: Made phone call using Gsm/Cmda
    Test: Monitored wakelocks with gsm while on call
    Merged-In: I6fffd302fadba170b1461647c7094a732d17eed1
    Change-Id: I6fffd302fadba170b1461647c7094a732d17eed1

--- src/java/com/android/internal/telephony/GsmCdmaCallTracker.java
+++ src/java/com/android/internal/telephony/GsmCdmaCallTracker.java
@@ -888,6 +888,8 @@ public class GsmCdmaCallTracker extends CallTracker {
                                 hoConnection.mPreHandoverState != GsmCdmaCall.State.HOLDING &&
                                 dc.state == DriverCall.State.ACTIVE) {
                             mConnections[i].onConnectedInOrOut();
+                        } else {
+                            mConnections[i].onConnectedConnectionMigrated();
                         }
 
                         mHandoverConnections.remove(hoConnection);
