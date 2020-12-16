96e2ae541f379f76fabe63bdf08a1cf872506433
Tyler Gunn <tgunn@google.com>
2020-12-16 12:52:52 -0800

    Ensure ImsCallProfile updates with only codec change updates to telecom.
    Fixes: 171606385
    Test: Added unit test to ensure audio codec update is registered.
    Change-Id: I45993cec58aa8f5318cc4cd0745dbcf99622f405

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneConnection.java
@@ -1068,6 +1068,7 @@ public class ImsPhoneConnection extends Connection implements
                 mAudioCodec = localCallProfile.mMediaProfile.mAudioQuality;
                 mMetrics.writeAudioCodecIms(mOwner.mPhone.getPhoneId(), imsCall.getCallSession());
                 mOwner.getPhone().getVoiceCallSessionStats().onAudioCodecChanged(this, mAudioCodec);
+                changed = true;
             }
 
             if (localCallProfile != null
