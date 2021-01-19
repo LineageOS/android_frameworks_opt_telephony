532e71bcbfa4872e4160b65aa13e8a4d9892d712
Taesu Lee <taesu82.lee@samsung.com>
2021-01-19 15:46:42 +0900

    Added fail-retry scheme for SMS memory status reporting
    Retry a notification for availability of memory(RP-SMMA) if failed since
    SMSC won't deliver any messages until RP-SMMA is received.
    
    Bug: 178338878
    Test: atest SmsStorageMonitorTest
    
    Signed-off-by: Taesu Lee <taesu82.lee@samsung.com>
    Change-Id: Iedc033170d8011c678492a5ef0611fbb10adeacc

--- tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
+++ tests/telephonytests/src/com/android/internal/telephony/SimulatedCommands.java
@@ -178,6 +178,7 @@ public class SimulatedCommands extends BaseCommands
     private boolean mDcSuccess = true;
     private SetupDataCallResult mSetupDataCallResult;
     private boolean mIsRadioPowerFailResponse = false;
+    private boolean mIsReportSmsMemoryStatusFailResponse = false;
 
     public boolean mSetRadioPowerForEmergencyCall;
     public boolean mSetRadioPowerAsSelectedPhoneForEmergencyCall;
@@ -1287,8 +1288,17 @@ public class SimulatedCommands extends BaseCommands
 
     @Override
     public void reportSmsMemoryStatus(boolean available, Message result) {
-        resultSuccess(result, null);
+        if (!mIsReportSmsMemoryStatusFailResponse) {
+            resultSuccess(result, null);
+        } else {
+            CommandException ex = new CommandException(CommandException.Error.GENERIC_FAILURE);
+            resultFail(result, null, ex);
+        }
         SimulatedCommandsVerifier.getInstance().reportSmsMemoryStatus(available, result);
+    }
+
+    public void setReportSmsMemoryStatusFailResponse(boolean fail) {
+        mIsReportSmsMemoryStatusFailResponse = fail;
     }
 
     @Override
--- tests/telephonytests/src/com/android/internal/telephony/SmsStorageMonitorTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/SmsStorageMonitorTest.java
@@ -16,12 +16,15 @@
 
 package com.android.internal.telephony;
 
+import static com.android.internal.telephony.TelephonyTestUtils.waitForMs;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import android.content.Intent;
 import android.os.Message;
 import android.provider.Telephony;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
@@ -36,12 +39,17 @@ import org.mockito.ArgumentCaptor;
 @TestableLooper.RunWithLooper
 public class SmsStorageMonitorTest extends TelephonyTest {
 
+    private static final int MAX_RETRIES = 1;
+    private static final int RETRY_DELAY = 200; // 200 millis
+
     private SmsStorageMonitor mSmsStorageMonitor;
 
     @Before
     public void setUp() throws Exception {
         super.setUp(getClass().getSimpleName());
         mSmsStorageMonitor = new SmsStorageMonitor(mPhone);
+        mSmsStorageMonitor.setMaxRetries(MAX_RETRIES);
+        mSmsStorageMonitor.setRetryDelayInMillis(RETRY_DELAY);
         processAllMessages();
     }
 
@@ -98,5 +106,55 @@ public class SmsStorageMonitorTest extends TelephonyTest {
         processAllMessages();
 
         verify(mSimulatedCommandsVerifier).reportSmsMemoryStatus(eq(true), any(Message.class));
+    }
+
+    @Test @MediumTest
+    public void testRetrySmsMemoryStatus() {
+        mSimulatedCommands.setReportSmsMemoryStatusFailResponse(true);
+
+        // Send DEVICE_STORAGE_FULL
+        mContextFixture.getTestDouble().sendBroadcast(
+                new Intent(Intent.ACTION_DEVICE_STORAGE_FULL));
+        processAllMessages();
+
+        // Wait until retrying is done.
+        for (int i = 0; i < MAX_RETRIES; i++) {
+            waitForMs(RETRY_DELAY);
+            processAllMessages();
+        }
+
+        verify(mSimulatedCommandsVerifier, times(1 + MAX_RETRIES))
+                .reportSmsMemoryStatus(eq(false), any(Message.class));
+        assertFalse(mSmsStorageMonitor.isStorageAvailable());
+
+        mSimulatedCommands.setReportSmsMemoryStatusFailResponse(false);
+
+        // Notify radio on
+        mSimulatedCommands.notifyRadioOn();
+        processAllMessages();
+
+        verify(mSimulatedCommandsVerifier, times(2 + MAX_RETRIES))
+                .reportSmsMemoryStatus(eq(false), any(Message.class));
+    }
+
+    @Test @SmallTest
+    public void testReportSmsMemoryStatusDuringRetry() {
+        mSimulatedCommands.setReportSmsMemoryStatusFailResponse(true);
+
+        // Send DEVICE_STORAGE_FULL
+        mContextFixture.getTestDouble().sendBroadcast(
+                new Intent(Intent.ACTION_DEVICE_STORAGE_FULL));
+        processAllMessages();
+
+        mSimulatedCommands.setReportSmsMemoryStatusFailResponse(false);
+
+        // Send DEVICE_STORAGE_NOT_FULL
+        mContextFixture.getTestDouble().sendBroadcast(
+                new Intent(Intent.ACTION_DEVICE_STORAGE_NOT_FULL));
+        processAllMessages();
+
+        verify(mSimulatedCommandsVerifier).reportSmsMemoryStatus(eq(false), any(Message.class));
+        verify(mSimulatedCommandsVerifier).reportSmsMemoryStatus(eq(true), any(Message.class));
+        assertTrue(mSmsStorageMonitor.isStorageAvailable());
     }
 }
