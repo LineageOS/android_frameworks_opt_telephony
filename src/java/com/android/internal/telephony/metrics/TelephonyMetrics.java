e45316a336c781c0b648af907a9b75150c5cca0d
Taesu Lee <taesu82.lee@samsung.com>
2021-03-04 16:03:27 +0900

    Fix typo
    Test: build & atest TelephonyMetricsTest
    
    Signed-off-by: Taesu Lee <taesu82.lee@samsung.com>
    Change-Id: Ifeda413a4939c8e17818ec3471f41c4d1656660d

--- src/java/com/android/internal/telephony/metrics/TelephonyMetrics.java
+++ src/java/com/android/internal/telephony/metrics/TelephonyMetrics.java
@@ -1774,7 +1774,7 @@ public class TelephonyMetrics {
             dataCall.type = response.getProtocolType() + 1;
 
             if (!TextUtils.isEmpty(response.getInterfaceName())) {
-                dataCall.iframe = response.getInterfaceName();
+                dataCall.ifname = response.getInterfaceName();
             }
         }
         setupDataCallResponse.call = dataCall;
