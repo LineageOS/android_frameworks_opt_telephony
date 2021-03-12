0f5031a2742831910880157fffe17c6fa46692e7
Michele Berionne <mberionne@google.com>
2021-03-12 21:56:40 +0000

    Fix handling of TelephonySettings in metrics
    Synchronize the 2 methods that access mLastSettings and avoid that new
    event completely overwrites the previous status.
    
    Bug: 182588672
    Test: added unit testing
    Change-Id: I6d7e9396f28846000f1b63441c6a5d019c953c37

--- src/java/com/android/internal/telephony/metrics/TelephonyMetrics.java
+++ src/java/com/android/internal/telephony/metrics/TelephonyMetrics.java
@@ -56,6 +56,7 @@ import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyHistogram;
 import android.telephony.TelephonyManager;
+import android.telephony.TelephonyManager.PrefNetworkMode;
 import android.telephony.data.DataCallResponse;
 import android.telephony.data.DataService;
 import android.telephony.emergency.EmergencyNumber;
@@ -1215,6 +1216,20 @@ public class TelephonyMetrics {
                 .setSignalStrength(signalStrength).build());
     }
 
+    private TelephonySettings cloneCurrentTelephonySettings(int phoneId) {
+        TelephonySettings newSettings = new TelephonySettings();
+        TelephonySettings lastSettings = mLastSettings.get(phoneId);
+        if (lastSettings != null) {
+            // No clone method available, so each relevant field is copied individually.
+            newSettings.preferredNetworkMode = lastSettings.preferredNetworkMode;
+            newSettings.isEnhanced4GLteModeEnabled = lastSettings.isEnhanced4GLteModeEnabled;
+            newSettings.isVtOverLteEnabled = lastSettings.isVtOverLteEnabled;
+            newSettings.isWifiCallingEnabled = lastSettings.isWifiCallingEnabled;
+            newSettings.isVtOverWifiEnabled = lastSettings.isVtOverWifiEnabled;
+        }
+        return newSettings;
+    }
+
     /**
      * Write IMS feature settings changed event
      *
@@ -1223,8 +1238,9 @@ public class TelephonyMetrics {
      * @param network The IMS network type
      * @param value The settings. 0 indicates disabled, otherwise enabled.
      */
-    public void writeImsSetFeatureValue(int phoneId, int feature, int network, int value) {
-        TelephonySettings s = new TelephonySettings();
+    public synchronized void writeImsSetFeatureValue(int phoneId, int feature, int network,
+            int value) {
+        TelephonySettings s = cloneCurrentTelephonySettings(phoneId);
         if (network == ImsRegistrationImplBase.REGISTRATION_TECH_LTE) {
             switch (feature) {
                 case MmTelFeature.MmTelCapabilities.CAPABILITY_TYPE_VOICE:
@@ -1244,7 +1260,6 @@ public class TelephonyMetrics {
                     break;
             }
         }
-
 
         // If the settings don't change, we don't log the event.
         if (mLastSettings.get(phoneId) != null &&
@@ -1272,8 +1287,9 @@ public class TelephonyMetrics {
      * @param phoneId Phone id
      * @param networkType The preferred network
      */
-    public void writeSetPreferredNetworkType(int phoneId, int networkType) {
-        TelephonySettings s = new TelephonySettings();
+    public synchronized void writeSetPreferredNetworkType(int phoneId,
+            @PrefNetworkMode int networkType) {
+        TelephonySettings s = cloneCurrentTelephonySettings(phoneId);
         s.preferredNetworkMode = networkType + 1;
 
         // If the settings don't change, we don't log the event.
