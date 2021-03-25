6892f28277f2fc82776cee6d54a392a3e805944e
Sarah Chin <sarahchin@google.com>
2021-03-24 20:33:11 -0700

    Update enterprise compatibility
    Default is not compatible with enterprise, and enterprise isn't
    compatible with any other APN
    
    Test: atest DcTrackerTest
    Bug: 183651104
    Change-Id: I2eab917cf1bb28c8c55af7b53776c17bbb3de4d6

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -2057,8 +2057,10 @@ public class DcTracker extends Handler {
         // a dun-profiled connection so we can't share an existing one
         // On GSM/LTE we can share existing apn connections provided they support
         // this type.
-        if (!apnContext.getApnType().equals(ApnSetting.TYPE_DUN_STRING)
-                || ServiceState.isGsm(getDataRat())) {
+        // If asking for ENTERPRISE, there are no compatible data connections, so skip this check
+        if ((apnContext.getApnTypeBitmask() != ApnSetting.TYPE_DUN
+                || ServiceState.isGsm(getDataRat()))
+                && apnContext.getApnTypeBitmask() != ApnSetting.TYPE_ENTERPRISE) {
             dataConnection = checkForCompatibleDataConnection(apnContext, apnSetting);
             if (dataConnection != null) {
                 // Get the apn setting used by the data connection
@@ -2482,7 +2484,7 @@ public class DcTracker extends Handler {
                             }
                         }
                     }
-                } else if (apnSetting != null && apnSetting.canHandleType(apnType)) {
+                } else if (isApnSettingCompatible(curDc, apnType)) {
                     if (curDc.isActive()) {
                         if (DBG) {
                             log("checkForCompatibleDataConnection:"
@@ -2500,6 +2502,20 @@ public class DcTracker extends Handler {
             log("checkForCompatibleDataConnection: potential dc=" + potentialDc);
         }
         return potentialDc;
+    }
+
+    private boolean isApnSettingCompatible(DataConnection dc, int apnType) {
+        ApnSetting apnSetting = dc.getApnSetting();
+        if (apnSetting == null) return false;
+
+        // Nothing can be compatible with type ENTERPRISE
+        for (ApnContext apnContext : dc.getApnContexts()) {
+            if (apnContext.getApnTypeBitmask() == ApnSetting.TYPE_ENTERPRISE) {
+                return false;
+            }
+        }
+
+        return apnSetting.canHandleType(apnType);
     }
 
     private void addRequestNetworkCompleteMsg(Message onCompleteMsg,
