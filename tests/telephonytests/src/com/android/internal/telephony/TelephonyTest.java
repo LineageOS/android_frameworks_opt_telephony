60cc084d6b153d7451e9e61b5c05996def0119b9
Sarah Chin <sarahchin@google.com>
2021-04-06 10:26:34 -0700

    Fix ENTERPRISE failures for duplicate CID and no default data
    When ENTERPRISE fails because of duplicate CID, ignore the request
    (transition to inactive state) and don't retry. When it fails due to
    no default data, fail and tear down the newly created connection.
    
    Test: atest DcTrackerTest, DataConnectionTest
    Bug: 183979790
    Bug: 183739359
    Change-Id: Id5b3a13fc5afb9908359321d2196802ce890e5a2
    Merged-In: Id5b3a13fc5afb9908359321d2196802ce890e5a2

--- tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
@@ -64,6 +64,8 @@ import android.provider.DeviceConfig;
 import android.provider.Settings;
 import android.telephony.AccessNetworkConstants;
 import android.telephony.CarrierConfigManager;
+import android.telephony.CellIdentity;
+import android.telephony.CellLocation;
 import android.telephony.NetworkRegistrationInfo;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
@@ -323,6 +325,10 @@ public abstract class TelephonyTest {
     protected PinStorage mPinStorage;
     @Mock
     protected LocationManager mLocationManager;
+    @Mock
+    protected CellIdentity mCellIdentity;
+    @Mock
+    protected CellLocation mCellLocation;
 
     protected ActivityManager mActivityManager;
     protected ImsCallProfile mImsCallProfile;
@@ -564,6 +570,8 @@ public abstract class TelephonyTest {
         doReturn(mSmsStats).when(mPhone).getSmsStats();
         doReturn(mImsStats).when(mImsPhone).getImsStats();
         mIccSmsInterfaceManager.mDispatchersController = mSmsDispatchersController;
+        doReturn(mCellIdentity).when(mPhone).getCurrentCellIdentity();
+        doReturn(mCellLocation).when(mCellIdentity).asCellLocation();
 
         //mUiccController
         doReturn(mUiccCardApplication3gpp).when(mUiccController).getUiccCardApplication(anyInt(),
