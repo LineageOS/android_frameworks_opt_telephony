2f7f416a981400470ada1ddfeabfe1f3774c5b5c
Jack Nudelman <jacknudelman@google.com>
2021-05-27 13:55:06 -0700

    Check for specific radio off reason for phone.
    Bug: 188928361
    Test: make, local testing
    Change-Id: Ifc83411967980f0887780b40640853d4514b924b
    Merged-In: Ifc83411967980f0887780b40640853d4514b924b

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -1087,8 +1087,7 @@ public class ServiceStateTracker extends Handler {
     /**
      * @return the current reasons for which the radio is off.
      */
-    @VisibleForTesting
-    public Set<Integer> getRadioPowerOffReasonsForTest() {
+    public Set<Integer> getRadioPowerOffReasons() {
         return sRadioPowerOffReasons;
     }
 
