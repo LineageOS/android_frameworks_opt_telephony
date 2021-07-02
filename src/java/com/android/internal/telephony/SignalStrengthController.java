67608ef9003ee7040c13f71c55e6a6086dfaa449
Rambo Wang <rambowang@google.com>
2021-07-02 09:26:25 -0700

    Move signal strength logic from DeviceStateMonitor to SignalStrengthController
    This is a pure clean-up CL after SSC is introduced(aosp/1545049).
    No addtional functions are added. The following CLs will locate
    the functions updates, including further optimization of cellular
    signal strength handling.
    
    Bug: 179077640
    Test: atest DeviceStateMonitorTest ServiceStateTrackerTest
    SignalStrengthControllerTest
    
    Change-Id: I5512898b2e0e5995ec1fccae1875760f84ae4ea5

--- src/java/com/android/internal/telephony/SignalStrengthController.java
+++ src/java/com/android/internal/telephony/SignalStrengthController.java
@@ -52,9 +52,12 @@ public class SignalStrengthController extends Handler {
     protected static final boolean DBG = false; /* STOPSHIP if true */
     protected static final String TAG = "SSCtr";
 
-    private static final int EVENT_SET_SIGNAL_STRENGTH_UPDATE_REQUEST = 1;
+    private static final int EVENT_SET_SIGNAL_STRENGTH_UPDATE_REQUEST   = 1;
     private static final int EVENT_CLEAR_SIGNAL_STRENGTH_UPDATE_REQUEST = 2;
-    private static final int EVENT_ON_DEVICE_IDLE_STATE_CHANGED        = 3;
+    private static final int EVENT_ON_DEVICE_IDLE_STATE_CHANGED         = 3;
+    private static final int EVENT_RIL_CONNECTED                        = 4;
+    private static final int EVENT_RADIO_AVAILABLE                      = 5;
+
 
     private final Phone mPhone;
     private final CommandsInterface mCi;
@@ -64,6 +67,9 @@ public class SignalStrengthController extends Handler {
     public SignalStrengthController(Phone phone) {
         mPhone = phone;
         mCi = mPhone.mCi;
+
+        mCi.registerForRilConnected(this, EVENT_RIL_CONNECTED, null);
+        mCi.registerForAvailable(this, EVENT_RADIO_AVAILABLE, null);
     }
 
     @Override
@@ -71,6 +77,10 @@ public class SignalStrengthController extends Handler {
         if (DBG) log("received event " + msg.what);
 
         switch (msg.what) {
+            case EVENT_RIL_CONNECTED: // fall through
+            case EVENT_RADIO_AVAILABLE:
+                onReset();
+                break;
             case EVENT_SET_SIGNAL_STRENGTH_UPDATE_REQUEST: {
                 Pair<SignalRequestRecord, Message> pair =
                         (Pair<SignalRequestRecord, Message>) msg.obj;
@@ -324,6 +334,158 @@ public class SignalStrengthController extends Handler {
         return false;
     }
 
+    private void setDefaultSignalStrengthReportingCriteria() {
+        mPhone.setSignalStrengthReportingCriteria(SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSSI,
+                AccessNetworkThresholds.GERAN, AccessNetworkConstants.AccessNetworkType.GERAN,
+                true);
+        mPhone.setSignalStrengthReportingCriteria(SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSCP,
+                AccessNetworkThresholds.UTRAN, AccessNetworkConstants.AccessNetworkType.UTRAN,
+                true);
+        mPhone.setSignalStrengthReportingCriteria(SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSRP,
+                AccessNetworkThresholds.EUTRAN_RSRP,
+                AccessNetworkConstants.AccessNetworkType.EUTRAN, true);
+        mPhone.setSignalStrengthReportingCriteria(SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSSI,
+                AccessNetworkThresholds.CDMA2000, AccessNetworkConstants.AccessNetworkType.CDMA2000,
+                true);
+        if (mPhone.getHalVersion().greaterOrEqual(RIL.RADIO_HAL_VERSION_1_5)) {
+            mPhone.setSignalStrengthReportingCriteria(
+                    SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSRQ,
+                    AccessNetworkThresholds.EUTRAN_RSRQ,
+                    AccessNetworkConstants.AccessNetworkType.EUTRAN, false);
+            mPhone.setSignalStrengthReportingCriteria(
+                    SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_RSSNR,
+                    AccessNetworkThresholds.EUTRAN_RSSNR,
+                    AccessNetworkConstants.AccessNetworkType.EUTRAN, true);
+
+            // Defaultly we only need SSRSRP for NGRAN signal criteria reporting
+            mPhone.setSignalStrengthReportingCriteria(
+                    SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_SSRSRP,
+                    AccessNetworkThresholds.NGRAN_RSRSRP,
+                    AccessNetworkConstants.AccessNetworkType.NGRAN, true);
+            mPhone.setSignalStrengthReportingCriteria(
+                    SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_SSRSRQ,
+                    AccessNetworkThresholds.NGRAN_RSRSRQ,
+                    AccessNetworkConstants.AccessNetworkType.NGRAN, false);
+            mPhone.setSignalStrengthReportingCriteria(
+                    SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_SSSINR,
+                    AccessNetworkThresholds.NGRAN_SSSINR,
+                    AccessNetworkConstants.AccessNetworkType.NGRAN, false);
+        }
+    }
+
+    /**
+     * dBm thresholds that correspond to changes in signal strength indications.
+     */
+    private static final class AccessNetworkThresholds {
+
+        /**
+         * List of dBm thresholds for GERAN {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * Calculated from GSM asu level thresholds - TS 27.007 Sec 8.5
+         */
+        public static final int[] GERAN = new int[]{
+                -109,
+                -103,
+                -97,
+                -89,
+        };
+
+        /**
+         * List of default dBm thresholds for UTRAN
+         * {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * These thresholds are taken from the WCDMA RSCP defaults in {@link CarrierConfigManager}.
+         * See TS 27.007 Sec 8.69.
+         */
+        public static final int[] UTRAN = new int[]{
+                -114, /* SIGNAL_STRENGTH_POOR */
+                -104, /* SIGNAL_STRENGTH_MODERATE */
+                -94,  /* SIGNAL_STRENGTH_GOOD */
+                -84   /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of default dBm RSRP thresholds for EUTRAN
+         * {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * These thresholds are taken from the LTE RSRP defaults in {@link CarrierConfigManager}.
+         */
+        public static final int[] EUTRAN_RSRP = new int[]{
+                -128, /* SIGNAL_STRENGTH_POOR */
+                -118, /* SIGNAL_STRENGTH_MODERATE */
+                -108, /* SIGNAL_STRENGTH_GOOD */
+                -98,  /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of default dB RSRQ thresholds for EUTRAN
+         * {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * These thresholds are taken from the LTE RSRQ defaults in {@link CarrierConfigManager}.
+         */
+        public static final int[] EUTRAN_RSRQ = new int[]{
+                -20,  /* SIGNAL_STRENGTH_POOR */
+                -17,  /* SIGNAL_STRENGTH_MODERATE */
+                -14,  /* SIGNAL_STRENGTH_GOOD */
+                -11   /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of default dB RSSNR thresholds for EUTRAN
+         * {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * These thresholds are taken from the LTE RSSNR defaults in {@link CarrierConfigManager}.
+         */
+        public static final int[] EUTRAN_RSSNR = new int[]{
+                -3,  /* SIGNAL_STRENGTH_POOR */
+                1,   /* SIGNAL_STRENGTH_MODERATE */
+                5,   /* SIGNAL_STRENGTH_GOOD */
+                13   /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of dBm thresholds for CDMA2000 {@link AccessNetworkConstants.AccessNetworkType}.
+         *
+         * These correspond to EVDO level thresholds.
+         */
+        public static final int[] CDMA2000 = new int[]{
+                -105,
+                -90,
+                -75,
+                -65
+        };
+
+        /**
+         * List of dB thresholds for NGRAN {@link AccessNetworkConstants.AccessNetworkType} RSRSRP
+         */
+        public static final int[] NGRAN_RSRSRP = new int[]{
+                -110, /* SIGNAL_STRENGTH_POOR */
+                -90, /* SIGNAL_STRENGTH_MODERATE */
+                -80, /* SIGNAL_STRENGTH_GOOD */
+                -65,  /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of dB thresholds for NGRAN {@link AccessNetworkConstants.AccessNetworkType} RSRSRP
+         */
+        public static final int[] NGRAN_RSRSRQ = new int[]{
+                -31, /* SIGNAL_STRENGTH_POOR */
+                -19, /* SIGNAL_STRENGTH_MODERATE */
+                -7, /* SIGNAL_STRENGTH_GOOD */
+                6  /* SIGNAL_STRENGTH_GREAT */
+        };
+
+        /**
+         * List of dB thresholds for NGRAN {@link AccessNetworkConstants.AccessNetworkType} SSSINR
+         */
+        public static final int[] NGRAN_SSSINR = new int[]{
+                -5, /* SIGNAL_STRENGTH_POOR */
+                5, /* SIGNAL_STRENGTH_MODERATE */
+                15, /* SIGNAL_STRENGTH_GOOD */
+                30  /* SIGNAL_STRENGTH_GREAT */
+        };
+    }
+
     private static boolean isRanAndSignalMeasurementTypeMatch(
             @AccessNetworkConstants.RadioAccessNetworkType int ran,
             @SignalThresholdInfo.SignalMeasurementType int measurement,
@@ -392,6 +554,14 @@ public class SignalStrengthController extends Handler {
         // TODO(b/177924721): TM#setAlwaysReportSignalStrength will be removed and we will not
         // worry about unset flag which was set by other client.
         mPhone.setAlwaysReportSignalStrength(alwaysReport);
+    }
+
+    /**
+     * Called when RIL is connected during boot up or after modem restart. Set the default criteria
+     * so that modem can start with default state before updated criteria is ready.
+     */
+    private void onReset() {
+        setDefaultSignalStrengthReportingCriteria();
     }
 
     private void log(String msg) {
