8a294d8ea6e2aa7ddcc036a0a5c25c2b7a799227
Jack Yu <jackyu@google.com>
2021-08-20 01:17:48 -0700

    Optimized transport manager
    Optimized transport manager so it always checks
    the latest preference update from QNS. If there are
    back-and-forth preference switch while the handover is ongoing,
    when handover is completed, those updates will be ignored except
    the last one.
    
    Fix: 192912296
    Test: Manual
    Change-Id: I37249021757c729401265b9e53c6bd33acb47155

--- tests/telephonytests/src/com/android/internal/telephony/ServiceStateTrackerTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/ServiceStateTrackerTest.java
@@ -2379,7 +2379,7 @@ public class ServiceStateTrackerTest extends TelephonyTest {
     @SmallTest
     @Test
     public void testRilDataTechnologyChangeTransportPreference() {
-        when(mTransportManager.isAnyApnPreferredOnIwlan()).thenReturn(false);
+        when(mTransportManager.isAnyApnOnIwlan()).thenReturn(false);
 
         // Start state: Cell data only LTE + IWLAN
         CellIdentityLte cellIdentity =
@@ -2398,7 +2398,7 @@ public class ServiceStateTrackerTest extends TelephonyTest {
                 mTestHandler, EVENT_DATA_RAT_CHANGED, null);
         // transport preference change for a PDN for IWLAN occurred, no registration change, but
         // trigger unrelated poll to pick up transport preference.
-        when(mTransportManager.isAnyApnPreferredOnIwlan()).thenReturn(true);
+        when(mTransportManager.isAnyApnOnIwlan()).thenReturn(true);
         changeRegStateWithIwlan(
                 // WWAN
                 NetworkRegistrationInfo.REGISTRATION_STATE_HOME, cellIdentity,
