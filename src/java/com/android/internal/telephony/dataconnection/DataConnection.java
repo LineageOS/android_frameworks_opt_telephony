5a0d4c289c892daf421e1b12b0e9565b2a8603a9
Sarah Chin <sarahchin@google.com>
2021-09-16 12:48:52 -0700

    Update and retry ENTERPRISE if TDs changed
    Test: atest DataConnectionTest, DcTrackerTest
    Bug: 198473491
    Change-Id: I12de99148901111197dd00f7b9d987f667e29e37

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -675,6 +675,8 @@ public class DataConnection extends StateMachine {
      */
     public void updateTrafficDescriptors(DataCallResponse response) {
         mTrafficDescriptors = response.getTrafficDescriptors();
+        mDcController.updateTrafficDescriptorsForCid(response.getId(),
+                response.getTrafficDescriptors());
     }
 
     @VisibleForTesting
@@ -1396,6 +1398,12 @@ public class DataConnection extends StateMachine {
             }
         } else if (cp.mApnContext.getApnTypeBitmask() == ApnSetting.TYPE_ENTERPRISE
                 && mDcController.getActiveDcByCid(response.getId()) != null) {
+            if (!mDcController.getTrafficDescriptorsForCid(response.getId())
+                    .equals(response.getTrafficDescriptors())) {
+                if (DBG) log("Updating traffic descriptors: " + response.getTrafficDescriptors());
+                mDcController.getActiveDcByCid(response.getId()).updateTrafficDescriptors(response);
+                mDct.obtainMessage(DctConstants.EVENT_TRAFFIC_DESCRIPTORS_UPDATED).sendToTarget();
+            }
             if (DBG) log("DataConnection already exists for cid: " + response.getId());
             result = SetupResult.ERROR_DUPLICATE_CID;
             result.mFailCause = DataFailCause.DUPLICATE_CID;
@@ -1811,14 +1819,8 @@ public class DataConnection extends StateMachine {
      * @return True if this data connection supports enterprise use.
      */
     private boolean isEnterpriseUse() {
-        boolean enterpriseTrafficDescriptor = mTrafficDescriptors
-                .stream()
-                .anyMatch(td -> td.getOsAppId() != null && Arrays.equals(td.getOsAppId(),
-                        getEnterpriseOsAppId()));
-        boolean enterpriseApnContext = mApnContexts.keySet()
-                .stream()
-                .anyMatch(ac -> ac.getApnTypeBitmask() == ApnSetting.TYPE_ENTERPRISE);
-        return enterpriseTrafficDescriptor || enterpriseApnContext;
+        return  mApnContexts.keySet().stream().anyMatch(
+                ac -> ac.getApnTypeBitmask() == ApnSetting.TYPE_ENTERPRISE);
     }
 
     /**
