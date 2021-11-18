38c6e2006c63c4160b5d4fdf1483991dbf212cfb
terrycrhuang <terrycrhuang@google.com>
2021-11-18 18:47:30 +0800

    Fxi phone crash by ClassCastException
    msg.obj is an AtomicLong and cannot be cast to java.lang.Long.
    Cast msg.obj to AtomicLong first and then get its long value.
    
    Bug: 203241555
    Test: no phone crash as switching mock modem and live modem
    
    Change-Id: I838f67b6777384447485fe08afce0d1d06ce0bd2

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -346,10 +346,11 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
                 case EVENT_RADIO_PROXY_DEAD:
                     int service = msg.arg1;
+                    AtomicLong obj = (AtomicLong) msg.obj;
                     riljLog("handleMessage: EVENT_RADIO_PROXY_DEAD cookie = " + msg.obj
                             + ", service = " + serviceToString(service) + ", service cookie = "
                             + mServiceCookies.get(service));
-                    if ((long) msg.obj == mServiceCookies.get(service).get()) {
+                    if (obj.get() == mServiceCookies.get(service).get()) {
                         resetProxyAndRequestList(service);
                     }
                     break;
