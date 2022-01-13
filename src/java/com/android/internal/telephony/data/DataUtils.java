75f3dfa33a53ccdfc9d5d2046b25031f522e7b3c
Ling Ma <linggm@google.com>
2022-01-13 23:04:36 +0000

    Add ApnType.ENTERPRISE
    Test: build succeed
    Bug: 214240937
    
    Change-Id: I917beddabed0a7d54a62b6c203229d201db76e0f

--- src/java/com/android/internal/telephony/data/DataUtils.java
+++ src/java/com/android/internal/telephony/data/DataUtils.java
@@ -211,6 +211,8 @@ public class DataUtils {
                 return ApnSetting.TYPE_MCX;
             case NetworkCapabilities.NET_CAPABILITY_IA:
                 return ApnSetting.TYPE_IA;
+            case NetworkCapabilities.NET_CAPABILITY_ENTERPRISE:
+                return ApnSetting.TYPE_ENTERPRISE;
             default:
                 return ApnSetting.TYPE_NONE;
         }
@@ -247,7 +249,8 @@ public class DataUtils {
             case ApnSetting.TYPE_IA:
                 return NetworkCapabilities.NET_CAPABILITY_IA;
             // Do not add TYPE_VSIM, TYPE_BIP, TYPE_HIPRI
-            // TODO: Add ENTERPRISE here if needed.
+            case ApnSetting.TYPE_ENTERPRISE:
+                return NetworkCapabilities.NET_CAPABILITY_ENTERPRISE;
             default:
                 return -1;
         }
