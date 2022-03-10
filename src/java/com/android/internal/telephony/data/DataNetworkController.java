f8205634f2ed93bb7d15abfcc333179f8fbf824b
Ling Ma <linggm@google.com>
2022-03-10 18:06:38 +0000

    Revert "Add preferredDataProfile teardown check"
    Revert "Enable new data stack by default except for Pixel 4, 4a"
    
    Revert "Enable new data stack by default"
    
    Revert "Enable new data stack by default"
    
    Revert submission 17112037-presubmit-am-19168ef09d5542b3b19f230396b120ce
    
    Reason for revert: b/223500278
    Reverted Changes:
    Ie6b537696:Enable new data stack by default except for Pixel ...
    Id45e9d66a:Enable new data stack by default
    Ib6deedeee:Enable new data stack by default
    I5f85f2718:Enable new data stack by default
    I42414ac02:Enable new data stack by default except for Pixel ...
    I78db1542a:Enable new data stack by default
    I2573ebc2e:Add preferredDataProfile teardown check
    I6d22205b7:Enable new data stack by default
    I194b0674f:Default unit tests to use new data stack
    
    Change-Id: I96e4ab0a0268c115c0d49fd3d6d9a48e1603c2dd

--- src/java/com/android/internal/telephony/data/DataNetworkController.java
+++ src/java/com/android/internal/telephony/data/DataNetworkController.java
@@ -1475,8 +1475,7 @@ public class DataNetworkController extends Handler {
 
         // If users switch preferred profile in APN editor, we need to tear down network.
         if (dataNetwork.isInternetSupported()
-                && !mDataProfileManager.isDataProfilePreferred(dataProfile)
-                && mDataProfileManager.isAnyPreferredDataProfileExisting()) {
+                && !mDataProfileManager.isDataProfilePreferred(dataProfile)) {
             evaluation.addDataDisallowedReason(DataDisallowedReason.DATA_PROFILE_NOT_PREFERRED);
         }
 
