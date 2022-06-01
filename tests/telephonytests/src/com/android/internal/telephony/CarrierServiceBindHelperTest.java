4a556d7116f0e305699a564a53120b12f585e5d6
Rambo Wang <rambowang@google.com>
2022-06-01 19:31:55 +0000

    Make carrier service persistent binding more robust
    The change introduces an additional checkpoint to make sure
    carrier service binding is connected whenever carrier package
    has changed to non-null.
    
    When carrier service package (e.g. Google Fi) gets upgraded e.g.
    through PlayStore, the persistent binding from telephony to the
    carrier service may get released. A rebinding may not perform until
    a new trigger event occurs later (e.g. SIM state change, user
    unlock or other package installation).
    
    The root cause comes from the race condition when both
    CarrierServiceBindHelper and CarrierPrivilegesTracker handle the
    package change events. When CSBH firstly received the events and
    tried to get current carrier package to rebind, the carrier package
    in CPT may not be updated yet, causing the rebinding fail.
    
    To fix the issue, CarrierPrivilegesCallback is introduced into
    CSBH which can rebind when reliably knowing carrier package has
    been updated.
    
    Bug: 232461097
    Test: atest CarrierServiceBindHelperTest
    Test: go/carrier-service-binding-sanity
    Test: Sanity test on telephony(voice, sms, data connection...)
    
    Merged-In: I09313571239da6a017a02b22aba247c94b45a35d
    Change-Id: I09313571239da6a017a02b22aba247c94b45a35d
    (cherry picked from commit 9e73af69a2b88033f27213c113670139c7718826)

--- tests/telephonytests/src/com/android/internal/telephony/CarrierServiceBindHelperTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/CarrierServiceBindHelperTest.java
@@ -18,10 +18,15 @@ package com.android.internal.telephony;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import android.os.Message;
+import android.telephony.TelephonyManager.CarrierPrivilegesCallback;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
@@ -30,10 +35,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 
 @RunWith(AndroidTestingRunner.class)
 @TestableLooper.RunWithLooper
 public class CarrierServiceBindHelperTest extends TelephonyTest {
+    private static final int PHONE_ID_0 = 0;
+    private static final int PHONE_ID_1 = 1;
+
     CarrierServiceBindHelper mCarrierServiceBindHelper;
     @Before
     public void setUp() throws Exception {
@@ -90,4 +99,38 @@ public class CarrierServiceBindHelperTest extends TelephonyTest {
                         CarrierServiceBindHelper.EVENT_PERFORM_IMMEDIATE_UNBIND,
                         new Integer(0)));
     }
+
+    @Test
+    public void testCarrierPrivilegesCallbackRegistration() {
+        // Device starts with DSDS mode
+        doReturn(2).when(mTelephonyManager).getActiveModemCount();
+        mCarrierServiceBindHelper = new CarrierServiceBindHelper(mContext);
+        processAllMessages();
+
+        // Verify that CarrierPrivilegesCallbacks are registered on both phones.
+        // Capture the callbacks for further verification
+        ArgumentCaptor<CarrierPrivilegesCallback> phone0CallbackCaptor = ArgumentCaptor.forClass(
+                CarrierPrivilegesCallback.class);
+        verify(mTelephonyManager).registerCarrierPrivilegesCallback(eq(PHONE_ID_0), any(),
+                phone0CallbackCaptor.capture());
+        CarrierPrivilegesCallback phone0Callback = phone0CallbackCaptor.getAllValues().get(0);
+        assertNotNull(phone0Callback);
+
+        ArgumentCaptor<CarrierPrivilegesCallback> phone1CallbackCaptor = ArgumentCaptor.forClass(
+                CarrierPrivilegesCallback.class);
+        verify(mTelephonyManager).registerCarrierPrivilegesCallback(eq(PHONE_ID_1), any(),
+                phone1CallbackCaptor.capture());
+        CarrierPrivilegesCallback phone1Callback = phone1CallbackCaptor.getAllValues().get(0);
+        assertNotNull(phone1Callback);
+
+        // Switch back to single SIM.
+        doReturn(1).when(mTelephonyManager).getActiveModemCount();
+        PhoneConfigurationManager.notifyMultiSimConfigChange(1);
+        processAllMessages();
+
+        // Verify the callback for phone1 had been unregistered while phone0 didn't.
+        verify(mTelephonyManager).unregisterCarrierPrivilegesCallback(eq(phone1Callback));
+        verify(mTelephonyManager, never()).unregisterCarrierPrivilegesCallback(eq(phone0Callback));
+    }
+    // TODO (b/232461097): Add UT cases to cover more scenarios (user unlock, SIM state change...)
 }
