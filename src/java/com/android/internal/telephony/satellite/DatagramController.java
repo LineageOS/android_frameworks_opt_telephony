e110a458e4331fa247bc146cab5883ae8328acde
Aishwarya Mallampati <amallampati@google.com>
2023-02-23 20:44:00 +0000

    Update SCO messaging pipeline.
    The following things are moved from SatelliteController to
    DatagramReciver:
    - SatelliteDatagramListenerHandler: stores incoming datagrams in DB and
      deletes from DB after receiving ack.
    - mDatagramReceiverAck
    - registerForSatelliteDatagram
    - unregisterForSatelliteDatagram
    - pollPendingSatelliteDatagrams
    
    The following things are moved from SatelliteController to
    DatagramDispatcher:
    - sendSatelliteDatagram: prioritizes sending emergency datagrams over
      non-emergency datagrams
    
    The following methods are moved from SatelliteController to
    SatelliteServiceUtils:
    - getSatelliteError()
    - getPhoneOrDefault()
    
    Bug: 269637555
    Test: atest SatelliteManagerTest, Basic sanity - flashed build on
    cheetah-userdebug: making calls and sending sms is working
    Change-Id: I919b2d603d71015d079a8ed2f61e7158b54cd89a

--- src/java/com/android/internal/telephony/satellite/DatagramController.java
+++ src/java/com/android/internal/telephony/satellite/DatagramController.java
@@ -17,9 +17,16 @@
 package com.android.internal.telephony.satellite;
 
 import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.content.Context;
-import android.os.Looper;
+import android.os.Message;
+import android.os.ResultReceiver;
 import android.telephony.Rlog;
+import android.telephony.satellite.ISatelliteDatagramCallback;
+import android.telephony.satellite.SatelliteDatagram;
+import android.telephony.satellite.SatelliteManager;
+
+import com.android.internal.telephony.Phone;
 
 /**
  * Datagram controller used for sending and receiving satellite datagrams.
@@ -68,6 +75,76 @@ public class DatagramController {
         // Create the DatagramReceiver singleton,
         // which is used to receive satellite datagrams.
         mDatagramReceiver = DatagramReceiver.make(mContext);
+    }
+
+    /**
+     * Register to receive incoming datagrams over satellite.
+     *
+     * @param subId The subId of the subscription to register for incoming satellite datagrams.
+     * @param datagramType datagram type indicating whether the datagram is of type
+     *                     SOS_SMS or LOCATION_SHARING.
+     * @param callback The callback to handle incoming datagrams over satellite.
+     *
+     * @return The {@link SatelliteManager.SatelliteError} result of the operation.
+     */
+    @SatelliteManager.SatelliteError public int registerForSatelliteDatagram(int subId,
+            @SatelliteManager.DatagramType int datagramType,
+            @NonNull ISatelliteDatagramCallback callback) {
+        return mDatagramReceiver.registerForSatelliteDatagram(subId, datagramType, callback);
+    }
+
+    /**
+     * Unregister to stop receiving incoming datagrams over satellite.
+     * If callback was not registered before, the request will be ignored.
+     *
+     * @param subId The subId of the subscription to unregister for incoming satellite datagrams.
+     * @param callback The callback that was passed to
+     *                 {@link #registerForSatelliteDatagram(int, int, ISatelliteDatagramCallback)}.
+     */
+    public void unregisterForSatelliteDatagram(int subId,
+            @NonNull ISatelliteDatagramCallback callback) {
+        mDatagramReceiver.unregisterForSatelliteDatagram(subId, callback);
+    }
+
+    /**
+     * Poll pending satellite datagrams over satellite.
+     *
+     * This method requests modem to check if there are any pending datagrams to be received over
+     * satellite. If there are any incoming datagrams, they will be received via
+     * {@link android.telephony.satellite.SatelliteDatagramCallback
+     * #onSatelliteDatagramReceived(long, SatelliteDatagram, int, ISatelliteDatagramReceiverAck)}
+     *
+     */
+    public void pollPendingSatelliteDatagrams(@NonNull Message message, @Nullable Phone phone) {
+        // TODO: set modemTransferState = SATELLITE_DATAGRAM_TRANSFER_STATE_RECEIVING
+        mDatagramReceiver.pollPendingSatelliteDatagrams(message, phone);
+    }
+
+    /**
+     * Send datagram over satellite.
+     *
+     * Gateway encodes SOS message or location sharing message into a datagram and passes it as
+     * input to this method. Datagram received here will be passed down to modem without any
+     * encoding or encryption.
+     *
+     * @param datagramId An id that uniquely identifies datagram requested to be sent.
+     * @param datagramType datagram type indicating whether the datagram is of type
+     *                     SOS_SMS or LOCATION_SHARING.
+     * @param datagram encoded gateway datagram which is encrypted by the caller.
+     *                 Datagram will be passed down to modem without any encoding or encryption.
+     * @param needFullScreenPointingUI this is used to indicate pointingUI app to open in
+     *                                 full screen mode.
+     * @param isSatelliteDemoModeEnabled True if satellite demo mode is enabled
+     * @param result The result receiver that returns datagramId if datagram is sent successfully
+     *               or {@link SatelliteManager.SatelliteError} of the request if it is failed.
+     */
+    public void sendSatelliteDatagram(long datagramId,
+            @SatelliteManager.DatagramType int datagramType, @NonNull SatelliteDatagram datagram,
+            boolean needFullScreenPointingUI, boolean isSatelliteDemoModeEnabled,
+            @NonNull ResultReceiver result) {
+        // TODO: set modemTransferState = SATELLITE_DATAGRAM_TRANSFER_STATE_SENDING
+        mDatagramDispatcher.sendSatelliteDatagram(datagramId, datagramType, datagram,
+                needFullScreenPointingUI, isSatelliteDemoModeEnabled, result);
     }
 
     private static void logd(@NonNull String log) {
