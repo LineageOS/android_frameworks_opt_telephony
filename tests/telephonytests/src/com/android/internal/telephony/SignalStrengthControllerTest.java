c0364c2f8f6a60429bb5e4e62efa9a2cba0a8fe9
Rambo Wang <rambowang@google.com>
2023-03-09 22:19:56 +0000

    Update SignalStrengthController with new CarrierConfigManager APIs
    - Replace carrier config change receiver with listener which has much
      lower lagency
    - Due to a large list of carrier configs are used and the list may change in near future, getConfigForSubId is NOT replaced with subset version.
    
    Bug: 263267340
    Test: atest SignalStrengthControllerTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:de3bb2f4e38215d6b81cc7fb0ba47cd11f4f86ae)
    Merged-In: Iab2ba44e502bb1134f2db9f3cd95c4a228e69c97
    
    Change-Id: Ifa12a583e94e3194ec695194909fe48d5b4effa3

--- tests/telephonytests/src/com/android/internal/telephony/SignalStrengthControllerTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/SignalStrengthControllerTest.java
@@ -23,6 +23,7 @@ import static android.telephony.SignalThresholdInfo.SIGNAL_MEASUREMENT_TYPE_SSSI
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
@@ -31,7 +32,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PersistableBundle;
@@ -47,6 +47,7 @@ import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.SignalStrength;
 import android.telephony.SignalStrengthUpdateRequest;
 import android.telephony.SignalThresholdInfo;
+import android.telephony.TelephonyManager;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
@@ -91,16 +92,13 @@ public class SignalStrengthControllerTest extends TelephonyTest {
 
     private SignalStrengthController mSsc;
     private PersistableBundle mBundle;
+    private CarrierConfigManager.CarrierConfigChangeListener mCarrierConfigChangeListener;
 
     @Before
     public void setUp() throws Exception {
         super.setUp(this.getClass().getSimpleName());
         mHandler = Mockito.mock(Handler.class);
-
         when(mPhone.getSubId()).thenReturn(ACTIVE_SUB_ID);
-        mSsc = new SignalStrengthController(mPhone);
-        replaceInstance(Handler.class, "mLooper", mHandler, mSsc.getLooper());
-        replaceInstance(Phone.class, "mLooper", mPhone, mSsc.getLooper());
 
         // Config a fixed supported RAN/MeasurementTypes to make the test more stable
         mBundle = mContextFixture.getCarrierConfigBundle();
@@ -147,6 +145,18 @@ public class SignalStrengthControllerTest extends TelephonyTest {
                         15, /* SIGNAL_STRENGTH_GOOD */
                         30  /* SIGNAL_STRENGTH_GREAT */
                 });
+
+        // Capture listener to emulate the carrier config change notification used later
+        ArgumentCaptor<CarrierConfigManager.CarrierConfigChangeListener> listenerArgumentCaptor =
+                ArgumentCaptor.forClass(CarrierConfigManager.CarrierConfigChangeListener.class);
+        mSsc = new SignalStrengthController(mPhone);
+        verify(mCarrierConfigManager).registerCarrierConfigChangeListener(any(),
+                listenerArgumentCaptor.capture());
+        mCarrierConfigChangeListener = listenerArgumentCaptor.getAllValues().get(0);
+
+        replaceInstance(Handler.class, "mLooper", mHandler, mSsc.getLooper());
+        replaceInstance(Phone.class, "mLooper", mPhone, mSsc.getLooper());
+
         processAllMessages();
         reset(mSimulatedCommandsVerifier);
     }
@@ -818,9 +828,8 @@ public class SignalStrengthControllerTest extends TelephonyTest {
                 .thenReturn(mockConfigManager);
         when(mockConfigManager.getConfigForSubId(anyInt())).thenReturn(mBundle);
 
-        Intent intent = new Intent().setAction(CarrierConfigManager.ACTION_CARRIER_CONFIG_CHANGED);
-        intent.putExtra(CarrierConfigManager.EXTRA_SLOT_INDEX, PHONE_ID);
-        mContext.sendBroadcast(intent);
+        mCarrierConfigChangeListener.onCarrierConfigChanged(PHONE_ID, ACTIVE_SUB_ID,
+                TelephonyManager.UNKNOWN_CARRIER_ID, TelephonyManager.UNKNOWN_CARRIER_ID);
         processAllMessages();
     }
 
