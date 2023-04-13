2a899fd3f1bcccb47ed5d0925776ea093e011171
Hunsuk Choi <forestchoi@google.com>
2023-04-13 14:56:18 +0000

    Copy eccCategory member when cloning ImsDialArgs
    Bug: 270101950
    Test: atest ImsPhoneTest#testImsDialArgsBuilderFromForAlternateService
    Change-Id: Ibdd71bf9ccbafdeeb066ef4ece73bbcdda0883e7

--- src/java/com/android/internal/telephony/imsphone/ImsPhone.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhone.java
@@ -196,6 +196,7 @@ public class ImsPhone extends ImsPhoneBase {
                     return new ImsDialArgs.Builder()
                             .setUusInfo(dialArgs.uusInfo)
                             .setIsEmergency(dialArgs.isEmergency)
+                            .setEccCategory(dialArgs.eccCategory)
                             .setVideoState(dialArgs.videoState)
                             .setIntentExtras(dialArgs.intentExtras)
                             .setRttTextStream(((ImsDialArgs)dialArgs).rttTextStream)
