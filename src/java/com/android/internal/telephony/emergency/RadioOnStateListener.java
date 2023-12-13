3cbb0ca8f67bcc0be8f74942f86756c6f50cff76
Hakjun Choi <hakjunc@google.com>
2023-12-13 09:49:05 +0000

    Rename the callback for listening satellite modem state change event
    Renamed from ISatelliteStateCallback to ISatelliteModemStateCallback to improve clarity of use.
    
    Bug: 297933128
    Test: atest SatelliteManagerTest, SatelliteManagerTestonMockService,
    SatelliteSessionControllerTest, RadioOnStateListenerTest
    Test: manual test using testSatelliteApp
    
    Change-Id: I5eb07b1f25b40433ee4d19c4d75c3f64f87bc736

--- src/java/com/android/internal/telephony/emergency/RadioOnStateListener.java
+++ src/java/com/android/internal/telephony/emergency/RadioOnStateListener.java
@@ -22,7 +22,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.telephony.ServiceState;
 import android.telephony.SubscriptionManager;
-import android.telephony.satellite.ISatelliteStateCallback;
+import android.telephony.satellite.ISatelliteModemStateCallback;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.SomeArgs;
@@ -142,7 +142,8 @@ public class RadioOnStateListener {
         }
     };
 
-    private final ISatelliteStateCallback mSatelliteCallback = new ISatelliteStateCallback.Stub() {
+    private final ISatelliteModemStateCallback mSatelliteCallback =
+            new ISatelliteModemStateCallback.Stub() {
         @Override
         public void onSatelliteModemStateChanged(int state) {
             mHandler.obtainMessage(MSG_SATELLITE_ENABLED_CHANGED).sendToTarget();
