848c6ac34c660c869ec02cc054928ded20aa9614
Sangyun Yun <sangyun@google.com>
2024-04-09 01:37:13 +0000

    Revert "Reload allowed networks from db after restore content."
    Revert submission 26793542-backup_and_restore_for_enable_2g
    
    Reason for revert: failing test
     - testBackupAndRestoreForAllowedNetworkTypesForReasons
    
    Reverted changes: /q/submissionid:26793542-backup_and_restore_for_enable_2g
    
    Change-Id: I02bbaea8cc01d1469a06b4d934c3b2b302262d05

--- src/java/com/android/internal/telephony/subscription/SubscriptionManagerService.java
+++ src/java/com/android/internal/telephony/subscription/SubscriptionManagerService.java
@@ -1565,10 +1565,6 @@ public class SubscriptionManagerService extends ISub.Stub {
                             SubscriptionManager.RESTORE_SIM_SPECIFIC_SETTINGS_DATABASE_UPDATED)) {
                         logl("Sim specific settings changed the database.");
                         mSubscriptionDatabaseManager.reloadDatabaseSync();
-                        if (mFeatureFlags.backupAndRestoreFor2gSetting()) {
-                            PhoneFactory.getPhone(phoneId)
-                                    .loadAllowedNetworksFromSubscriptionDatabase();
-                        }
                     }
                 }
 
@@ -4153,10 +4149,6 @@ public class SubscriptionManagerService extends ISub.Stub {
                     SubscriptionManager.RESTORE_SIM_SPECIFIC_SETTINGS_DATABASE_UPDATED)) {
                 logl("Sim specific settings changed the database.");
                 mSubscriptionDatabaseManager.reloadDatabaseSync();
-                if (mFeatureFlags.backupAndRestoreFor2gSetting()) {
-                    Arrays.stream(PhoneFactory.getPhones())
-                            .forEach(Phone::loadAllowedNetworksFromSubscriptionDatabase);
-                }
             }
         } finally {
             Binder.restoreCallingIdentity(token);
