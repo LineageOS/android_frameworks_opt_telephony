32175166066c1fbbcc577fcb2ed61b7ec82505be
Han Sol Jin <hansol@hansol.ca>
2024-04-08 22:19:47 -0700

    Revert "Clear up the obsoleted cascading signal strength polling logic"
    Some vendors appear to not use the standard UNSOL command to deliver
    signal strength, or not send an UNSOL at all. In particular, on LG
    devices with SDM845 or later chipset, LG's custom RIL stack does not
    appear to be sending neither the AOSP nor the LG variant of
    RIL_UNSOL_SIGNAL_STRENGTH whatsoever.
    
    Because the polling logic was removed, these devices are currently
    stuck on default SignalStrength values forever.
    
    Bring it back.
    
    This reverts commit 682dd4df959150fabdcb34efeef283e037bb2bc4.
    
    Test: *#*#4636#*#* -> disable/enable radio power
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/6954
    Change-Id: I149a129d39291fa67fa5c05c94fa7a4cb87e106b

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -1332,6 +1332,8 @@ public class ServiceStateTracker extends Handler {
                 mPrevSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
                 mIsSimReady = true;
                 pollStateInternal(false);
+                // Signal strength polling stops when radio is off
+                queueNextSignalStrengthPoll();
                 break;
 
             case EVENT_RADIO_STATE_CHANGED:
@@ -1341,6 +1343,9 @@ public class ServiceStateTracker extends Handler {
                 if(!mPhone.isPhoneTypeGsm() &&
                         mCi.getRadioState() == TelephonyManager.RADIO_POWER_ON) {
                     handleCdmaSubscriptionSource(mCdmaSSM.getCdmaSubscriptionSource());
+
+                    // Signal strength polling stops when radio is off.
+                    queueNextSignalStrengthPoll();
                 }
                 // This will do nothing in the 'radio not available' case
                 setPowerStateToDesired();
@@ -4701,6 +4706,10 @@ public class ServiceStateTracker extends Handler {
             return mUiccController.getUiccCardApplication(mPhone.getPhoneId(),
                     UiccController.APP_FAM_3GPP2);
         }
+    }
+
+    private void queueNextSignalStrengthPoll() {
+        mPhone.getSignalStrengthController().queueNextSignalStrengthPoll();
     }
 
     private void notifyCdmaSubscriptionInfoReady() {
