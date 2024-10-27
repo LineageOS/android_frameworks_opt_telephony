8117849b8cbecef00a24c15d3185f493ed9362b7
LuK1337 <priv.luk@gmail.com>
2024-10-27 21:25:32 +0100

    PhoneSwitcher: Skip state check guard for HAL_COMMAND_ALLOW_DATA users
    Test: On nile, set data sim to slot#2 and reboot, notice that data works
          on every boot.
    Change-Id: I0015489779fa7e29487f24546638605f57d90661

--- src/java/com/android/internal/telephony/data/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/data/PhoneSwitcher.java
@@ -1272,7 +1272,7 @@ public class PhoneSwitcher extends Handler {
 
     private void switchPhone(int phoneId, boolean active) {
         PhoneState state = mPhoneStates[phoneId];
-        if (state.active == active) return;
+        if (mHalCommandToUse != HAL_COMMAND_ALLOW_DATA && state.active == active) return;
         state.active = active;
         logl((active ? "activate " : "deactivate ") + phoneId);
         state.lastRequested = System.currentTimeMillis();
