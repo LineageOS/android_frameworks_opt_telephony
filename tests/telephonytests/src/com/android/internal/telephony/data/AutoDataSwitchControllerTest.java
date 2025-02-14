a294d1ccb42d32b2d1ae9f17d30067b605edecb7
Ling Ma <linggm@google.com>
2025-02-13 17:20:51 -0800

    Enable auto switch to satellite
    Fix: 393553449
    Test: manual repro + review log
    Test: basic voice call + data browsing
    Flag: EXEMPT bug fix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:11698a3934bd37f218386328418d416821149bb9)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:cfe0bc6ce425df5e0006c6367b89b1ef11ad734d)
    Merged-In: I0ba3c965e439171f974b8d358e3816988fafe23a
    Change-Id: I0ba3c965e439171f974b8d358e3816988fafe23a

--- tests/telephonytests/src/com/android/internal/telephony/data/AutoDataSwitchControllerTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/data/AutoDataSwitchControllerTest.java
@@ -142,7 +142,9 @@ public class AutoDataSwitchControllerTest extends TelephonyTest {
                     .when(phone).isUserDataEnabled();
         }
         mDataEvaluation = new DataEvaluation(DataEvaluation.DataEvaluationReason.EXTERNAL_QUERY);
-        doReturn(mDataEvaluation).when(mDataNetworkController).getInternetEvaluation(anyBoolean());
+        doReturn(mDataEvaluation).when(mDataNetworkController).evaluateNetworkRequest(
+                any(TelephonyNetworkRequest.class),
+                eq(DataEvaluation.DataEvaluationReason.EXTERNAL_QUERY));
         doReturn(new int[]{SUB_1, SUB_2}).when(mSubscriptionManagerService)
                 .getActiveSubIdList(true);
         doAnswer(invocation -> {
@@ -167,6 +169,8 @@ public class AutoDataSwitchControllerTest extends TelephonyTest {
                 .getAutoDataSwitchAvailabilityStabilityTimeThreshold();
         doReturn(120000L).when(mDataConfigManager)
                 .getAutoDataSwitchPerformanceStabilityTimeThreshold();
+        doReturn(150000L).when(mDataConfigManager)
+                .getAutoDataSwitchAvailabilitySwitchbackStabilityTimeThreshold();
         doReturn(MAX_RETRY).when(mDataConfigManager).getAutoDataSwitchValidationMaxRetry();
         doReturn(SCORE_TOLERANCE).when(mDataConfigManager).getAutoDataSwitchScoreTolerance();
         doAnswer(invocation -> {
@@ -252,7 +256,9 @@ public class AutoDataSwitchControllerTest extends TelephonyTest {
         mDataEvaluation.addDataDisallowedReason(DataEvaluation.DataDisallowedReason
                 .NO_SUITABLE_DATA_PROFILE);
         doReturn(mDataEvaluation)
-                .when(mDataNetworkController).getInternetEvaluation(anyBoolean());
+                .when(mDataNetworkController).evaluateNetworkRequest(
+                        any(TelephonyNetworkRequest.class),
+                        eq(DataEvaluation.DataEvaluationReason.EXTERNAL_QUERY));
         mAutoDataSwitchControllerUT.evaluateAutoDataSwitch(EVALUATION_REASON_DATA_SETTINGS_CHANGED);
         processAllFutureMessages();
 
