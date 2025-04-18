0fafa287b73d6a174ae5d9c5a57ce6bcbcef0169
Michael W <baddaemon87@gmail.com>
2025-04-18 10:47:31 +0200

    telephony: Make "Do not ask again" functional
    * When on WiFi I get a notification telling me emergency services are
      not available with a "Do not ask again" button. This button is not
      functional, thus I am being asked again all the time
    * Exporting the receiver finally makes the notification disappear
    
    Change-Id: I32408f08e7c6e29e5fc8b6386129e4f897fd761a

--- src/java/com/android/internal/telephony/CarrierServiceStateTracker.java
+++ src/java/com/android/internal/telephony/CarrierServiceStateTracker.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2025 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -178,7 +179,7 @@ public class CarrierServiceStateTracker extends Handler {
             mPhone.getContext().registerReceiver(
                     mActionReceiver,
                     new IntentFilter(ACTION_NEVER_ASK_AGAIN),
-                    Context.RECEIVER_NOT_EXPORTED);
+                    Context.RECEIVER_EXPORTED);
         }
     }
 
